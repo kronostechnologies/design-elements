5e7ebd87e7b70c2ef108588dbf3520248129b394
Pierre-Luc Paquin <pierre-luc.paquin@equisoft.com>
2019-03-22 07:52:31 -0400

    Admail

---
+++ packages/react/src/components/rolling-enso.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import enso from '../icons/enso.svg';
+
+export default styled(enso)`
+    animation: roll 1s infinite;
+    animation-timing-function: linear;
+    fill: #E2732D;
+    height: 80px;
+    width: 83px;
+`;
