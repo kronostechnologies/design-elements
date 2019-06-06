13db75978d5ebfe3d77c61e6ca8b523264c1f8f6
Hugo Soucy <hugo.soucy@equisoft.com>
2019-06-06 12:57:11 -0400

    Add textarea component.

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,3 +1,4 @@
+// Miscellaneous
 export Card from './components/card';
 export Enso from './components/enso';
 export Headband from './components/headband';
@@ -5,6 +6,7 @@ export Progress from './components/progress';
 export RollingEnso from './components/enso';
 export SVG from './components/svg';
 
+// Buttons
 export ButtonPrimary from './components/buttons/primary';
 export ButtonSecondary from './components/buttons/secondary';
 export ButtonTertiary from './components/buttons/tertiary';
@@ -12,8 +14,11 @@ export ButtonTertiary from './components/buttons/tertiary';
 export ButtonAddPrimary from './components/buttons/add/primary';
 export ButtonAddSecondary from './components/buttons/add/secondary';
 
+// Forms Elements
 export Checkbox from './components/inputs/checkbox';
 export OptionButton from './components/inputs/option';
 export SearchInput from './components/inputs/search';
 export Select from './components/inputs/select';
 export TextInput from './components/inputs/text';
+
+export Textarea from './components/textarea';
