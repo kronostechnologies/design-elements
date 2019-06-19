ade339d3f82576dd8f8fc6cad81eb288b4843416
Hugo Soucy <hugo.soucy@equisoft.com>
2019-06-19 15:07:32 -0400

    Adds a visuallyhidden styles and component

---
+++ packages/react/src/components/a11y/visuallyhidden.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import styles from './styles/visuallyhidden';
+
+const VisuallyHidden = styled.span`
+  ${styles}
+`;
+
+export default ({ children }) => (
+    <VisuallyHidden>
+        {children}
+    </VisuallyHidden>
+);
