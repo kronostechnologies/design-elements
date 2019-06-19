52e3d2487716ff841086463972c472be35601c5a
Hugo Soucy <hugo.soucy@equisoft.com>
2019-06-19 14:16:47 -0400

    Delete not needed (anymore) components

--- packages/react/src/components/inputs/select.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export default ({ children }) => <select>{children}</select>;
--- packages/react/src/components/inputs/text.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default ({ label }) => (
-    <div>
-        <label htmlFor="id">{label}</label>
-        <input id="id" type="text" />
-    </div>
-);
