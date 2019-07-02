14e06438fa9e865752d38cbb65f1b313e6453769
Christian Roy <christian-roy@users.noreply.github.com>
2019-07-01 21:28:08 -0400

    Card update

--- packages/react/src/components/card.js
+++ packages/react/src/components/card.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  background-color: #fff;
-  border: 1px solid #dcdcdc;
-  border-radius: 8px;
-  box-shadow: 0 3px 6px 0 rgba(1, 38, 57, 0.2);
-  margin-bottom: 48px;
-  padding: 32px;
+  background: rgb(255, 255, 255);
+  border: 1px solid rgb(220, 220, 220);
+  border-radius: 0.5rem;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  margin-bottom: 3rem;
+  padding: 2rem;
 `;
