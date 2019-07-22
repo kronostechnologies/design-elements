ce850a5b1a33cadf4bf1d76726a070dd65216e8e
Christian Roy <christian-roy@users.noreply.github.com>
2019-07-22 13:59:17 -0400

    Bumps design-elements-react to 2.0.0 in Storybook
    TextInput refactor to include React state and callback functions
    
    TextArea uses React state and function callbacks
    
    Adds React state and callbacks to TextArea, buttons
    
    Adds callbacks to search inputs
    
    Don't pass event when not necessary

--- packages/react/src/components/forms/inputs/search-global.jsx
+++ packages/react/src/components/forms/inputs/search-global.jsx
@@ -1,31 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import SearchInput from './search-input';
-import SearchButton from '../../buttons/search-button.jsx';
 
-const SearchSubmit = styled(SearchButton)`
-  border-left: 0;
-  border-radius: 0 0.25rem 0.25rem 0;
-  position: relative;
-`;
-
-const SearchGlobal = ({ disabled, id, label, onSubmit }) => (
+const SearchGlobal = ({ disabled, id, label, onSearch }) => (
     <SearchInput
         disabled={disabled}
+        hasButton
         id={id}
         label={label}
-        onSubmit={onSubmit}
-        global
-    >
-        <SearchSubmit
-            disabled={disabled}
-            type="submit"
-            onClick={onSubmit}
-        >
-            Search
-        </SearchSubmit>
-    </SearchInput>
+        onSearch={onSearch}
+    />
 );
 
 export default SearchGlobal;
--- packages/storybook/stories/buttons.jsx
+++ packages/storybook/stories/buttons.jsx
@@ -5,13 +5,26 @@ import { AddButton, Button } from '@equisoft/design-elements-react';
 
 storiesOf('Buttons', module)
     .add('Primary', () => (
-        <Button type="primary">Primary Button</Button>
+        <Button type="primary">
+            Primary Button
+        </Button>
     ))
     .add('Secondary', () => (
         <Button type="secondary">Secondary Button</Button>
     ))
     .add('Tertiary', () => (
         <Button type="tertiary">Tertiary Button</Button>
+    ))
+    .add('Disabled', () => (
+        <Button disabled type="primary">Disabled Button</Button>
+    ))
+    .add('Event callback', () => (
+        <Button
+            onClick={() => {console.log("The button has been clicked!")}}
+            type="primary"
+        >
+            See Console For Callback
+        </Button>
     ));
 
 storiesOf('Buttons/Add', module)
