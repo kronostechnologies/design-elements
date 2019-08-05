432b9e79aac3664db5838585e9a97ac8ce9de09c
mgendron-equisofte <maxime.gendron@equisoft.comm>
2019-08-05 16:36:33 -0400

    description style

--- packages/react/src/components/results/Legend.jsx
+++ packages/react/src/components/results/Legend.jsx
@@ -1,50 +1,64 @@
-import React from 'react';
-import styled from 'styled-components';
-
-
+import React from "react";
+import styled from "styled-components";
 
 const Item = styled.li`
-    list-style: none;
-    display: flex;
+  list-style: none;
+  display: flex;
+  margin: 15px 0;
 
-    ::before {
-        content: '•';
-        font-size: 2.5em;
-        color: ${props => props.color|| "red"};
-        font-weight: bold;
-        display: inline; 
-        width: 0.5em;
-        margin-left: -0.5em;
-        margin-top: -0.4em;
-    }
+  & p{
+      margin: 0;
+  }
+
+  ::before {
+    content: "•";
+    font-size: 2.8em;
+    color: ${props => props.color || "#27A7C9"};
+    font-weight: bold;
+    display: inline;
+    width: 0.5em;
+    margin-left: -0.5em;
+    margin-top: -0.4em;
+  }
+`;
+
+const Description = styled.span`
+  font-size: 0.9em;
+  color: gray;
 `;
 
 const legend = props => {
+  
+    // Variable de test
+  const legend = [
+    {
+      name: "Vous",
+      description: "Données provenants de vos réponses"
+    },
+    {
+      name: "Pairs d'Equisoft",
+      description: "Données privée d'Equisoft",
+      color: "#000014"
+    },
+    {
+      name: "Pairs Général",
+      description: "Données publiques accessible à tous",
+      color: "#304E63"
+    }
+  ];
 
-    const legend = [
-        {
-            name: "Barry",
-            description: "white"
-        },
-        {
-            name: "Samuel",
-            description: "L.Chang",
-            color: '#E87BDB'
-        },
-    ]
-
-    return (
-        <ul>
-            {legend.map(lgd => (
-                <Item color={lgd.color}>
-                    <div>
-                        {lgd.name}<br />
-                        <span>{lgd.description}</span>
-                    </div>
-                </Item>
-            ))}
-        </ul>
-    )
-}
+  return (
+    <ul>
+      {legend.map(lgd => (
+        <Item color={lgd.color}>
+          <div>
+            <p>{lgd.name}</p>
+            <Description>{lgd.description}</Description>
+          </div>
+        </Item>
+      ))}
+    </ul>
+  );
+};
 
 export default legend;
