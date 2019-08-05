7ca6c22f94115aeb712e319f54764a4959faa974
mgendron-equisofte <maxime.gendron@equisoft.comm>
2019-08-05 15:53:26 -0400

    List Style Props Color

--- packages/react/src/components/results/Legend.jsx
+++ packages/react/src/components/results/Legend.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+
+const Item = styled.li`
+    list-style: none;
+    display: flex;
+
+    ::before {
+        content: '•';
+        font-size: 2.5em;
+        color: ${props => props.color|| "red"};
+        font-weight: bold;
+        display: inline; 
+        width: 0.5em;
+        margin-left: -0.5em;
+        margin-top: -0.4em;
+    }
+`;
 
 const legend = props => {
+
     const legend = [
         {
             name: "Barry",
@@ -8,17 +28,20 @@ const legend = props => {
         },
         {
             name: "Samuel",
-            description: "L.Chang"
+            description: "L.Chang",
+            color: '#E87BDB'
         },
     ]
 
     return (
         <ul>
             {legend.map(lgd => (
-                <li>
-                    <p>{lgd.name}</p>
-                    <span>{lgd.description}</span>
-                </li>
+                <Item color={lgd.color}>
+                    <div>
+                        {lgd.name}<br />
+                        <span>{lgd.description}</span>
+                    </div>
+                </Item>
             ))}
         </ul>
     )
