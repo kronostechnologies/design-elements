c58a510db6e112ca28614300a298466f6fa38359
Christian Roy <christian-roy@users.noreply.github.com>
2019-08-06 16:06:40 -0400

    Refacors buttons to add TypeScript typings

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -4,7 +4,7 @@ import svgr from '@svgr/rollup';
 import typescript from 'rollup-plugin-typescript';
 
 export default {
-    input: 'src/index.js',
+    input: 'src/index.ts',
     output: {
         file: 'dist/design-elements-react.cjs.js',
         format: 'cjs',
