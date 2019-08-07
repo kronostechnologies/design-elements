d42be4e297197576535263decc6ea05babeaaf93
mgendron-equisofte <maxime.gendron@equisoft.comm>
2019-08-07 12:41:55 -0400

    Class based to functionnal

--- packages/react/src/components/results/progress-circle.jsx
+++ packages/react/src/components/results/progress-circle.jsx
@@ -1,94 +1,87 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 
 // Source: https://css-tricks.com/building-progress-ring-quickly/
-class ProgressCircle extends React.Component {
-  constructor(props) {
-    super(props);
+const progressCircle = props => {
 
-    const { radius, stroke } = this.props;
-    this.normalizedRadius = radius - stroke * 2;
-    this.circumference = this.normalizedRadius * 2 * Math.PI;
-  }
+  const { radius, stroke, progress } = props;
+  const normalizedRadius = radius - stroke * 2;
+  const circumference = normalizedRadius * 2 * Math.PI;
+  const PlaceholderDashoffset = circumference - 1 * circumference;
+  const strokeDashoffset =
+    circumference - (progress / 100) * circumference;
 
-  render() {
-    const { radius, stroke, progress } = this.props;
-    const PlaceholderDashoffset = this.circumference - 1 * this.circumference;
-    const strokeDashoffset =
-      this.circumference - (progress / 100) * this.circumference;
+  // Styled Components
+  const Number = styled.div`
+    width: ${radius * 2}px;
+    height: ${radius * 2}px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    p {
+      font-size: 1.625rem;
+    }
+  `;
 
-    // Styled Components
-    const Number = styled.div`
-      width: ${radius * 2}px;
-      height: ${radius * 2}px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: absolute;
-      top: 0;
-      left: 0;
-      p {
-        font-size: 1.625rem;
-      }
-    `;
+  const Label = styled.p`
+    width: ${radius * 2}px;
+    text-align: center;
+  `;
 
-    const Label = styled.p`
-      width: ${radius * 2}px;
-      text-align: center;
-    `;
+  const CirclePath = styled.circle`
+    transform: rotate(90deg);
+    transform-origin: 50% 50%;
+  `;
 
-    const CirclePath = styled.circle`
-      transform: rotate(90deg);
-      transform-origin: 50% 50%;
-    `;
+  const Wrapper = styled.div`
+    position: relative;
+    width: ${radius * 2}px;
+    height: ${radius * 2}px;
+  `;
 
-    const Wrapper = styled.div`
-      position: relative;
-      width: ${radius * 2}px;
-      height: ${radius * 2}px;
-    `;
+  const Container = styled.div`
+    display: inline-block;
+  `;
 
-    const Container = styled.div`
-      display: inline-block;
-    `;
+  return (
+    <Container>
+      <Wrapper>
+        <svg height={radius * 2} width={radius * 2}>
+          <circle
+            stroke="#DCDCDC"
+            fill="transparent"
+            strokeWidth={stroke}
+            strokeDasharray={circumference + " " + circumference}
+            style={{ PlaceholderDashoffset }}
+            stroke-width={stroke}
+            strokeLinecap="round"
+            r={normalizedRadius}
+            cx={radius}
+            cy={radius}
+          />
+          <CirclePath
+            stroke={props.color}
+            fill="transparent"
+            strokeWidth={stroke}
+            strokeDasharray={circumference + " " + circumference}
+            style={{ strokeDashoffset }}
+            stroke-width={stroke}
+            strokeLinecap="round"
+            r={normalizedRadius}
+            cx={radius}
+            cy={radius}
+          />
+        </svg>
+        <Number>
+          <p>{props.number}</p>
+        </Number>
+      </Wrapper>
+      <Label>{props.label}</Label>
+    </Container>
+  );
+};
 
-    return (
-      <Container>
-        <Wrapper>
-          <svg height={radius * 2} width={radius * 2}>
-            <circle
-              stroke="#DCDCDC"
-              fill="transparent"
-              strokeWidth={stroke}
-              strokeDasharray={this.circumference + " " + this.circumference}
-              style={{ PlaceholderDashoffset }}
-              stroke-width={stroke}
-              strokeLinecap="round"
-              r={this.normalizedRadius}
-              cx={radius}
-              cy={radius}
-            />
-            <CirclePath
-              stroke={this.props.color}
-              fill="transparent"
-              strokeWidth={stroke}
-              strokeDasharray={this.circumference + " " + this.circumference}
-              style={{ strokeDashoffset }}
-              stroke-width={stroke}
-              strokeLinecap="round"
-              r={this.normalizedRadius}
-              cx={radius}
-              cy={radius}
-            />
-          </svg>
-          <Number>
-            <p>{this.props.number}</p>
-          </Number>
-        </Wrapper>
-        <Label>{this.props.label}</Label>
-      </Container>
-    );
-  }
-}
-
-export default ProgressCircle;
+export default progressCircle;
