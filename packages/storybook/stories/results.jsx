0c56edae13e338b4c33626e3197794798ef5bb75
Christian Roy <christian-roy@users.noreply.github.com>
2019-08-13 11:17:12 -0400

    wip

--- packages/storybook/stories/results.jsx
+++ packages/storybook/stories/results.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Legend } from '@equisoft/design-elements-react';
 
-const legendItems = [
-    {
-      name: "Vous",
-      description: "Données provenants de vos réponses"
+const legendItems = [{
+        name: "Vous",
+        description: "Données provenants de vos réponses"
     },
     {
-      name: "Pairs Equisoft",
-      description: "Données privée d'Equisoft",
-      color: "#000014"
+        name: "Pairs Equisoft",
+        description: "Données privée d'Equisoft",
+        color: "#000014"
     },
     {
-      name: "Pairs Général",
-      description: "Données publiques accessible à tous",
-      color: "#304E63"
+        name: "Pairs Général",
+        description: "Données publiques accessible à tous",
+        color: "#304E63"
     }
 ];
 
