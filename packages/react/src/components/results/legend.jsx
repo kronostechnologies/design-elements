f5a1077116e33c612627bee95411e768356eec66
mgendron-equisofte <maxime.gendron@equisoft.comm>
2019-08-13 09:26:06 -0400

    PR fixes

--- packages/react/src/components/results/legend.jsx
+++ packages/react/src/components/results/legend.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 const List = styled.ul`
-    margin: 0;
-    padding: 0 1.2rem;
+  margin: 0;
+  padding: 0 1.2rem;
 `;
 
 const Item = styled.li`
-    list-style: none;
-    display: flex;
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0 0 15px;
+
+  p {
+    font-size: 0.875rem;
     margin: 0;
-    padding: 0 0 15px;
+  }
 
-    p {
-        margin: 0;
-        font-size: 0.875rem;
-    }
-
-    ::before {
-        content: '•';
-        font-size: 2.6em;
-        color: ${props => props.color || 'rgb(101, 226, 255)'};
-        font-weight: bold;
-        width: 0.5em;
-        margin: -0.4em 0 0 -0.5em;
-    }
+  ::before {
+    color: ${props => props.color || 'rgb(101, 226, 255)'};
+    content: '•';
+    font-size: 2.6em;
+    font-weight: 600;
+    margin: -0.4em 0 0 -0.5em;
+    width: 0.5em;
+  }
 `;
 
 const Description = styled.span`
-    font-size: 0.75rem;
-    color: rgb(87, 102, 110);
+  color: rgb(87, 102, 110);
+  font-size: 0.75rem;
 `;
 
 const legend = ({ items }) => (
