28bd5f48cf8f478833fbaf83d82330382fed8418
mgendron-equisofte <maxime.gendron@equisoft.comm>
2019-08-21 10:44:58 -0400

    Fixes

--- packages/react/src/components/headband/equisoft-default.tsx
+++ packages/react/src/components/headband/equisoft-default.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Child } from '../buttons/abstract-button';
 import { MediaView } from '../media-view';
 import { breakpoints } from '../tokens/breakpoints';
 
