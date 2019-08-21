8e814c6eb6e2932fb94e35bd37313e7fc80ab73e
Christian Roy <christian-roy@users.noreply.github.com>
2019-08-21 19:47:37 -0400

    Removes build files and and package-lock

--- packages/storybook/build/lib/card.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const react_1 = require("@storybook/react");
-const design_elements_react_1 = require("@equisoft/design-elements-react");
-react_1.storiesOf('Card', module)
-    .add('default', () => (React.createElement(design_elements_react_1.Card, null, "Hello, World!")));
-//# sourceMappingURL=card.js.map
