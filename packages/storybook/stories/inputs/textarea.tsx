5c344f2a500015d838f5024f5994444d2f3b90fc
mgendron-equisofte <maxime.gendron@equisoft.comm>
2019-08-22 10:05:51 -0400

    fixed validMsg entries on textarea

--- packages/storybook/stories/inputs/textarea.tsx
+++ packages/storybook/stories/inputs/textarea.tsx
@@ -7,7 +7,6 @@ storiesOf('TextArea', module)
         <TextArea
             id="ta_normal"
             label="Text area label"
-            validMsg='Temporary Message'
         />
     ))
     .add('Event callbacks (see console)', () => (
@@ -17,14 +16,13 @@ storiesOf('TextArea', module)
             changeCallback={(value) => console.log(`Custom function called on change. Current value: ${value}`)}
             blurCallback={(value) => console.log(`Custom function called on blur. Current value: ${value}`)}
             focusCallback={(value) => console.log(`Custom function called on focus. Current value: ${value}`)}
-            validMsg='Temporary Message'
         />
     ))
     .add('Required', () => (
         <TextArea
             id="ta_required"
             label="Text area label"
-            validMsg='Temporary Message'
+            validMsg="This field is required"
             required
         />
     ))
@@ -33,7 +31,7 @@ storiesOf('TextArea', module)
             defaultValue="Nullam eu ante vel est convallis dignissim. Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio."
             id="ta_filled"
             label="A label for a filled text area"
-            validMsg='Temporary Message'
+            validMsg="This field is required"
             required
         />
     ))
@@ -42,7 +40,7 @@ storiesOf('TextArea', module)
             disabled
             id="ta_disabled"
             label="A label for the disabled text area"
-            validMsg='Temporary Message'
+            validMsg="This field is required"
             required
         />
     ));
