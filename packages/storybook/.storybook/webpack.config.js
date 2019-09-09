b47ac66455fe71b2c91eb44fa041728104b3ef4d
Christian Roy <christian-roy@users.noreply.github.com>
2019-09-09 15:43:03 -0400

    Removes design-elements-web from React peer dependencies

--- packages/storybook/.storybook/webpack.config.js
+++ packages/storybook/.storybook/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     resolve: {
         alias: {
-            react: path.resolve(__dirname, '../node_modules/react'),
+            'react': path.resolve(__dirname, '../node_modules/react'),
             'react-dom': path.resolve(__dirname, '../node_modules/react-dom'),
             'styled-components': path.resolve(__dirname, '../node_modules/styled-components'),
         }
