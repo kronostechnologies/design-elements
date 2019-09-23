76a9d59a21b2663b9b08bcbba8934af99bb01166
Christian Roy <christian-roy@users.noreply.github.com>
2019-09-23 10:25:37 -0400

    Removes build folder from git

--- packages/storybook/build/lib/enso-spinner.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const react_1 = require("@storybook/react");
-const design_elements_react_1 = require("@equisoft/design-elements-react");
-react_1.storiesOf('Spinners', module)
-    .add('Enso', () => (React.createElement(design_elements_react_1.EnsoSpinner, null)));
-//# sourceMappingURL=enso-spinner.js.map
--- packages/storybook/build/lib/headband.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const react_1 = require("@storybook/react");
-const design_elements_react_1 = require("@equisoft/design-elements-react");
-react_1.storiesOf('Headband', module)
-    .add('Equisoft Default', () => (React.createElement(design_elements_react_1.Headband, { appName: "Benchmark" }, "Hello, World!")));
-//# sourceMappingURL=headband.js.map
