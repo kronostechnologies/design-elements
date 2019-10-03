6003ec28d032439325e075618c7c5729a779f3ec
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-10-03 12:50:15 -0400

    Added snapshot tests

---
+++ packages/react/test/date-picker.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import DatePicker from '../src/components/forms/inputs/date-picker/date-picker';
+
+const setup = (position: string) => {
+    const tree = renderer.create(
+        <DatePicker position={position} />,
+    ).toJSON();
+    return tree;
+};
+
+describe('Date Picker', () => {
+    test('Renders top left calendar', () => {
+        const tree = setup('topLeft');
+        expect(tree).toMatchSnapshot();
+    });
+    test('Renders top right calendar', () => {
+        const tree = setup('topRight');
+        expect(tree).toMatchSnapshot();
+    });
+    test('Renders bottom left calendar', () => {
+        const tree = setup('bottomLeft');
+        expect(tree).toMatchSnapshot();
+    });
+    test('Renders bottom right calendar', () => {
+        const tree = setup('bottomRight');
+        expect(tree).toMatchSnapshot();
+    });
+});
