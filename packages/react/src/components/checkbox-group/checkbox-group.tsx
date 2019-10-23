6fe755bc8a9f4109d6000fe58a705a4f073c787a
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-10-23 15:07:33 -0400

    Adds theme to checkbox-group

--- packages/react/src/components/checkbox-group/checkbox-group.tsx
+++ packages/react/src/components/checkbox-group/checkbox-group.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, ReactElement } from 'react';
 
 import CheckMark from 'feather-icons/dist/icons/check.svg';
 import styled from 'styled-components';
+import { equisoftTheme } from '../../themes/equisoft';
 
 const Legend = styled.legend`
   font-size: 0.75rem;
@@ -10,54 +11,66 @@ const Legend = styled.legend`
 `;
 
 const StyledCheckMark = styled(CheckMark)`
-  color: #fff;
-  height: 100%;
-  opacity: 0;
-  width: 100%;
+    color: ${equisoftTheme.greys.white};
+    height: 100%;
+    opacity: 0;
+    width: 100%;
 `;
 
-const StyledLabel = styled.label `
-  cursor: ${(props: { disabled?: boolean }) => props.disabled ? 'default' : 'pointer;'};
-  display: block;
-  font-size: 0.875rem;
-  padding-left: 24px;
-  position: relative;
-  user-select: none;
+const StyledLabel = styled.label`
+    ${(props: {theme?: Theme, disabled: boolean | undefined}) => {
+        let theme = props.theme;
+        if (theme) {
+            if (Object.entries(theme).length === 0 && theme.constructor === Object) {
+                theme = equisoftTheme;
+            }
+        } else {
+            theme = equisoftTheme;
+        }
+        return (`
+            ${props.disabled ? '' : 'cursor: pointer;'}
+            display: block;
+            font-size: 0.875rem;
+            padding-left: 24px;
+            position: relative;
+            user-select: none;
 
-  &:not(:first-of-type) {
-    margin-top: 16px;
-  }
+            &:not(:first-of-type) {
+                margin-top: 16px;
+            }
 
-  .box {
-    align-self: center;
-    background-color: ${(props: { disabled?: boolean }) => props.disabled ? '#f1f2f2' : '#fff'};
-    border: 1px solid ${(props: { disabled?: boolean }) => props.disabled ? '#d9dde2' : '#57666e'};
-    border-radius: 5px;
-    display: inline-block;
-    height: 16px;
-    left: 0;
-    margin-top: 2px;
-    position: absolute;
-    top: 0;
-    width: 16px;
-  }
+            input {
+                display: none;
 
-  &:hover .box {
-    border: 1px solid ${(props: { disabled?: boolean }) => props.disabled ? '#d9dde2' : '#0080a5'};
-  }
+                &:checked + .box {
+                    background-color: ${theme.main['primary-1.1']};
+                    border: 1px solid ${theme.main['primary-1.1']};
 
-  input {
-    display: none;
+                    ${StyledCheckMark} {
+                        opacity: 1;
+                    }
+                }
+            }
 
-    &:checked + .box {
-      background-color: #0080a5;
-      border: 1px solid #0080a5;
+            .box {
+                align-self: center;
+                background-color: ${props.disabled ? theme.greys['light-grey'] : theme.greys.white};
+                border: 1px solid ${props.disabled ? theme.greys.grey : theme.greys['dark-grey']};
+                border-radius: 5px;
+                display: inline-block;
+                height: 16px;
+                left: 0;
+                margin-top: 2px;
+                position: absolute;
+                top: 0;
+                width: 16px;
+            }
 
-      ${StyledCheckMark} {
-        opacity: 1;
-      }
-    }
-  }
+            &:hover .box {
+                border: 1px solid ${props.disabled ? theme.greys.grey : theme.main['primary-1.1']};
+            }
+        `);
+    }}
 `;
 
 interface CheckboxProps {
--- packages/react/src/themes/greys.ts
+++ packages/react/src/themes/greys.ts
@@ -1,9 +1,9 @@
 export default {
-    'white': '#FFFFFF',
+    white: '#FFFFFF',
     'colored-white': '#F8F8FA',
     'light-grey': '#F1F2F2',
-    'grey': '#D9DDE2',
+    grey: '#D9DDE2',
     'mid-grey': '#9CA7B4',
-    'dark-grey': '57666E',
-    'black': '#000000',
-}
+    'dark-grey': '#57666E',
+    black: '#000000',
+};
