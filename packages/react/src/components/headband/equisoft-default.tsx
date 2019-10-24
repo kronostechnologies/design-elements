5dc00a0576591a5581a5ac8c5a7e919607ece087
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-10-24 09:50:51 -0400

    Adds theme to headband

--- packages/react/src/components/headband/equisoft-default.tsx
+++ packages/react/src/components/headband/equisoft-default.tsx
@@ -6,23 +6,36 @@ import { MediaView } from '../media-view/media-view';
 
 import EquisoftIco from '../../logos/logo-equisoft-ico.svg';
 import EquisoftLogo from '../../logos/logo-equisoft-reversed.svg';
+import { equisoftTheme } from '../../themes/equisoft';
 
 const tabletMin = `${(breakpoints.tablet / 16)}rem`;
 
 const Header = styled.header`
-  align-items: center;
-  background: rgba(1, 38, 57, 1);
-  box-sizing: border-box;
-  color: rgba(255, 255, 255);
-  display: flex;
-  justify-content: space-between;
-  min-height: 2.75rem;
-  padding: 0.75rem 1rem;
+  ${(props: {theme?: Theme}) => {
+      let theme = props.theme;
+      if (theme) {
+          if (Object.entries(theme).length === 0 && theme.constructor === Object) {
+              theme = equisoftTheme;
+          }
+      } else {
+          theme = equisoftTheme;
+      }
+      return (`
+      align-items: center;
+      background: ${theme.main['primary-2']};
+      box-sizing: border-box;
+      color: ${theme.greys.white};
+      display: flex;
+      justify-content: space-between;
+      min-height: 2.75rem;
+      padding: 0.75rem 1rem;
 
-  @media screen and (min-width: ${tabletMin}) {
-    min-height: 5rem;
-    padding: 1.25rem 1.5rem;
-  }
+      @media screen and (min-width: ${tabletMin}) {
+        min-height: 5rem;
+        padding: 1.25rem 1.5rem;
+      }
+    `);
+  }}
 `;
 
 const Brand = styled.a`
@@ -66,7 +79,7 @@ const Project = styled.em`
   padding: 0 1.5rem;
 
   @media screen and (min-width: ${tabletMin}) {
-    border-left: 1px solid rgb(255, 255, 255);
+    border-left: 1px solid ${equisoftTheme.greys.white};
     font-size: 1rem;
     line-height: 2.5rem;
     margin: 0 0 0 1.5rem;
