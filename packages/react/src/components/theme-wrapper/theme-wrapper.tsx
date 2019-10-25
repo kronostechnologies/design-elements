9c33e013d7a0ea4617623dd090c141c68ee50764
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-10-25 13:53:33 -0400

    Theme wrapper now can take different themes

--- packages/react/src/components/theme-wrapper/theme-wrapper.tsx
+++ packages/react/src/components/theme-wrapper/theme-wrapper.tsx
@@ -5,11 +5,20 @@ import { equisoftTheme } from '../../themes/equisoft';
 
 interface ThemeWrapperProps {
     children: ReactElement;
+    theme?: Theme;
 }
 
-export function ThemeWrapper({ children }: ThemeWrapperProps): ReactElement {
+export function ThemeWrapper({ children, theme }: ThemeWrapperProps): ReactElement {
+    let selectedTheme = theme;
+    if (selectedTheme) {
+        if (Object.entries(selectedTheme).length === 0 && selectedTheme.constructor === Object) {
+            selectedTheme = equisoftTheme;
+        }
+    } else {
+        selectedTheme = equisoftTheme;
+    }
     return (
-        <ThemeProvider theme={equisoftTheme}>
+        <ThemeProvider theme={selectedTheme}>
             {children}
         </ThemeProvider>
     );
