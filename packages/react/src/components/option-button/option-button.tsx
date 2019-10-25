20d846ce8721152cb20d3acf78f85f1595ab2f99
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-10-25 08:43:48 -0400

    Adds theme to option-button

--- packages/react/src/components/option-button/option-button.tsx
+++ packages/react/src/components/option-button/option-button.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import uuid from 'uuid/v4';
+import { equisoftTheme } from '../../themes/equisoft';
 
 const Input = styled.input`
-  position: absolute;
-  z-index: -1;
+  ${(props: {theme?: Theme}) => {
+      let theme = props.theme;
+      if (theme) {
+          if (Object.entries(theme).length === 0 && theme.constructor === Object) {
+              theme = equisoftTheme;
+          }
+      } else {
+          theme = equisoftTheme;
+      }
+      return `
+      position: absolute;
+      z-index: -1;
 
-  + label {
-    align-items: center;
-    background-color: #fff;
-    border: 1px solid #d9dde2;
-    border-radius: 8px;
-    color: #637282;
-    display: flex;
-    font-size: 24px;
-    justify-content: center;
-    min-height: 40px;
-  }
+      + label {
+        align-items: center;
+        background-color: ${theme.greys.white};
+        border: 1px solid ${theme.greys.grey};
+        border-radius: 8px;
+        color: ${theme.greys['dark-grey']};
+        display: flex;
+        font-size: 24px;
+        justify-content: center;
+        min-height: 40px;
+      }
 
-  &:checked + label {
-    background-color: #006296;
-    border-color: #006296;
-    color: #fff;
-  }
+      &:checked + label {
+        background-color: ${theme.main['primary-1.1']};
+        border-color: ${theme.main['primary-1.1']};
+        color: ${theme.greys.white};
+      }
+    `;
+  }}
 `;
 
 interface OptionButtonProps {
