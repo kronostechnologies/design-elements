f51a897e8ef0044ef3014c5ca44f6ce15bb75343
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-10-29 09:11:51 -0400

    Adds styles

--- packages/storybook/stories/global-navigation-mobile.stories.tsx
+++ packages/storybook/stories/global-navigation-mobile.stories.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
-import { GlobalNavigationMobile } from '@equisoft/design-elements-react';
+import { ExternalLink, GlobalNavigationMobile, RouteLink } from '@equisoft/design-elements-react';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 
 export default {
     title: 'Global Navigation Mobile',
     component: GlobalNavigationMobile,
+    decorators: [(storyFn: () => ReactElement) => <Router>{storyFn()}</Router>],
 };
 
 export const globalNavigationMobile = () => (
-    <GlobalNavigationMobile />
+    <GlobalNavigationMobile>
+        <ul>
+            <li><RouteLink routerLink={NavLink} label="Profile" href="/test" iconName="mail"/></li>
+            <li><ExternalLink label="Log out" href="/test2"/></li>
+        </ul>
+        <ul>
+            <li><RouteLink routerLink={NavLink} label="Section 1" href="/section1" iconName="home"/></li>
+            <li><RouteLink routerLink={NavLink} label="Section 2" href="/section2" iconName="mail"/></li>
+            <li><RouteLink routerLink={NavLink} label="Section 3" href="/section3" iconName="mapPin"/></li>
+        </ul>
+        <ul>
+            <li><RouteLink routerLink={NavLink} label="Settings" href="/settings"/></li>
+            <li><RouteLink routerLink={NavLink} label="Contact us" href="/contact" iconName="info"/></li>
+            <li>
+                <RouteLink routerLink={NavLink} label="Legal mentions" href="/legal-mentions" iconName="helpCircle"/>
+            </li>
+        </ul>
+    </GlobalNavigationMobile>
 );
