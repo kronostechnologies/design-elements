326a5cee011e32e2406679ae43beac33bde8ff27
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-11-04 15:52:16 -0500

    Adds styles to link

--- packages/react/src/components/link/link.tsx
+++ packages/react/src/components/link/link.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 
-import { Link as DomLink, NavLink } from 'react-router-dom';
+import { BrowserRouter, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 interface LinkProps {
@@ -11,37 +11,33 @@ interface LinkProps {
 }
 
 const Container = styled.div`
-    .external {
-        color: ${(props: {disabled?: boolean}) => props.disabled ? '#7fbfd2 !important' : '#0080a5'}
+  .link {
+    font-size: 0.875rem;
+    margin: 0;
+    ${props => props.disabled ? 'cursor: default' : ''};
+    text-decoration: none;
 
-        &:hover {
-            color: #006c8a;
-        }
+    &:visited {
+      color: #0080a5;
+      font-weight: 600;
     }
+  }
 
-    .link {
-        ${props => props.disabled ? 'cursor: default' : ''};
-        font-size: 0.875rem;
-        text-decoration: none;
-
-        // &:visited {
-        //     color: #0080a5;
-        //     font-weight: 600;
-        // }
+  .external {
+    color: ${(props: {disabled?: boolean}) => props.disabled ? '#7fbfd2 !important' : '#0080a5'}
+    &:hover {
+      color: #006c8a;
     }
+  }
 
-    .navigation {
-        color: ${props => props.disabled ? '#9ca7b4 !important' : '#57666e'};
+  .navigation {
+    color: ${props => props.disabled ? '#9ca7b4 !important' : '#57666e'};
 
-        &:hover {
-            color: #000000;
-        }
+    &:hover {
+      color: #000;
     }
+  }
 `;
-
-// const handleDisabled = (evt: ChangeEvent<HTMLLinkElement>) => {
-//     evt.preventDefault();
-// };
 
 export function Link({ type, href, label, disabled }: LinkProps): ReactElement {
     if (type === 'ext') {
@@ -51,24 +47,26 @@ export function Link({ type, href, label, disabled }: LinkProps): ReactElement {
                     <p className="link external">
                         {label}
                     </p> :
-                    <DomLink to={href} className="link external">
+                    <a href={href} className="link external">
                         {label}
-                    </DomLink>
+                    </a>
                 }
             </Container>
         );
     } else {
         return (
-            <Container disabled={disabled}>
-                {disabled ?
-                    <p className="link navigation">
-                        {label}
-                    </p> :
-                    <NavLink to={href} className="link navigation">
-                        {label}
-                    </NavLink>
-                }
-            </Container>
+            <BrowserRouter>
+                <Container disabled={disabled}>
+                    {disabled ?
+                        <p className="link navigation">
+                            {label}
+                        </p> :
+                        <NavLink to={href} className="link navigation">
+                            {label}
+                        </NavLink>
+                    }
+                </Container>
+            </BrowserRouter>
         );
     }
 }
