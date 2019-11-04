ee72134c735a739e352ce3933da74c7984c3f0ff
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-11-04 15:53:21 -0500

    Adds stories

--- packages/storybook/stories/link.stories.tsx
+++ packages/storybook/stories/link.stories.tsx
@@ -9,7 +9,22 @@ export default {
 
 export const all = () => (
     <div>
-        <Link href="https://github.com/" label="Navigation Link"/>
+        <Link href="/story" label="Navigation Link"/>
+        <Link href="/" label="Navigation Link" disabled/>
+        <Link type="ext" href="https://github.com/" label="External Link"/>
         <Link type="ext" href="https://github.com/" label="External Link" disabled/>
     </div>
+);
+
+export const disabled = () => (
+    <>
+        <Link href="/" label="Navigation Link" disabled/>
+        <Link type="ext" href="https://github.com/" label="External Link" disabled/>
+    </>
+);
+export const navigationLink = () => (
+    <Link href="/story" label="Navigation Link"/>
+);
+export const externalLink = () => (
+    <Link type="ext" href="https://github.com/" label="External Link"/>
 );
