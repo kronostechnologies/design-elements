82c7bf0ee172284de773fd34684a92c70195e05a
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-11-05 11:25:20 -0500

    Adds Icon to links

--- packages/storybook/stories/link.stories.tsx
+++ packages/storybook/stories/link.stories.tsx
@@ -9,18 +9,23 @@ export default {
 
 export const all = () => (
     <div>
-        <Link href="/story" label="Navigation Link"/>
-        <Link href="/" label="Navigation Link" disabled/>
+        <Link href="/story" label="Navigation Link" iconName="mail"/>
+        <Link href="/" label="Navigation Link"/>
+        <Link type="ext" href="https://github.com/" label="External Link"  iconName="mail"/>
         <Link type="ext" href="https://github.com/" label="External Link"/>
-        <Link type="ext" href="https://github.com/" label="External Link" disabled/>
     </div>
 );
 
 export const disabled = () => (
     <>
+        <Link href="/" label="Navigation Link" iconName="mail" disabled/>
         <Link href="/" label="Navigation Link" disabled/>
+        <Link type="ext" href="https://github.com/" label="External Link" iconName="mail" disabled/>
         <Link type="ext" href="https://github.com/" label="External Link" disabled/>
     </>
+);
+export const withIcon = () => (
+    <Link href="/story" label="Navigation Link" iconName="mail"/>
 );
 export const navigationLink = () => (
     <Link href="/story" label="Navigation Link"/>
