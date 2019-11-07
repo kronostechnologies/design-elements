dec4f93a5ef47cb73c9a8f3befb583c4949b4b45
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-11-07 10:45:35 -0500

    Navlink debug process

--- packages/storybook/stories/link.stories.tsx
+++ packages/storybook/stories/link.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Link } from '@equisoft/design-elements-react';
+import { BrowserRouter } from 'react-router-dom';
 
 export default {
     title: 'Link',
@@ -8,28 +9,28 @@ export default {
 };
 
 export const all = () => (
-    <>
+    <BrowserRouter>
         <Link href="/story" label="Navigation Link" iconName="mail"/><br/>
-        <Link href="/story" label="Navigation Link"/><br/>
+        {/* <Link href="/story" label="Navigation Link"/><br/>
         <Link type="ext" href="#" label="External Link"  iconName="mail"/><br/>
-        <Link type="ext" href="#" label="External Link"/><br/>
-    </>
+        <Link type="ext" href="#" label="External Link"/><br/> */}
+    </BrowserRouter>
 );
 
-export const disabled = () => (
-    <>
-        <Link href="/story" label="Navigation Link" iconName="mail" disabled/><br/>
-        <Link href="/story" label="Navigation Link" disabled/><br/>
-        <Link type="ext" href="#" label="External Link" iconName="mail" disabled/><br/>
-        <Link type="ext" href="#" label="External Link" disabled/><br/>
-    </>
-);
-export const withIcon = () => (
-    <Link href="/story" label="Navigation Link" iconName="mail"/>
-);
-export const navigationLink = () => (
-    <Link href="/story" label="Navigation Link"/>
-);
-export const externalLink = () => (
-    <Link type="ext" href="#" label="External Link"/>
-);
+// export const disabled = () => (
+//     <>
+//         <Link href="/story" label="Navigation Link" iconName="mail" disabled/><br/>
+//         <Link href="/story" label="Navigation Link" disabled/><br/>
+//         <Link type="ext" href="#" label="External Link" iconName="mail" disabled/><br/>
+//         <Link type="ext" href="#" label="External Link" disabled/><br/>
+//     </>
+// );
+// export const withIcon = () => (
+//     <Link href="/story" label="Navigation Link" iconName="mail"/>
+// );
+// export const navigationLink = () => (
+//     <Link href="/story" label="Navigation Link"/>
+// );
+// export const externalLink = () => (
+//     <Link type="ext" href="#" label="External Link"/>
+// );
