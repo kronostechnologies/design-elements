b0414d9c12359a83c9c09a0ff4ae05e47db27e27
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-11-08 14:34:37 -0500

    Updates link stories

--- packages/storybook/stories/link.stories.tsx
+++ packages/storybook/stories/link.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import { Link } from '@equisoft/design-elements-react';
 import { BrowserRouter as Router, NavLink } from 'react-router-dom';
@@ -6,28 +6,25 @@ import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 export default {
     title: 'Link',
     component: Link,
+    decorators: [(storyFn: () => ReactElement) => <Router>{storyFn()}</Router>],
 };
 
 export const all = () => (
-    <Router>
+    <>
         <Link routerLink={NavLink} href="/story1" label="Navigation Link" iconName="mail"/><br/>
         <Link routerLink={NavLink} href="/story2" label="Navigation Link"/><br/>
-    </Router>
+    </>
 );
 
 export const disabled = () => (
-    <Router>
+    <>
         <Link routerLink={NavLink} href="/story" label="Navigation Link" iconName="mail" disabled/><br/>
         <Link routerLink={NavLink} href="/story" label="Navigation Link" disabled/><br/>
-    </Router>
+    </>
 );
 export const withIcon = () => (
-    <Router>
-        <Link routerLink={NavLink} href="/story" label="Navigation Link" iconName="mail"/>
-    </Router>
+    <Link routerLink={NavLink} href="/story" label="Navigation Link" iconName="mail"/>
 );
 export const onlyIcon = () => (
-    <Router>
-        <Link routerLink={NavLink} href="/story" iconName="mail"/>
-    </Router>
+    <Link routerLink={NavLink} href="/story" iconName="mail"/>
 );
