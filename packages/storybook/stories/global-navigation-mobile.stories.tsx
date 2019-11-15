38cdc7ec5e183d6b4be0c3048fec87d0c200b474
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-11-15 10:17:47 -0500

    Adds stories

--- packages/storybook/stories/global-navigation-mobile.stories.tsx
+++ packages/storybook/stories/global-navigation-mobile.stories.tsx
@@ -1,9 +1,8 @@
 import React, { ReactElement } from 'react';
 
-import { ExternalLink, GlobalNavigationMobile, RouteLink } from '@equisoft/design-elements-react';
-import { BrowserRouter as Router, NavLink } from 'react-router-dom';
-
+import { Button, ExternalLink, GlobalNavigationMobile, RouteLink } from '@equisoft/design-elements-react';
 import { forceReRender } from '@storybook/react';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 
 export default {
     title: 'Global Navigation Mobile',
@@ -11,25 +10,25 @@ export default {
     decorators: [(storyFn: () => ReactElement) => <Router>{storyFn()}</Router>],
 };
 
-let MainMenuOpen = false;
-let SideMenuOpen = false;
+type menuType = 'main' | 'second' | 'nested';
 
-const HandleMainMenuClick = () => {
-    MainMenuOpen = !MainMenuOpen;
+const allMenus = {
+    main: false,
+    second: false,
+    nested: false,
+};
+
+const HandleMenuClick = (menu: menuType) => {
+    allMenus[menu] = !allMenus[menu];
     forceReRender();
 };
 
-const HandleSideMenuClick = () => {
-    SideMenuOpen = !SideMenuOpen;
-    forceReRender();
-};
-
-export const globalNavigationMobile = () => (
+export const normal = () => (
     <>
-        <GlobalNavigationMobile open={MainMenuOpen}>
+        <GlobalNavigationMobile open={allMenus.main}>
             <ul>
-                <li><RouteLink routerLink={NavLink} label="Profile" href="/test" iconName="mail"/></li>
-                <li><ExternalLink label="Log out" href="/test2"/></li>
+                <li><RouteLink routerLink={NavLink} label="Profile" href="/profile" iconName="mail"/></li>
+                <li><ExternalLink label="Log out" href="/logout"/></li>
             </ul>
             <ul>
                 <li><RouteLink routerLink={NavLink} label="Section 1" href="/section1" iconName="home"/></li>
@@ -39,25 +38,29 @@ export const globalNavigationMobile = () => (
             <ul>
                 <li><RouteLink routerLink={NavLink} label="Settings" href="/settings"/></li>
                 <li><RouteLink routerLink={NavLink} label="Contact us" href="/contact" iconName="info"/></li>
-                <li>
-                    <RouteLink
-                        routerLink={NavLink}
-                        label="Legal mentions"
-                        href="/legal-mentions"
-                        iconName="helpCircle"
-                    />
-                </li>
+                <li><RouteLink routerLink={NavLink} label="Legal mentions" href="/legal" iconName="helpCircle"/></li>
             </ul>
-            <GlobalNavigationMobile open={SideMenuOpen}>
-                <ul>
-                    <li><RouteLink routerLink={NavLink} label="Settings" href="/settings"/></li>
-                    <li><RouteLink routerLink={NavLink} label="Contact us" href="/contact" iconName="info"/></li>
-                </ul>
-                <button onClick={HandleSideMenuClick}>Close current menu</button>
-            </GlobalNavigationMobile>
-            <button onClick={HandleMainMenuClick}>Close current menu</button><br/>
-            <button onClick={HandleSideMenuClick}>Toggle menu</button>
+            <Button label="Close menu" buttonType="primary" onClick={() => HandleMenuClick('main')}/>
         </GlobalNavigationMobile>
-        <button onClick={HandleMainMenuClick}>Toggle menu</button>
+        <Button label="Click to open menu" buttonType="primary" onClick={() => HandleMenuClick('main')}/>
+    </>
+);
+
+export const withNestedMenu = () => (
+    <>
+        <GlobalNavigationMobile open={allMenus.second}>
+            <ul>
+                <li><RouteLink routerLink={NavLink} label="Section 1" href="/section1" iconName="home"/></li>
+                <li><RouteLink routerLink={NavLink} label="Section 2" href="/section2" iconName="mail"/></li>
+                <li><RouteLink routerLink={NavLink} label="Section 3" href="/section3" iconName="mapPin"/></li>
+            </ul>
+            <GlobalNavigationMobile open={allMenus.nested}>
+                <p>Nested Menu</p>
+                <Button label="Close menu" buttonType="primary" onClick={() => HandleMenuClick('nested')}/>
+            </GlobalNavigationMobile>
+            <Button label="Open nested menu" buttonType="primary" onClick={() => HandleMenuClick('nested')}/><br/>
+            <Button label="Close menu" buttonType="primary" onClick={() => HandleMenuClick('second')}/>
+        </GlobalNavigationMobile>
+        <Button label="Click to open menu" buttonType="primary" onClick={() => HandleMenuClick('second')}/>
     </>
 );
