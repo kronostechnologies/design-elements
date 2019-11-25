e26b42b8f845cc3a015a6dae146142dcbe3fae53
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-11-25 10:02:10 -0500

    Adds mobile-menu styles

--- packages/react/src/components/mobile-menu/mobile-menu.tsx
+++ packages/react/src/components/mobile-menu/mobile-menu.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
+import styled from 'styled-components';
 
-const MobileMenu = () => {
+const StyledUl = styled.ul`
+  font-size: 1.125rem;
+  list-style: none;
+  margin: 0;
+  padding: 12px 24px;
+`;
+
+interface MobileMenuProps {
+    children: ReactNode;
+}
+
+export const MobileMenu = ({ children }: MobileMenuProps): ReactElement => {
     return (
-        <div>
-            MobileMenu
-        </div>
+        <StyledUl>
+            {children}
+        </StyledUl>
     );
 };
-
-export { MobileMenu };
--- packages/storybook/stories/mobile-menu.stories.tsx
+++ packages/storybook/stories/mobile-menu.stories.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 
-import { MobileMenu } from '@equisoft/design-elements-react';
+import { ExternalLink, MobileMenu, MobileMenuItem, RouteLink, SideDrawer } from '@equisoft/design-elements-react';
 
 export default {
     title: 'Mobile Menu',
     component: MobileMenu,
+    decorators: [(storyFn: () => ReactElement) => <Router>{storyFn()}</Router>],
 };
 
 export const normal = () => (
-    <MobileMenu/>
+    <SideDrawer open>
+        <MobileMenu>
+            <MobileMenuItem>
+                <h2>Mobile Menu</h2>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <ExternalLink label="External Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <RouteLink routerLink={NavLink} href="/story1" label="Navigation Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <RouteLink routerLink={NavLink} href="/story2" label="Navigation Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <RouteLink routerLink={NavLink} href="/story3" label="Navigation Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <RouteLink routerLink={NavLink} href="/story4" label="Navigation Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <p>Test paragraph</p>
+            </MobileMenuItem>
+        </MobileMenu>
+        <MobileMenu>
+            <MobileMenuItem>
+                <ExternalLink label="External Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <RouteLink routerLink={NavLink} href="/story1" label="Navigation Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <RouteLink routerLink={NavLink} href="/story2" label="Navigation Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <RouteLink routerLink={NavLink} href="/story3" label="Navigation Link" iconName="mail"/>
+            </MobileMenuItem>
+            <MobileMenuItem>
+                <RouteLink routerLink={NavLink} href="/story4" label="Navigation Link" iconName="mail"/>
+            </MobileMenuItem>
+        </MobileMenu>
+    </SideDrawer>
 );
