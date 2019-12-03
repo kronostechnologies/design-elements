5d59ad8d42f166b99e7a7928197a5b003d3eaf2f
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-12-03 13:14:13 -0500

    Adds validation

--- packages/storybook/stories/dropdown.stories.tsx
+++ packages/storybook/stories/dropdown.stories.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
 
 import { Dropdown } from '@equisoft/design-elements-react';
+
+const Container = styled.div`
+    height: 200px;
+`;
+
+const options = [
+    {
+        label: 'Option A',
+        value: 'optionA',
+    },
+    {
+        label: 'Option B',
+        value: 'optionB',
+    },
+    {
+        label: 'Option C',
+        value: 'optionC',
+    },
+    {
+        label: 'Option D',
+        value: 'optionD',
+    },
+];
 
 export default {
     title: 'Dropdown',
     component: Dropdown,
+    decorators: [(storyFn: () => ReactElement) => <Container>{storyFn()}</Container>],
 };
 
 export const normal = () => (
-    <Dropdown/>
+    <Dropdown options={options}/>
 );
 
 export const disabled = () => (
-    <Dropdown disabled/>
+    <Dropdown options={options} disabled/>
+);
+export const invalid = () => (
+    <Dropdown options={options} valid={false}/>
+);
+export const withCallback = () => (
+    <Dropdown options={options} onChange={(option) => console.log(`Label: ${option.label} | Value: ${option.value}`)}/>
 );
