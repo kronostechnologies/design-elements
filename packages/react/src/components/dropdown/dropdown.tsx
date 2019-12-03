6bf5b0196334f0a26811394dc35dc4974b13def4
mgendron-equisoft <maxime.gendron@equisoft.com>
2019-12-03 13:14:13 -0500

    Adds validation

--- packages/react/src/components/dropdown/dropdown.tsx
+++ packages/react/src/components/dropdown/dropdown.tsx
@@ -1,25 +1,38 @@
 import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
+import uuid from 'uuid';
 
+import { FieldContainer } from '../field-container/field-container';
 import { Icon } from '../icon//icon';
 import { List } from '../list/list';
 import { Theme } from '../theme-wrapper/theme-wrapper';
+
+interface Option {
+    label: string;
+    value: string;
+}
 
 interface InputWrapperProps {
     disabled?: boolean;
     focus?: boolean;
     theme: Theme;
+    valid: boolean;
 }
+
+const StyledFieldContainer = styled(FieldContainer)`
+  position: relative;
+`;
 
 const InputWrapper = styled.div<InputWrapperProps>`
   align-items: center;
   background-color: ${props => props.disabled ? props.theme.greys['light-grey'] : props.theme.greys.white};
-  border: 1px solid ${props => props.focus ? props.theme.main['primary-1.1'] : props.theme.greys.grey};
+  border: 1px solid ${props => props.valid ? props.focus ? props.theme.main['primary-1.1'] : props.theme.greys.grey : props.theme.notifications['error-2.1']};
   border-radius: 0.25rem;
   box-sizing: border-box;
   display: flex;
   height: 32px;
   justify-content: space-between;
+  margin-top: 8px;
   padding: 0.5rem;
   width: 100%;
 
@@ -46,6 +59,8 @@ const StyledInput = styled.input`
 
 const ListWrapper = styled.div`
   display: ${(props: {open?: boolean}) => props.open ? 'flex' : 'none'};
+  position: absolute;
+  width: 100%;
 
   ul {
     border-radius: 0.25rem;
@@ -54,34 +69,28 @@ const ListWrapper = styled.div`
   }
 `;
 
-const options = [
-    {
-        label: 'Option A',
-        value: 'optionA',
-    },
-    {
-        label: 'Option B',
-        value: 'optionB',
-    },
-    {
-        label: 'Option C',
-        value: 'optionC',
-    },
-    {
-        label: 'Option D',
-        value: 'optionD',
-    },
-];
-
 interface DropdownProps {
     disabled?: boolean;
+    label?: string;
+    options: Option[];
+    valid?: boolean;
+    validationErrorMessage?: string;
+    onChange?(option: Option): void;
 }
 
-export const Dropdown = ({ disabled }: DropdownProps) => {
+export const Dropdown = ({
+    disabled,
+    label = 'Select an option',
+    onChange,
+    options,
+    valid = true,
+    validationErrorMessage = 'You must select an option',
+}: DropdownProps) => {
     const [focus, setFocus] = useState(false);
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState('');
     const inputRef = useRef<HTMLInputElement>(null);
+    const id = uuid();
 
     const handleClick = () => {
         setOpen(!open);
@@ -90,31 +99,45 @@ export const Dropdown = ({ disabled }: DropdownProps) => {
         } else setFocus(false);
     };
 
-    const handleChange = (option: any) => {
+    const handleChange = (option: Option): void => {
         setValue(option.label);
         setOpen(!open);
         setFocus(!focus);
+        if (onChange) onChange(option);
     };
 
     return (
         <>
-            <InputWrapper onClick={disabled ? undefined : handleClick} focus={focus} disabled={disabled}>
-                <StyledInput
+            <StyledFieldContainer
+                fieldId={id}
+                label={label}
+                valid={valid}
+                validationErrorMessage={validationErrorMessage}
+            >
+                <InputWrapper
+                    onClick={disabled ? undefined : handleClick}
+                    focus={focus}
                     disabled={disabled}
-                    ref={inputRef}
-                    type="text"
-                    value={value}
-                    placeholder="Select an option"
-                    readOnly
-                />
-                <Icon name={open ? 'chevronUp' : 'chevronDown'} />
-            </InputWrapper>
-            <ListWrapper open={open}>
-                <List
-                    options={options}
-                    onChange={handleChange}
-                />
-            </ListWrapper>
+                    valid={valid}
+                >
+                    <StyledInput
+                        disabled={disabled}
+                        ref={inputRef}
+                        type="text"
+                        value={value}
+                        placeholder="Select an option"
+                        readOnly
+                    />
+                    <Icon name={open ? 'chevronUp' : 'chevronDown'}/>
+                </InputWrapper>
+                <ListWrapper open={open}>
+                    <List
+                        checkIndicator
+                        options={options}
+                        onChange={handleChange}
+                    />
+                </ListWrapper>
+            </StyledFieldContainer>
         </>
     );
 };
