c370a595b04737906731b7a8a7d551c8e60995af
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2020-01-22 23:24:42 -0500

    feat(CarouselComponent) : Ajout d'un component de carousel

---
+++ packages/react/src/components/carousel/carousel.tsx
@@ -0,0 +1,218 @@
+import { Icon } from '@design-elements/components/icon/icon';
+import { useId } from '@design-elements/hooks/use-id';
+import { useTheme } from '@design-elements/hooks/use-theme';
+import React, { AriaAttributes, ReactElement, ReactNode, ReactNodeArray } from 'react';
+import styled, { DefaultTheme, StyledComponent } from 'styled-components';
+import { useCarousel } from './use-carousel';
+
+const Main = styled.section`
+    align-items: center;
+    display: flex;
+    word-break: break-word;
+`;
+
+const Content = styled.div`
+    height: 100%;
+    margin: 0;
+    overflow: hidden;
+    padding: 0;
+    width: 100%;
+`;
+
+interface SlideProps {
+    index: number;
+}
+
+const Slide: StyledComponent<'div', DefaultTheme, SlideProps> = styled.div.attrs<SlideProps>({
+    role: 'group',
+    'aria-roledescription': 'slide',
+})`
+    flex-grow: 1;
+    height: 100%;
+    width: 100%;
+`;
+
+const Slides = styled.div.attrs({ 'aria-live': 'polite' })`
+    align-items: center;
+    display: flex;
+`;
+
+const Dots = styled.div`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`;
+
+interface DotProps {
+    active?: boolean;
+}
+
+const Dot = styled.button<DotProps>`
+    background-color: ${({ active, theme }) => active ? theme.main['primary-1.1'] : theme.greys.grey};
+    border-radius: 50%;
+    box-sizing: border-box;
+    cursor: pointer;
+    display: inline-block;
+    height: 12px;
+    width: 12px;
+
+    & + & {
+        margin-left: var(--spacing-1x);
+    }
+`;
+
+interface NavigationButtonProps {
+    disabled?: boolean;
+}
+
+const NavigationButton = styled.button.attrs<NavigationButtonProps>({ type: 'button' })`
+    cursor: pointer;
+    height: 32px;
+    visibility: ${({ disabled }) => disabled ? 'hidden' : 'visible'};
+    width: 32px;
+`;
+
+function LeftArrowIcon(): ReactElement {
+    const theme = useTheme();
+    return <Icon name="arrowLeft" size="100%" color={theme.greys['dark-grey']} />;
+}
+
+function RightArrowIcon(): ReactElement {
+    const theme = useTheme();
+    return <Icon name="arrowRight" size="100%" color={theme.greys['dark-grey']} />;
+}
+
+export interface CarouselProps extends Pick<AriaAttributes, 'aria-label'> {
+    autoTransitionDelay?: number;
+    children: ReactNode;
+    header: ReactNode;
+    initialSlide: number;
+    loop: boolean;
+    transitionTime: number;
+    withArrows: boolean;
+}
+
+function getChildrenAsArray(children: ReactNode): ReactNodeArray {
+    if (!children) {
+        return [];
+    }
+    if (Array.isArray(children)) {
+        return children;
+    }
+    return [children];
+}
+
+export function Carousel(
+    { autoTransitionDelay, initialSlide, children, header, loop, transitionTime, withArrows, ...props }: CarouselProps,
+): ReactElement {
+    const carouselId = useId('carousel-slides-');
+
+    const childrenAsArray = getChildrenAsArray(children);
+    const slidesCount = childrenAsArray.length;
+    const { active, setActive, handlers, style } = useCarousel({
+        autoTransitionDelay,
+        initial: initialSlide,
+        length: slidesCount,
+        loop,
+        transitionTime,
+    });
+
+    function renderDots(): ReactElement {
+        return (
+            <Dots data-testid="carousel-dots">{childrenAsArray.map((_, i) => {
+                const isActive = i === active;
+
+                return (
+                    <Dot
+                        data-testid={`carousel-dot-${i}`}
+                        key={i}
+                        active={isActive}
+                        aria-controls={carouselId}
+                        aria-disabled={isActive}
+                        onClick={() => setActive(i)}
+                    />
+                );
+            })}
+            </Dots>
+        );
+    }
+
+    function renderSlides(): ReactElement {
+        return (
+            <Slides id={carouselId} data-testid="carousel-slides" {...handlers.mouse} style={style} aria-live="polite">
+                <Slide
+                    index={-1}
+                    data-testid="carousel-slide--1"
+                    aria-hidden={true}
+                >
+                    {childrenAsArray[slidesCount - 1]}
+                </Slide>
+
+                {childrenAsArray.map((child, i) => (
+                    <Slide
+                        index={i}
+                        data-testid={`carousel-slide-${i}`}
+                        aria-hidden={i !== active}
+                        key={i}
+                        aria-label={`${i + 1} of ${slidesCount}`}
+                    >
+                        {child}
+                    </Slide>
+                ))}
+
+                <Slide index={slidesCount} data-testid={`carousel-slide-${slidesCount}`} aria-hidden={true}>
+                    {childrenAsArray[0]}
+                </Slide>
+            </Slides>
+        );
+    }
+
+    const disablePrevious = !loop && active === 0;
+    const disableNext = !loop && active === slidesCount - 1;
+    return (
+        <Main aria-label={props['aria-label']} aria-roledescription="carousel">
+            {withArrows && (
+                <NavigationButton
+                    onClick={!disablePrevious ? handlers.onPrevious : undefined}
+                    disabled={disablePrevious}
+                    data-testid="carousel-previous"
+                    aria-label="previous"
+                    aria-controls={carouselId}
+                >
+                    <LeftArrowIcon />
+                </NavigationButton>
+            )}
+
+            <Content>
+                {header}
+
+                {renderSlides()}
+
+                {renderDots()}
+            </Content>
+
+            {withArrows && (
+                <NavigationButton
+                    onClick={!disableNext ? handlers.onNext : undefined}
+                    disabled={disableNext}
+                    data-testid="carousel-next"
+                    aria-label="next"
+                    aria-controls={carouselId}
+                >
+                    <RightArrowIcon />
+                </NavigationButton>
+            )}
+        </Main>
+    );
+}
+
+Carousel.defaultProps = {
+    initialSlide: 0,
+    header: null,
+    loop: false,
+    transitionTime: 400,
+    withArrows: true,
+};
---
+++ packages/react/src/hooks/use-id.test.tsx
@@ -0,0 +1,56 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { mount } from 'enzyme';
+import React, { ReactElement, useEffect } from 'react';
+import { getByTestId } from '../test-utils/enzyme-selectors';
+import { resetId, useId } from './use-id';
+
+describe('useId', () => {
+    beforeEach(() => {
+        resetId();
+    });
+
+    it('should use prefix', async () => {
+        const { result } = renderHook(useId, { initialProps: 'some-prefix-' });
+
+        expect(result.current).toEqual('some-prefix-1');
+    });
+
+    it('should increment when used multiple times', () => {
+        const wrapper = mount(<DummyContainer />);
+
+        const dummy1 = getByTestId(wrapper, 'dummy-1');
+        const dummy2 = getByTestId(wrapper, 'dummy-2');
+
+        expect(dummy1.text()).toEqual('id-1');
+        expect(dummy2.text()).toEqual('id-2');
+    });
+
+    it('should keep same id when re-rendering', () => {
+        const onRendered = jest.fn();
+        const wrapper = mount(<DummyComponent onRendered={onRendered} />);
+
+        act(() => {
+            wrapper.setProps({});
+        });
+
+        expect(wrapper.text()).toEqual('id-1');
+        expect(onRendered).toHaveBeenCalledTimes(2);
+    });
+});
+
+function DummyContainer(): ReactElement {
+    return (
+        <>
+            <DummyComponent data-testid="dummy-1" />
+            <DummyComponent data-testid="dummy-2" />
+        </>
+    );
+}
+
+function DummyComponent({ onRendered = () => undefined }: { onRendered?(): void }): ReactElement {
+    const id = useId('id-');
+
+    useEffect(onRendered);
+
+    return <>{id}</>;
+}
