641a723126206ab37098309bce51b6c36b59175a
mgendron-equisoft <maxime.gendron@equisoft.com>
2020-02-20 12:58:54 -0500

    Update tests

--- packages/react/src/components/headband/headband.test.tsx
+++ packages/react/src/components/headband/headband.test.tsx
@@ -1,11 +1,34 @@
-import { renderWithProviders } from '@design-elements/test-utils/renderer';
-import React from 'react';
+import React, { ReactElement } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { ThemeWrapped } from '../../test-utils/theme-wrapped';
 import { Headband } from './equisoft-default';
+
+const setup = (children: ReactElement) => (
+    renderer.create(
+        ThemeWrapped(
+            <Router>
+                {children}
+            </Router>,
+        ),
+    ).toJSON()
+);
 
 describe('Headband', () => {
     test('Matches the snapshot', () => {
-        const tree = renderWithProviders(
-            <Headband>
+        const tree = setup(
+            <Headband mobileDrawerContent={(<p>Test</p>)} routerLink={Link}>
+                Hello, World!
+            </Headband>,
+        );
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('Has burger button and side drawer', () => {
+        Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 500 });
+        const tree = setup(
+            <Headband mobileDrawerContent={(<p>Test</p>)} routerLink={Link}>
                 Hello, World!
             </Headband>,
         );
