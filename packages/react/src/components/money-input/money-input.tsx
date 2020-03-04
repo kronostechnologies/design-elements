a40bf798d606fb4c824b076a317f64a8a28c4174
mgendron-equisoft <maxime.gendron@equisoft.com>
2020-03-04 14:44:20 -0500

    Add base component

---
+++ packages/react/src/components/money-input/money-input.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+export function MoneyInput(): ReactElement {
+    return (
+        <div>
+            MoneyInput
+        </div>
+    );
+}
---
+++ packages/storybook/stories/money-input.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { MoneyInput } from '@equisoft/design-elements-react';
+
+export default {
+    title: 'Money Input',
+    component: MoneyInput,
+};
+
+export const normal = () => (
+    <MoneyInput/>
+);
