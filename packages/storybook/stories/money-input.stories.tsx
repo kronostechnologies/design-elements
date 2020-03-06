65c335acf0e1caf09aefb9412984a96e9a561c9e
mgendron-equisoft <maxime.gendron@equisoft.com>
2020-03-06 12:42:52 -0500

    Add stories

--- packages/storybook/stories/money-input.stories.tsx
+++ packages/storybook/stories/money-input.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { MoneyInput } from '@equisoft/design-elements-react';
+import { Button, MoneyInput } from '@equisoft/design-elements-react';
 
 export default {
     title: 'Money Input',
@@ -8,5 +8,44 @@ export default {
 };
 
 export const normal = () => (
-    <MoneyInput label="test" language="fr"/>
+    <>
+        <MoneyInput label="Entrez un montant"/>
+        <MoneyInput label="Chose a number" language="en"/>
+    </>
+);
+
+export const english = () => (
+    <MoneyInput label="Chose a number" language="en"/>
+);
+export const noLabel = () => (
+    <MoneyInput/>
+);
+export const disabled = () => (
+    <MoneyInput disabled label="Entrez un montant"/>
+);
+export const withPrecision = () => (
+    <MoneyInput label="Entrez un montant" precision={0}/>
+);
+export const withValue = () => (
+    <MoneyInput label="Entrez un montant" value={350}/>
+);
+export const required = () => (
+    <form onSubmit={(event) => event.preventDefault()}>
+        <MoneyInput required label="Entrez un montant"/>
+        <Button buttonType="primary" type="submit">Soumettre</Button>
+    </form>
+);
+export const customErrorMessage = () => (
+    <form onSubmit={(event) => event.preventDefault()}>
+        <MoneyInput required label="Entrez un montant" validationErrorMessage="Custom error message."/>
+    </form>
+);
+export const onChangeCallback = () => (
+    <MoneyInput
+        label="Entrez un montant"
+        onChange={(value, formattedValue) => {
+            console.log('value:', value);
+            console.log('formattedValue: ', formattedValue);
+        }}
+    />
 );
