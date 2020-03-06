72659266e04e40446de81877ac1d1688d112305a
pgagne <pierre.gagne@equisoft.com>
2020-03-06 09:19:02 -0500

    button hitbox & placement

--- packages/react/src/components/banner/banner.tsx
+++ packages/react/src/components/banner/banner.tsx
@@ -14,19 +14,20 @@ const Container = styled.div<{messageType: MessageType, device: DeviceType}>`
     }
 
     color: ${props => props.messageType === 'error' ? props.theme.greys.white : props.theme.greys.black };
-    display: flex;
     font-size: ${props => props.device === 'desktop' ? 0.75 : 1}rem;
     font-weight: ${props => props.device === 'desktop' ? 600 : 'var(--font-normal)' };
     letter-spacing: ${props => props.device === 'desktop' ? 0.2 : 0.46}px;
     line-height: 1.25rem;
     padding: ${props => props.device === 'desktop' ? 'var(--spacing-2x)' : 'var(--spacing-3x) var(--spacing-2x)'};
-    width: 100%;
+    padding-right: 3em;
+    position: relative;
 `;
 
 const Content = styled.div<{device: DeviceType}>`
     display: flex;
     flex-grow: 1;
     justify-content: center;
+    position: relative;
 
     svg {
         flex-shrink: 0;
@@ -39,14 +40,17 @@ const Content = styled.div<{device: DeviceType}>`
     }
 `;
 
-const CloseButton = styled.button`
+const CloseButton = styled.button<{device: DeviceType}>`
     appearance: none;
     background: transparent;
     border: 0;
     cursor: pointer;
-    height: 24px;
-    margin-left: var(--spacing-2x);
+    height: ${props => props.device === 'desktop' ? 32 : 48}px;
     padding: 0;
+    position: absolute;
+    right: calc(${props => props.device === 'desktop' ? 'var(--spacing-2x) - 6px' : 'var(--spacing-1x) - 10px'});
+    top: calc(var(--spacing-2x) - ${props => props.device === 'desktop' ? 6 : 10}px);
+    width: ${props => props.device === 'desktop' ? 32 : 48}px;
 `;
 
 const GetIconName = (messageType: MessageType): IconName => {
@@ -79,13 +83,13 @@ export const Banner = ({ children, type, device, hidden }: BannerProps) => {
 
     return visible ? (
         <Container data-testid="container" role="alert" messageType={type} device={concreteDevice}>
+            <CloseButton data-testid="closeButton" onClick={() => setVisible(false)} device={concreteDevice} aria-label="Close">
+                <Icon name="x" size={device === 'mobile' ? '28' : '20'}/>
+            </CloseButton>
             <Content device={concreteDevice}>
-                <Icon name={GetIconName(type)} aria-hidden="true"/>
+                <Icon name={GetIconName(type)} aria-hidden="true" size={device === 'mobile' ? '24' : '20'}/>
                 <p>{children}</p>
             </Content>
-            <CloseButton data-testid="closeButton" onClick={() => setVisible(false)} aria-label="Close">
-                <Icon name="x" size={device === 'mobile' ? '20' : '16'}/>
-            </CloseButton>
         </Container>
     ) : null;
 };
