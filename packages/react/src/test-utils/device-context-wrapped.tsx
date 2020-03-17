e117946cb4724f6e8476d79c3df1ece5c5fd85e8
Maxime Gendron <maxime.gendron07@gmail.com>
2020-03-17 14:57:59 -0400

    Add DeviceContext test util

---
+++ packages/react/src/test-utils/device-context-wrapped.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import { DeviceContextProvider, DeviceType } from '../components/device-context-provider/device-context-provider';
+
+export function DeviceContextWrapped(children: ReactElement, device: DeviceType): ReactElement {
+    return (
+        <DeviceContextProvider staticDevice={device}>
+            {children}
+        </DeviceContextProvider>
+    );
+}
