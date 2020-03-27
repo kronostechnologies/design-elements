ec24fb43ca32c1a46f1442d91d5d2bb277b2cd45
Maxime Gendron <maxime.gendron07@gmail.com>
2020-03-27 08:43:57 -0400

    Update device-context-wrapped test-util

--- packages/react/src/test-utils/device-context-wrapped.tsx
+++ packages/react/src/test-utils/device-context-wrapped.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 import { DeviceContextProvider, DeviceType } from '../components/device-context-provider/device-context-provider';
 
-export function DeviceContextWrapped(children: ReactElement, device: DeviceType): ReactElement {
+export function DeviceContextWrapped(children: ReactElement, device?: DeviceType): ReactElement {
     return (
         <DeviceContextProvider staticDevice={device}>
             {children}
