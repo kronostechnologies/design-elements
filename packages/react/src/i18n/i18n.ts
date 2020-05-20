00902bd2871637b56fcad2c14601af90c46762fc
Maxime Gendron <maxime.gendron07@gmail.com>
2020-05-20 12:47:58 -0400

    Add translation to text-area

--- packages/react/src/i18n/i18n.ts
+++ packages/react/src/i18n/i18n.ts
@@ -4,27 +4,7 @@ import { initReactI18next } from 'react-i18next';
 void i18n
     .use(initReactI18next)
     .init({
-        resources: {
-            en: {
-                translation: {
-                    'Welcome to React': 'Welcome to React and Jean-Bag',
-                },
-                'inline-message': {
-                    Tips: 'Tips',
-                    Success: 'Success',
-                    Alert: 'Alert',
-                    Error: 'Error',
-                },
-            },
-            fr: {
-                'inline-message': {
-                    Tips: 'Astuce',
-                    Success: 'Succès',
-                    Alert: 'Alerte',
-                    Error: 'Erreur',
-                },
-            },
-        },
+        resources: {},
         lng: 'en',
         fallbackLng: 'en',
 
