ed8b77e79d4d5b967a81e105d1ae7ebbc102e82c
Maxime Gendron <maxime.gendron07@gmail.com>
2020-06-11 09:11:07 -0400

    Add stories to Table component

---
+++ packages/storybook/stories/table.stories.tsx
@@ -0,0 +1,116 @@
+import { ColumnsProps, Table, Tooltip } from '@equisoft/design-elements-react';
+import React from 'react';
+
+export default {
+    title: 'Table',
+    component: Table,
+};
+
+export const normal = () => {
+    const columns: ColumnsProps = [
+        {
+            Header: 'Column 1',
+            accessor: 'column1',
+        },
+        {
+            Header: 'Column 2',
+            accessor: 'column2',
+        },
+        {
+            Header: 'Column 3',
+            accessor: 'column3',
+        },
+    ];
+
+    const data = [
+        {
+            column1: 'Hello',
+            column2: 'World',
+            column3: 'Hello',
+        },
+        {
+            column1: 'Hello',
+            column2: 'World',
+            column3: 'Hello',
+        },
+    ];
+    return (
+        <Table columns={columns} data={data} />
+    );
+};
+
+export const customTextAlignment = () => {
+    const column: ColumnsProps = [
+        {
+            Header: 'Column 1',
+            accessor: 'column1',
+        },
+        {
+            Header: () => <div style={{ textAlign: 'center' }}>Column 2</div>,
+            Cell: ({ value }) => <div style={{ textAlign: 'center' }}>{value}</div>,
+            accessor: 'column2',
+        },
+        {
+            Header: () => <div style={{ textAlign: 'right' }}>Column 3</div>,
+            Cell: ({ value }) => <div style={{ textAlign: 'right' }}>{value}</div>,
+            accessor: 'column3',
+        },
+    ];
+
+    const data = [
+        {
+            column1: 'Hello',
+            column2: 'World',
+            column3: 'Hello',
+        },
+        {
+            column1: 'Hello',
+            column2: 'World',
+            column3: 'Hello',
+        },
+    ];
+
+    return (
+        <Table columns={column} data={data} />
+    );
+};
+
+export const customColumns = () => {
+    const column: ColumnsProps = [
+        {
+            Header: 'Category',
+            accessor: 'category',
+            Cell: ({ value }) => (
+                <div style={{ display: 'flex' }}>
+                    <p style={{ marginRight: 'var(--spacing-half)' }}>{value.value}</p>
+                    {value.tooltip && (<Tooltip>{value.tooltip}</Tooltip>)}
+                </div>
+            ),
+        },
+        {
+            Header: () => <div style={{ textAlign: 'right' }}>Amount ($)</div>,
+            accessor: 'amount',
+            Cell: ({ value }) => <div style={{ textAlign: 'right' }}>{value}</div>,
+        },
+    ];
+
+    const data = [
+        {
+            category: {
+                value: 'Safety fund',
+                tooltip: 'Money for emergencies',
+            },
+            amount: '2000$',
+        },
+        {
+            category: {
+                value: 'Investments',
+            },
+            amount: '12000$',
+        },
+    ];
+
+    return (
+        <Table columns={column} data={data} />
+    );
+}
