1ad7ea979b944125580e36222130754ca500e808
Maxime Gendron <maxime.gendron07@gmail.com>
2020-08-05 15:35:49 -0400

    Update types in  react-table-config.d.ts

--- packages/react/src/react-table-config.d.ts
+++ packages/react/src/react-table-config.d.ts
@@ -1,45 +1,4 @@
 import {
-    UseColumnOrderInstanceProps,
-    UseColumnOrderState,
-    UseExpandedHooks,
-    UseExpandedInstanceProps,
-    UseExpandedOptions,
-    UseExpandedRowProps,
-    UseExpandedState,
-    UseFiltersColumnOptions,
-    UseFiltersColumnProps,
-    UseFiltersInstanceProps,
-    UseFiltersOptions,
-    UseFiltersState,
-    UseGlobalFiltersColumnOptions,
-    UseGlobalFiltersInstanceProps,
-    UseGlobalFiltersOptions,
-    UseGlobalFiltersState,
-    UseGroupByCellProps,
-    UseGroupByColumnOptions,
-    UseGroupByColumnProps,
-    UseGroupByHooks,
-    UseGroupByInstanceProps,
-    UseGroupByOptions,
-    UseGroupByRowProps,
-    UseGroupByState,
-    UsePaginationInstanceProps,
-    UsePaginationOptions,
-    UsePaginationState,
-    UseResizeColumnsColumnOptions,
-    UseResizeColumnsColumnProps,
-    UseResizeColumnsOptions,
-    UseResizeColumnsState,
-    UseRowSelectHooks,
-    UseRowSelectInstanceProps,
-    UseRowSelectOptions,
-    UseRowSelectRowProps,
-    UseRowSelectState,
-    UseRowStateCellProps,
-    UseRowStateInstanceProps,
-    UseRowStateOptions,
-    UseRowStateRowProps,
-    UseRowStateState,
     UseSortByColumnOptions,
     UseSortByColumnProps,
     UseSortByHooks,
@@ -49,69 +8,23 @@ import {
 } from 'react-table';
 
 declare module 'react-table' {
-
   export interface TableOptions<D extends object>
-    extends UseExpandedOptions<D>,
-      UseFiltersOptions<D>,
-      UseGlobalFiltersOptions<D>,
-      UseGroupByOptions<D>,
-      UsePaginationOptions<D>,
-      UseResizeColumnsOptions<D>,
-      UseRowSelectOptions<D>,
-      UseRowStateOptions<D>,
-      UseSortByOptions<D>,
+    extends UseSortByOptions<D>,
       Record<string, any> {}
 
   export interface Hooks<D extends object = {}>
-    extends UseExpandedHooks<D>,
-      UseGroupByHooks<D>,
-      UseRowSelectHooks<D>,
-      UseSortByHooks<D> {}
+    extends UseSortByHooks<D> {}
 
   export interface TableInstance<D extends object = {}>
-    extends UseColumnOrderInstanceProps<D>,
-      UseExpandedInstanceProps<D>,
-      UseFiltersInstanceProps<D>,
-      UseGlobalFiltersInstanceProps<D>,
-      UseGroupByInstanceProps<D>,
-      UsePaginationInstanceProps<D>,
-      UseRowSelectInstanceProps<D>,
-      UseRowStateInstanceProps<D>,
-      UseSortByInstanceProps<D> {}
+    extends UseSortByInstanceProps<D> {}
 
   export interface TableState<D extends object = {}>
-    extends UseColumnOrderState<D>,
-      UseExpandedState<D>,
-      UseFiltersState<D>,
-      UseGlobalFiltersState<D>,
-      UseGroupByState<D>,
-      UsePaginationState<D>,
-      UseResizeColumnsState<D>,
-      UseRowSelectState<D>,
-      UseRowStateState<D>,
-      UseSortByState<D> {}
+    extends UseSortByState<D> {}
 
   export interface ColumnInterface<D extends object = {}>
-    extends UseFiltersColumnOptions<D>,
-      UseGlobalFiltersColumnOptions<D>,
-      UseGroupByColumnOptions<D>,
-      UseResizeColumnsColumnOptions<D>,
-      UseSortByColumnOptions<D>,
+    extends UseSortByColumnOptions<D>,
       Record<string, any> {}
 
   export interface ColumnInstance<D extends object = {}>
-    extends UseFiltersColumnProps<D>,
-      UseGroupByColumnProps<D>,
-      UseResizeColumnsColumnProps<D>,
-      UseSortByColumnProps<D> {}
-
-  export interface Cell<D extends object = {}, V = any>
-    extends UseGroupByCellProps<D>,
-      UseRowStateCellProps<D> {}
-
-  export interface Row<D extends object = {}>
-    extends UseExpandedRowProps<D>,
-      UseGroupByRowProps<D>,
-      UseRowSelectRowProps<D>,
-      UseRowStateRowProps<D> {}
+    extends UseSortByColumnProps<D> {}
 }
