2d6c620fdc706da31e189ea586f35ebdc743ec83
Maxime Gendron <maxime.gendron07@gmail.com>
2020-09-22 13:59:34 -0400

    Refactor button tests

--- packages/react/src/components/buttons/button.test.tsx
+++ packages/react/src/components/buttons/button.test.tsx
@@ -1,56 +1,58 @@
-import { mount } from 'enzyme';
+import { mountWithProviders, renderWithProviders } from '@design-elements/test-utils/renderer';
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { ThemeWrapped } from '../../test-utils/theme-wrapped';
 import { Button } from './button';
 
 describe('Button', () => {
     test('onClick callback is called when clicked', () => {
         const callback = jest.fn();
-        const wrapper = mount(
-            ThemeWrapped(<Button onClick={callback} buttonType="primary" label="Primary Button" />),
-        );
+        const wrapper = mountWithProviders(<Button onClick={callback} buttonType="primary" label="Primary Button" />);
+
         wrapper.find(Button).simulate('click');
+
         expect(callback).toHaveBeenCalledTimes(1);
     });
 
     test('onClick callback cannot be called when disabled', () => {
         const callback = jest.fn();
-        const wrapper = mount(
-            ThemeWrapped(<Button onClick={callback} buttonType="primary" disabled label="Primary Button" />),
-        );
+        const wrapper = mountWithProviders(<Button onClick={callback} buttonType="primary" disabled label="Primary Button" />);
+
         wrapper.find(Button).simulate('click');
+
         expect(callback).toHaveBeenCalledTimes(0);
     });
 
     test('Is disabled', () => {
-        const tree = renderer.create(
-            ThemeWrapped(<Button onClick={() => {}} buttonType="primary" disabled label="Primary Button" />),
-        ).toJSON();
+        const tree = renderWithProviders(
+            <Button onClick={() => {}} buttonType="primary" disabled label="Primary Button" />,
+            'desktop',
+        );
 
         expect(tree).toMatchSnapshot();
     });
 
     test('Has primary styles', () => {
-        const tree = renderer.create(
-            ThemeWrapped(<Button onClick={() => {}} buttonType="primary" label="Primary Button" />),
-        ).toJSON();
+        const tree = renderWithProviders(
+            <Button onClick={() => {}} buttonType="primary" label="Primary Button" />,
+            'desktop',
+        );
 
         expect(tree).toMatchSnapshot();
     });
 
     test('Has secondary styles', () => {
-        const tree = renderer.create(
-            ThemeWrapped(<Button onClick={() => {}} buttonType="secondary" label="Secondary Button" />),
-        ).toJSON();
+        const tree = renderWithProviders(
+            <Button onClick={() => {}} buttonType="secondary" label="Secondary Button" />,
+            'desktop',
+        );
 
         expect(tree).toMatchSnapshot();
     });
 
     test('Has tertiary styles', () => {
-        const tree = renderer.create(
-            ThemeWrapped(<Button onClick={() => {}} buttonType="tertiary" label="Tertiary Button" />),
-        ).toJSON();
+        const tree = renderWithProviders(
+            <Button onClick={() => {}} buttonType="tertiary" label="Tertiary Button" />,
+            'desktop',
+        );
 
         expect(tree).toMatchSnapshot();
     });
