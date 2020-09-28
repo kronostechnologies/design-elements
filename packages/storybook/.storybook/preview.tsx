15fb79145226efab526eaa732e2620abfc3ab2e2
Maxime Gendron <maxime.gendron07@gmail.com>
2020-09-28 15:05:07 -0400

    Remove design-elements/web from storybook package

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -1,6 +1,4 @@
 import { DeviceContextProvider, ThemeWrapper } from '@equisoft/design-elements-react';
-// tslint:disable-next-line:no-import-side-effect
-import '@equisoft/design-elements-web/style/body.scss';
 import { DocsContainer, DocsPage } from '@storybook/addon-docs/blocks';
 import React from 'react';
 
