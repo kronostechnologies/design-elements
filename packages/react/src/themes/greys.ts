94fc38af6b5671a05f8064ba5e90b111fd20f724
Maxime Gendron <maxime.gendron07@gmail.com>
2020-10-26 12:55:54 -0400

    update(theme): update colored-white color

--- packages/react/src/themes/greys.ts
+++ packages/react/src/themes/greys.ts
@@ -2,7 +2,7 @@ import { Theme } from '@design-elements/components/theme-wrapper/theme-wrapper';
 
 export const greys: Theme['greys'] = {
     white: '#FFFFFF',
-    'colored-white': '#F8F8FA',
+    'colored-white': '#F8F9FA',
     'light-grey': '#F1F2F2',
     grey: '#D9DDE2',
     'mid-grey': '#9CA7B4',
