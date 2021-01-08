6a69ba05c4ed1bdc12443fd36db50f813ff170b1
Maxime <maxime.gendron07@gmail.com>
2021-01-08 14:31:49 -0500

    refactor(Links): Optimize use of styled-components

--- packages/react/src/components/external-link/external-link.tsx
+++ packages/react/src/components/external-link/external-link.tsx
@@ -1,6 +1,24 @@
 import React, { MouseEvent, ReactElement, useCallback } from 'react';
+import styled from 'styled-components';
+
 import { Icon, IconName } from '../icon/icon';
 import { StyledLink } from '../route-link/styles/styled-link';
+
+const Link = styled(StyledLink)`
+    color: ${({ disabled, theme }) => (disabled ? '#7fbfd2' : theme.main['primary-1.1'])};
+
+    &:hover {
+        ${({ disabled }) => (disabled ? '' : 'text-decoration: underline')};
+    }
+
+    &:visited {
+        color: ${({ theme }) => theme.main['primary-3']};
+
+        svg {
+            color: ${({ theme }) => theme.main['primary-3']};
+        }
+    }
+`;
 
 interface ExternalLinkProps {
     className?: string;
@@ -24,16 +42,18 @@ export function ExternalLink({
     }, [href, onClick]);
 
     return (
-        <StyledLink
+        <Link
             aria-disabled={disabled ? 'true' : 'false'}
-            className={['external', className, !label && 'iconOnly'].filter(Boolean).join(' ')}
+            className={className}
             disabled={disabled}
+            $hasLabel={!!label}
             href={disabled ? undefined : href}
             onClick={disabled ? undefined : handleClick}
             target={target}
+            type="external"
         >
             {iconName && <Icon name={iconName} size="16" />}
             {label}
-        </StyledLink>
+        </Link>
     );
 }
