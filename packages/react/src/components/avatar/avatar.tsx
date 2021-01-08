d432f682fd1f3ddcb93e29d11eef98389ddfe05a
AlexisH <alexishinse@hotmail.com>
2021-01-07 19:45:42 -0500

    feat(Avatar); QLT-87 - Added differents avatars sizes support

--- packages/react/src/components/avatar/avatar.tsx
+++ packages/react/src/components/avatar/avatar.tsx
@@ -6,34 +6,101 @@ import { useDeviceContext } from '../device-context-provider/device-context-prov
 interface AvatarProps {
     className?: string;
     username: string;
+    size?: AvatarSize;
 }
 
-const StyledDiv = styled.div<{isMobile: boolean}>`
+export enum AvatarSize {
+    XSmall,
+    Small,
+    Medium,
+    Large
+}
+
+const StyledDiv = styled.div`
     align-items: center;
     background: ${({ theme }) => theme.greys['colored-white']};
     border-radius: 50%;
     display: flex;
-    height: ${({ isMobile }) => (isMobile ? '2.5rem' : '1.5rem')};
     justify-content: center;
-    width: ${({ isMobile }) => (isMobile ? '2.5rem' : '1.5rem')};
 `;
 
-const StyledSpan = styled.span<{isMobile: boolean}>`
-    /* TODO change colors when updating thematization */
-    color: #60666e;
-    font-size: ${({ isMobile }) => (isMobile ? '0.875rem' : '0.625rem')};
-    letter-spacing: ${({ isMobile }) => (isMobile ? '0.23px' : '0.17px')};
+const StyledSpan = styled.span`
+    color: ${({ theme }) => theme.greys['dark-grey']};
 `;
 
-export function Avatar({ className, username }: AvatarProps): ReactElement {
+const XSmallAvatarContainer = styled(StyledDiv)<{isMobile: boolean}>`
+    height: ${({ isMobile }) => (isMobile ? '2rem' : '1.5rem')};
+    width: ${({ isMobile }) => (isMobile ? '2rem' : '1.5rem')};
+`;
+
+const SmallAvatarContainer = styled(StyledDiv)<{isMobile: boolean}>`
+    height: ${({ isMobile }) => (isMobile ? '2.5rem' : '2rem')};
+    width: ${({ isMobile }) => (isMobile ? '2.5rem' : '2rem')};
+`;
+
+const MediumAvatarContainer = styled(StyledDiv)<{isMobile: boolean}>`
+    height: ${({ isMobile }) => (isMobile ? '3rem' : '3rem')};
+    width: ${({ isMobile }) => (isMobile ? '3rem' : '3rem')};
+`;
+
+const LargeAvatarContainer = styled(StyledDiv)<{isMobile: boolean}>`
+    height: ${({ isMobile }) => (isMobile ? '4.5rem' : '5rem')};
+    width: ${({ isMobile }) => (isMobile ? '4.5rem' : '5rem')};
+`;
+
+const XSmallAvatarSpan = styled(StyledSpan)<{isMobile: boolean}>`
+    font-size: ${({ isMobile }) => (isMobile ? '0.75rem' : '0.625rem')};
+    letter-spacing: ${({ isMobile }) => (isMobile ? '0.2px' : '0.17px')};
+`;
+
+const SmallAvatarSpan = styled(StyledSpan)<{isMobile: boolean}>`
+    font-size: ${({ isMobile }) => (isMobile ? '0.875rem' : '0.75rem')};
+    letter-spacing: ${({ isMobile }) => (isMobile ? '0.23px' : '0.2px')};
+`;
+
+const MediumAvatarSpan = styled(StyledSpan)<{isMobile: boolean}>`
+    font-size: ${({ isMobile }) => (isMobile ? '1rem' : '1rem')};
+`;
+
+const LargeAvatarSpan = styled(StyledSpan)<{isMobile: boolean}>`
+    font-size: ${({ isMobile }) => (isMobile ? '1.5rem' : '1.5rem')};
+`;
+
+export function Avatar({ className, username, size = AvatarSize.XSmall }: AvatarProps): ReactElement {
     const { isMobile } = useDeviceContext();
     const initials = useMemo(() => getInitialsFromUsername(username), [username]);
+    const ariaLabel = useMemo(() => username.concat(' avatar'), [username]);
 
     return (
-        <StyledDiv className={className} role="img" aria-label={username.concat(' avatar')} isMobile={isMobile}>
-            <StyledSpan data-testid="avatar-initials" isMobile={isMobile}>
-                {initials.length <= 2 && initials}
-            </StyledSpan>
-        </StyledDiv>
+        <div>
+            {(() => {
+                switch (size) {
+                    case AvatarSize.XSmall:
+                        return <XSmallAvatarContainer role="img" aria-label={ariaLabel} className={className} isMobile={isMobile}>
+                            <XSmallAvatarSpan data-testid="avatar-initials" isMobile={isMobile}>
+                                {initials.length <= 2 && initials}
+                            </XSmallAvatarSpan>
+                        </XSmallAvatarContainer>;
+                    case AvatarSize.Small:
+                        return <SmallAvatarContainer role="img" aria-label={ariaLabel} className={className} isMobile={isMobile}>
+                            <SmallAvatarSpan data-testid="avatar-initials" isMobile={isMobile}>
+                                {initials.length <= 2 && initials}
+                            </SmallAvatarSpan>
+                        </SmallAvatarContainer>;
+                    case AvatarSize.Medium:
+                        return <MediumAvatarContainer role="img" aria-label={ariaLabel} className={className} isMobile={isMobile}>
+                            <MediumAvatarSpan data-testid="avatar-initials" isMobile={isMobile}>
+                                {initials.length <= 2 && initials}
+                            </MediumAvatarSpan>
+                        </MediumAvatarContainer>;
+                    case AvatarSize.Large:
+                        return <LargeAvatarContainer role="img" aria-label={ariaLabel} className={className} isMobile={isMobile}>
+                            <LargeAvatarSpan data-testid="avatar-initials" isMobile={isMobile}>
+                                {initials.length <= 2 && initials}
+                            </LargeAvatarSpan>
+                        </LargeAvatarContainer>;
+                }
+            })()}
+        </div>
     );
 }
--- packages/storybook/stories/avatar.stories.tsx
+++ packages/storybook/stories/avatar.stories.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from '@equisoft/design-elements-react';
+import { Avatar, AvatarSize } from '@equisoft/design-elements-react';
 import { Story } from '@storybook/react';
 import React from 'react';
 import { DesktopDecorator, MobileDecorator } from './utils/device-context-decorator';
@@ -11,5 +11,23 @@ export default {
 export const Desktop: Story = () => <Avatar username="Patrick B" />;
 Desktop.decorators = [DesktopDecorator];
 
+export const smallSizeAvatar: Story = () => <Avatar username="Patrick B" size={AvatarSize.Small} />;
+smallSizeAvatar.decorators = [DesktopDecorator];
+
+export const mediumSizeAvatar: Story = () => <Avatar username="Patrick B" size={AvatarSize.Medium} />;
+mediumSizeAvatar.decorators = [DesktopDecorator];
+
+export const largeSizeAvatar: Story = () => <Avatar username="Patrick B" size={AvatarSize.Large} />;
+largeSizeAvatar.decorators = [DesktopDecorator];
+
 export const Mobile: Story = () => <Avatar username="John" />;
 Mobile.decorators = [MobileDecorator];
+
+export const smallSizeMobileAvatar: Story = () => <Avatar username="John" size={AvatarSize.Small} />;
+smallSizeMobileAvatar.decorators = [MobileDecorator];
+
+export const mediumSizeMobileAvatar: Story = () => <Avatar username="John" size={AvatarSize.Medium} />;
+mediumSizeMobileAvatar.decorators = [MobileDecorator];
+
+export const largeSizeMobileAvatar: Story = () => <Avatar username="John" size={AvatarSize.Large} />;
+largeSizeMobileAvatar.decorators = [MobileDecorator];
