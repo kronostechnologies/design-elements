21b4c7024f9770a3af9807dd12252bfdc92a54b8
Maxime <maxime.gendron07@gmail.com>
2021-01-12 08:21:15 -0500

    feat(Buttons): Add destructive styles

--- packages/react/src/components/buttons/button.tsx
+++ packages/react/src/components/buttons/button.tsx
@@ -5,7 +5,7 @@ import { focus } from '../../utils/css-state';
 import { useDeviceContext } from '../device-context-provider/device-context-provider';
 import { AbstractButton, AbstractButtonProps } from './abstract-button';
 
-type ButtonType = 'primary' | 'secondary' | 'tertiary';
+type ButtonType = 'primary' | 'secondary' | 'tertiary' | 'destructive';
 
 type Type = 'submit' | 'button' | 'reset';
 
@@ -20,11 +20,12 @@ interface ButtonProps extends AbstractButtonProps {
      * @default submit
      */
     type?: Type;
+    inversed?: boolean;
 }
 
 const StyledButton = styled(AbstractButton)<{ theme: Theme } & ButtonProps>`
     ${(props) => focus(props, true)};
-    ${({ theme, buttonType }) => {
+    ${({ theme, buttonType, inversed }) => {
         switch (buttonType) {
             case 'primary':
                 return `
@@ -84,6 +85,28 @@ const StyledButton = styled(AbstractButton)<{ theme: Theme } & ButtonProps>`
                         &:hover {
                             background-color: transparent;
                             color: ${theme.greys['mid-grey']};
+                        }
+                    }
+                `;
+            case 'destructive':
+                // TODO change colors when updating thematization
+                return `
+                    background-color: ${inversed ? theme.greys.white : theme.notifications['error-2.1']};
+                    border-color: ${inversed ? theme.greys.white : theme.notifications['error-2.1']};
+                    color: ${inversed ? theme.notifications['error-2.1'] : theme.greys.white};
+
+                    &:hover {
+                        background-color: ${inversed ? theme.greys.white : '#62071b'};
+                        color: ${inversed ? '#62071b' : theme.greys.white};
+                    }
+
+                    &:disabled {
+                        &,
+                        &:focus,
+                        &:hover {
+                            background-color: ${inversed ? theme.greys.white : '#ea8da3'};
+                            border-color: ${inversed ? theme.greys.white : '#ea8da3'};
+                            color: ${inversed ? '#ea8da3' : theme.greys.white};
                         }
                     }
                 `;
--- packages/storybook/stories/buttons/buttons.stories.tsx
+++ packages/storybook/stories/buttons/buttons.stories.tsx
@@ -12,15 +12,23 @@ export const Buttons: Story = () => (
         <Button label="Primary" buttonType="primary" disabled={false} />
         <Button label="Secondary" buttonType="secondary" disabled={false} />
         <Button label="Tertiary" buttonType="tertiary" disabled={false} />
+        <Button label="Destructive" buttonType="destructive" />
     </>
 );
+
+export const Inversed: Story = () => (
+    <Button label="Destructive" buttonType="destructive" inversed />
+);
+
 export const Disabled: Story = () => (
     <>
         <Button label="Primary" buttonType="primary" disabled />
         <Button label="Secondary" buttonType="secondary" disabled />
         <Button label="Tertiary" buttonType="tertiary" disabled />
+        <Button label="Destructive" buttonType="destructive" disabled />
     </>
 );
+
 export const EventCallback: Story = () => (
     <Button
         label="See Console For Callback"
