63e5f1ca973000bbcf4f413a8f328c3e87351e3c
Maxime <maxime.gendron07@gmail.com>
2021-01-15 10:31:55 -0500

    refactor(Buttons): Update buttonType styles

--- packages/storybook/stories/buttons/icon-button.stories.tsx
+++ packages/storybook/stories/buttons/icon-button.stories.tsx
@@ -12,15 +12,23 @@ export const IconButtons: Story = () => (
         <IconButton label="home" buttonType="primary" iconName="home" />
         <IconButton label="mail" buttonType="secondary" iconName="mail" />
         <IconButton label="map" buttonType="tertiary" iconName="mapPin" />
+        <IconButton label="Delete" buttonType="destructive" iconName="x" />
     </>
 );
+
+export const Inversed: Story = () => (
+    <IconButton label="Delete" buttonType="destructive" iconName="x" inversed />
+);
+
 export const Disabled: Story = () => (
     <>
         <IconButton label="home" buttonType="primary" iconName="home" disabled />
         <IconButton label="mail" buttonType="secondary" iconName="mail" disabled />
         <IconButton label="map" buttonType="tertiary" iconName="mapPin" disabled />
+        <IconButton label="Delete" buttonType="destructive" iconName="x" disabled />
     </>
 );
+
 export const EventCallback: Story = () => (
     <IconButton
         label="home"
