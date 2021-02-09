9ecc32a2d75ec1f98f6689521e7a51a9a891cb08
Hugo Deschênes <HDeschenes@equisoft.com>
2021-02-09 14:00:12 -0500

    feat(Storybook): Add example of currentPage state on two Pagination

--- packages/storybook/stories/pagination.stories.tsx
+++ packages/storybook/stories/pagination.stories.tsx
@@ -1,6 +1,6 @@
 import { Pagination } from '@equisoft/design-elements-react';
 import { Story } from '@storybook/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { DeviceContextDecorator } from './utils/device-context-decorator';
 
 export default {
@@ -16,6 +16,17 @@ export const Normal: Story = () => (
         <Pagination totalPages={50} numberOfResults={1530} />
     </>
 );
+
+export const LinkedPagination: Story = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    return (
+        <>
+            <Pagination totalPages={5} numberOfResults={100} onPageChange={(page: number) => setCurrentPage(page)} activePage={currentPage} />
+            <Pagination totalPages={5} numberOfResults={100} onPageChange={(page: number) => setCurrentPage(page)} activePage={currentPage} />
+        </>
+    )
+}
+
 export const WithoutResults: Story = () => (
     <>
         <Pagination totalPages={11} />
