fdafb51aa7a1b280f735adb96a66f77183993d78
Hugo Deschenes <hugo.deschenes@equisoft.com>
2021-05-18 15:53:02 -0400

    feat(TextInput): add autocomplete props to textinput

--- packages/react/src/components/card/card.tsx
+++ packages/react/src/components/card/card.tsx
@@ -4,7 +4,7 @@ const Card = styled.div`
     background: ${(props) => props.theme.greys.white};
     border: 1px solid ${(props) => props.theme.greys['light-grey']};
     border-radius: var(--border-radius);
-    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.1);
+    box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.2);
     box-sizing: border-box;
     margin-bottom: var(--spacing-3x);
     padding: var(--spacing-4x);
