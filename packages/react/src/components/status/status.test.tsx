5b80c9fa6d244b19eb93583b7f4167b025c07356
Maxime Gendron <53787300+maxime-gendron@users.noreply.github.com>
2021-06-28 09:23:16 -0400

    feat: add Status component (#260)

---
+++ packages/react/src/components/status/status.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Status, StatusType } from './status';
+import { mountWithTheme } from '../../test-utils/renderer';
+
+const statusTypesArray: StatusType[] = ['enabled', 'disabled', 'blocked'];
+
+describe('Status', () => {
+    statusTypesArray.forEach((type) => (
+        it(`matches snapshot (${type})`, () => {
+            const tree = mountWithTheme(<Status type={type} label={type} />);
+
+            expect(tree).toMatchSnapshot();
+        })
+    ));
+});
---
+++ packages/storybook/stories/status.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Status } from '@equisoft/design-elements-react';
+import { Story } from '@storybook/react';
+
+export default {
+    component: Status,
+    title: 'Data/Status',
+};
+
+export const Normal: Story = () => (
+    <>
+        <Status type="enabled" label="Enabled" />
+        <Status type="disabled" label="Disabled" />
+        <Status type="blocked" label="Blocked" />
+    </>
+);
