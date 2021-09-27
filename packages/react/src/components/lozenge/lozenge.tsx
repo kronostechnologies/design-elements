a8427a32ec17a877c02ef23a841aaf87eb5b5953
Jean-Simon Garneau <js.garneau@gmail.com>
2021-09-27 07:57:23 -0400

    feat(Lozenge): added basic lozenge component (#301)

---
+++ packages/react/src/components/lozenge/lozenge.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import { useDeviceContext } from '../device-context-provider/device-context-provider';
+
+const MAXIMUM_LENGTH = '312px';
+
+const StyledLozenge = styled.div<{ isMobile: boolean }>`
+    align-items: center;
+
+    /* TODO change colors when updating thematization */
+    background-color: ${({ theme }) => theme.greys['light-grey']};
+    border: 1px solid #878f9a;
+    border-radius: ${({ isMobile }) => (isMobile ? 'var(--border-radius)' : 'var(--border-radius-half)')};
+    box-sizing: border-box;
+    display: inline-block;
+    font-size: ${({ isMobile }) => (isMobile ? '0.875rem' : '0.75rem')};
+    line-height: ${({ isMobile }) => (isMobile ? '1.375rem' : '0.875rem')};
+    max-width: ${MAXIMUM_LENGTH};
+    overflow: hidden;
+    padding: 0 var(--spacing-1x);
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`;
+
+export const Lozenge: FunctionComponent = ({
+    children,
+}) => {
+    const { isMobile } = useDeviceContext();
+    return <StyledLozenge isMobile={isMobile}>{children}</StyledLozenge>;
+};
