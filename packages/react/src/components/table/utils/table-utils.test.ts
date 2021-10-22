6ccddf7de95bfee22e8cb566c07ee7ceacf3a4aa
Jeremi LC <jeremi.leclerc-cote@equisoft.com>
2021-10-22 14:22:12 -0400

    feat: sticky column/row

---
+++ packages/react/src/components/table/utils/table-utils.test.ts
@@ -0,0 +1,123 @@
+import {
+    calculateStickyColumns, calculateStickyHeader,
+} from './table-utils';
+
+function getTable(): HTMLTableElement {
+    const table: HTMLTableElement = document.createElement('table');
+    const header: HTMLTableRowElement = document.createElement('tr');
+    header.appendChild(document.createElement('th'));
+    header.appendChild(document.createElement('th'));
+    header.appendChild(document.createElement('th'));
+    table.appendChild(header);
+    const row1: HTMLTableRowElement = document.createElement('tr');
+    row1.appendChild(document.createElement('td'));
+    row1.appendChild(document.createElement('td'));
+    row1.appendChild(document.createElement('td'));
+    table.appendChild(row1);
+    const row2: HTMLTableRowElement = document.createElement('tr');
+    row2.appendChild(document.createElement('td'));
+    row2.appendChild(document.createElement('td'));
+    row2.appendChild(document.createElement('td'));
+    table.appendChild(row2);
+    const row3: HTMLTableRowElement = document.createElement('tr');
+    row3.appendChild(document.createElement('td'));
+    row3.appendChild(document.createElement('td'));
+    row3.appendChild(document.createElement('td'));
+    table.appendChild(row3);
+    return table;
+}
+
+function getTableValues() : {
+    headerCells: HTMLCollectionOf<HTMLTableHeaderCellElement>,
+    rows: HTMLCollectionOf<HTMLTableRowElement>,
+    } {
+    const table = getTable();
+
+    return {
+        headerCells: table.getElementsByTagName('th'),
+        rows: table.getElementsByTagName('tr'),
+    };
+}
+
+describe('Table utils', () => {
+    describe('calculateStickyColumns', () => {
+        test('should set header cell z-index when column is sticky', () => {
+            const { headerCells, rows } = getTableValues();
+
+            calculateStickyColumns([true, true, false], headerCells, rows);
+
+            expect(headerCells[0].style.getPropertyValue('z-index')).toEqual('2');
+            expect(headerCells[1].style.getPropertyValue('z-index')).toEqual('2');
+            expect(headerCells[2].style.getPropertyValue('z-index')).toEqual('');
+        });
+
+        test('should set all rows column z-index when column is sticky', () => {
+            const { headerCells, rows } = getTableValues();
+
+            calculateStickyColumns([true, true, false], headerCells, rows);
+
+            Array.from(rows).forEach((row) => {
+                expect(row.cells[0].style.getPropertyValue('z-index')).toEqual('2');
+                expect(row.cells[1].style.getPropertyValue('z-index')).toEqual('2');
+                expect(row.cells[2].style.getPropertyValue('z-index')).toEqual('');
+            });
+        });
+
+        test('should set header cell left when column is sticky', () => {
+            const { headerCells, rows } = getTableValues();
+
+            calculateStickyColumns([true, true, false], headerCells, rows);
+
+            expect(headerCells[0].style.getPropertyValue('left')).toEqual('0px');
+            const left = headerCells[0].clientWidth;
+            expect(headerCells[1].style.getPropertyValue('left')).toEqual(`${left}px`);
+            expect(headerCells[2].style.getPropertyValue('left')).toEqual('');
+        });
+
+        test('should set all rows column left when column is sticky', () => {
+            const { headerCells, rows } = getTableValues();
+
+            calculateStickyColumns([true, true, false], headerCells, rows);
+
+            const left = headerCells[0].clientWidth;
+            Array.from(rows).forEach((row) => {
+                expect(row.cells[0].style.getPropertyValue('left')).toEqual('0px');
+                expect(row.cells[1].style.getPropertyValue('left')).toEqual(`${left}px`);
+                expect(row.cells[2].style.getPropertyValue('left')).toEqual('');
+            });
+        });
+    });
+
+    describe('calculateStickyRows', () => {
+        test('should set header cell z-index when header is sticky', () => {
+            const { headerCells } = getTableValues();
+
+            calculateStickyHeader([false, false, false], headerCells);
+
+            expect(headerCells[0].style.getPropertyValue('z-index')).toEqual('4');
+            expect(headerCells[1].style.getPropertyValue('z-index')).toEqual('4');
+            expect(headerCells[2].style.getPropertyValue('z-index')).toEqual('4');
+        });
+
+        test('should set header cell z-index higher when both header and column are sticky', () => {
+            const { headerCells } = getTableValues();
+
+            // first column is sticky
+            calculateStickyHeader([true, false, false], headerCells);
+
+            expect(headerCells[0].style.getPropertyValue('z-index')).toEqual('5');
+            expect(headerCells[1].style.getPropertyValue('z-index')).toEqual('4');
+            expect(headerCells[2].style.getPropertyValue('z-index')).toEqual('4');
+        });
+
+        test('should set header cell top when header is sticky', () => {
+            const { headerCells } = getTableValues();
+
+            calculateStickyHeader([true, false, false], headerCells);
+
+            expect(headerCells[0].style.getPropertyValue('top')).toEqual('0px');
+            expect(headerCells[1].style.getPropertyValue('top')).toEqual('0px');
+            expect(headerCells[2].style.getPropertyValue('top')).toEqual('0px');
+        });
+    });
+});
--- packages/storybook/stories/table.stories.tsx
+++ packages/storybook/stories/table.stories.tsx
@@ -476,3 +476,256 @@ export const SelectableRows: Story = () => {
         <Table<SelectableData> selectableRows columns={columns} data={data} onSelectedRowsChange={console.info} />
     );
 };
+
+interface StickyData {
+    column1: string;
+    column2: string;
+    column3: string;
+    column4: string;
+    column5: string;
+    column6: string;
+    column7: string;
+    column8: string;
+    column9: string;
+    column10: string;
+    column11: string;
+    column12: string;
+    column13: string;
+    column14: string;
+    column15: string,
+}
+
+export const Sticky: Story = () => {
+    const columns: TableColumn<StickyData> = [
+        {
+            Header: 'Column 1',
+            accessor: 'column1',
+            sticky: true,
+        },
+        {
+            Header: 'Column 2',
+            accessor: 'column2',
+            sticky: true,
+        },
+        {
+            Header: 'Column 3',
+            accessor: 'column3',
+        },
+        {
+            Header: 'Column 4',
+            accessor: 'column4',
+        },
+        {
+            Header: 'Column 5',
+            accessor: 'column5',
+        },
+        {
+            Header: 'Column 6',
+            accessor: 'column6',
+        },
+        {
+            Header: 'Column 7',
+            accessor: 'column7',
+        },
+        {
+            Header: 'Column 8',
+            accessor: 'column8',
+        },
+        {
+            Header: 'Column 9',
+            accessor: 'column9',
+        },
+        {
+            Header: 'Column 10',
+            accessor: 'column10',
+        },
+        {
+            Header: 'Column 11',
+            accessor: 'column11',
+        },
+        {
+            Header: 'Column 12',
+            accessor: 'column12',
+        },
+        {
+            Header: 'Column 13',
+            accessor: 'column13',
+        },
+        {
+            Header: 'Column 14',
+            accessor: 'column14',
+        },
+        {
+            Header: 'Column 15',
+            accessor: 'column15',
+        },
+    ];
+
+    const data: TableRow<StickyData>[] = [
+        {
+            column1: 'a',
+            column2: 'a',
+            column3: 'a',
+            column4: 'a',
+            column5: 'a',
+            column6: 'a',
+            column7: 'a',
+            column8: 'a',
+            column9: 'a',
+            column10: 'a',
+            column11: 'a',
+            column12: 'a',
+            column13: 'a',
+            column14: 'a',
+            column15: 'a',
+        },
+        {
+            column1: 'b',
+            column2: 'b',
+            column3: 'b',
+            column4: 'b',
+            column5: 'b',
+            column6: 'b',
+            column7: 'b',
+            column8: 'b',
+            column9: 'b',
+            column10: 'b',
+            column11: 'b',
+            column12: 'b',
+            column13: 'b',
+            column14: 'b',
+            column15: 'b',
+        },
+        {
+            column1: 'c',
+            column2: 'c',
+            column3: 'c',
+            column4: 'c',
+            column5: 'c',
+            column6: 'c',
+            column7: 'c',
+            column8: 'c',
+            column9: 'c',
+            column10: 'c',
+            column11: 'c',
+            column12: 'c',
+            column13: 'c',
+            column14: 'c',
+            column15: 'c',
+            error: true,
+        },
+        {
+            column1: 'd',
+            column2: 'd',
+            column3: 'd',
+            column4: 'd',
+            column5: 'd',
+            column6: 'd',
+            column7: 'd',
+            column8: 'd',
+            column9: 'd',
+            column10: 'd',
+            column11: 'd',
+            column12: 'd',
+            column13: 'd',
+            column14: 'd',
+            column15: 'd',
+        },
+        {
+            column1: 'e',
+            column2: 'e',
+            column3: 'e',
+            column4: 'e',
+            column5: 'e',
+            column6: 'e',
+            column7: 'e',
+            column8: 'e',
+            column9: 'e',
+            column10: 'e',
+            column11: 'e',
+            column12: 'e',
+            column13: 'e',
+            column14: 'e',
+            column15: 'e',
+        },
+        {
+            column1: 'f',
+            column2: 'f',
+            column3: 'f',
+            column4: 'f',
+            column5: 'f',
+            column6: 'f',
+            column7: 'f',
+            column8: 'f',
+            column9: 'f',
+            column10: 'f',
+            column11: 'f',
+            column12: 'f',
+            column13: 'f',
+            column14: 'f',
+            column15: 'f',
+        },
+        {
+            column1: 'g',
+            column2: 'g',
+            column3: 'g',
+            column4: 'g',
+            column5: 'g',
+            column6: 'g',
+            column7: 'g',
+            column8: 'g',
+            column9: 'g',
+            column10: 'g',
+            column11: 'g',
+            column12: 'g',
+            column13: 'g',
+            column14: 'g',
+            column15: 'g',
+        },
+        {
+            column1: 'h',
+            column2: 'h',
+            column3: 'h',
+            column4: 'h',
+            column5: 'h',
+            column6: 'h',
+            column7: 'h',
+            column8: 'h',
+            column9: 'h',
+            column10: 'h',
+            column11: 'h',
+            column12: 'h',
+            column13: 'h',
+            column14: 'h',
+            column15: 'h',
+        },
+        {
+            column1: 'i',
+            column2: 'i',
+            column3: 'i',
+            column4: 'i',
+            column5: 'i',
+            column6: 'i',
+            column7: 'i',
+            column8: 'i',
+            column9: 'i',
+            column10: 'i',
+            column11: 'i',
+            column12: 'i',
+            column13: 'i',
+            column14: 'i',
+            column15: 'i',
+        },
+    ];
+
+    const Wrap = styled.div`
+        height: 200px;
+        overflow: scroll;
+`;
+
+    return (
+        <Wrap>
+            <Table columns={columns} data={data} stickyHeader onRowClick={(row) => console.info('row: ', row)} />
+        </Wrap>
+    );
+};
