8e951d662631c29268bbb5775e5371a0f2f1e144
Maxime <maxime.gendron07@gmail.com>
2021-11-17 09:49:53 -0500

    feat(Lozenge): add icon prop

--- packages/react/src/components/lozenge/lozenge.tsx
+++ packages/react/src/components/lozenge/lozenge.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 import { useDeviceContext } from '../device-context-provider/device-context-provider';
+import { Icon, IconName } from '../icon/icon';
 
 const MAXIMUM_LENGTH = '312px';
 
@@ -11,7 +12,7 @@ const StyledLozenge = styled.span<{ isMobile: boolean }>`
     border-radius: ${({ isMobile }) => (isMobile ? 'var(--border-radius)' : 'var(--border-radius-half)')};
     box-sizing: border-box;
     color: ${({ theme }) => theme.greys['dark-grey']};
-    display: inline-block;
+    display: inline-flex;
     font-size: ${({ isMobile }) => (isMobile ? '0.875rem' : '0.75rem')};
     line-height: ${({ isMobile }) => (isMobile ? '1.375rem' : '0.875rem')};
     max-width: ${MAXIMUM_LENGTH};
@@ -20,16 +21,35 @@ const StyledLozenge = styled.span<{ isMobile: boolean }>`
     text-overflow: ellipsis;
     text-transform: uppercase;
     white-space: nowrap;
+    width: fit-content;
+`;
+
+const StyledIcon = styled(Icon)<{ isMobile: boolean }>`
+    margin-right: ${({ isMobile }) => (isMobile ? 'var(--spacing-1x)' : 'var(--spacing-half)')};
 `;
 
 interface Props {
     className?: string;
+    icon?: IconName;
 }
 
 export const Lozenge: FunctionComponent<Props> = ({
     children,
     className,
+    icon,
 }) => {
     const { isMobile } = useDeviceContext();
-    return <StyledLozenge className={className} isMobile={isMobile}>{children}</StyledLozenge>;
+    return (
+        <StyledLozenge className={className} isMobile={isMobile}>
+            {icon && (
+                <StyledIcon
+                    data-testid="lozenge-icon"
+                    name={icon}
+                    size={isMobile ? '16' : '12'}
+                    isMobile={isMobile}
+                />
+            )}
+            {children}
+        </StyledLozenge>
+    );
 };
