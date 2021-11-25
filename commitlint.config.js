db78efafa788e84e95367f5c78c00e540b8bc5b8
Christopher Viel <christopher.viel@equisoft.com>
2021-11-25 12:32:40 -0500

    chore: dependabot commit headers can hide versions

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    ignores: [(message) => /^chore\(.+\): bump [^ ]+ from [^ ]+ to [^ ]+$/m.test(message)],
+    ignores: [(message) => /^chore\([a-z-]+\): bump [^ ]+( from [^ ]+ to [^ ]+)?$/m.test(message)],
     rules: {
         'scope-case': [2, 'always', ['lower-case', 'pascal-case', 'camel-case']],
     },
