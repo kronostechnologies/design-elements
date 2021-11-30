e3db147574bf65bd8805db4ab9922b211655d100
Maxime <maxime.gendron07@gmail.com>
2021-11-30 16:59:29 -0500

    chore(Card): update border-radius

--- packages/react/src/components/card/card.tsx
+++ packages/react/src/components/card/card.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Card = styled.div`
     background: ${(props) => props.theme.greys.white};
     border: 1px solid ${(props) => props.theme.greys['light-grey']};
-    border-radius: var(--border-radius);
+    border-radius: var(--border-radius-2x);
     box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.2);
     box-sizing: border-box;
     margin-bottom: var(--spacing-3x);
