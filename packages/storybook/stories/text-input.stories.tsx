a84120fbb43c00aa015206cce3d8fc75995c86d4
Maxime <maxime.gendron07@gmail.com>
2021-12-17 09:53:13 -0500

    refactor(TextInput): remove useless props in stories

--- packages/storybook/stories/text-input.stories.tsx
+++ packages/storybook/stories/text-input.stories.tsx
@@ -9,51 +9,35 @@ export default {
 };
 
 export const Inputs: Story = () => (
-    <div>
+    <>
         <TextInput
-            disabled={false}
-            required={false}
             label="First Name"
             type="text"
-            defaultValue=""
-            pattern=""
             validationErrorMessage="Error message"
             hint="Hint"
         />
         <TextInput
-            disabled={false}
-            required={false}
             label="Email"
             placeholder="you@example.com"
             type="email"
-            defaultValue=""
-            pattern=""
             validationErrorMessage="Error message"
             hint="Hint"
         />
         <TextInput
-            disabled={false}
-            required={false}
             label="Phone"
             placeholder="Ex.: 555-555-5555"
             type="tel"
-            defaultValue=""
-            pattern=""
             validationErrorMessage="Error message"
             hint="Hint"
         />
-    </div>
+    </>
 );
 
 export const DefaultValue: Story = () => (
     <TextInput
-        disabled={false}
-        required={false}
         label="Address"
         type="text"
         defaultValue="1234 Main Street"
-        pattern=""
-        validationErrorMessage="Error message"
     />
 );
 
@@ -67,25 +51,18 @@ export const WithTooltip: Story = () => (
 
 export const Required: Story = () => (
     <TextInput
-        disabled={false}
         required
         label="Last Name (required)"
         type="text"
-        defaultValue=""
-        pattern=""
         validationErrorMessage="This field is required"
     />
 );
 
 export const EventCallbacks: Story = () => (
     <TextInput
-        disabled={false}
         required
         label="See console for callbacks"
         type="text"
-        defaultValue=""
-        pattern=""
-        validationErrorMessage="Error message"
         onChange={(event) => {
             console.info(`Custom function called on change. Current value: ${event.currentTarget.value}`);
         }}
@@ -101,27 +78,20 @@ EventCallbacks.parameters = rawCodeParameters;
 
 export const PatternValidation: Story = () => (
     <TextInput
-        disabled={false}
         required
         label="Telephone"
         placeholder="Ex.: 555-123-4567"
         type="tel"
-        defaultValue=""
         pattern="[0-9]{3}-?[0-9]{3}-?[0-9]{4}"
-        validationErrorMessage="Please enter a valid phone number"
     />
 );
 
 export const Disabled: Story = () => (
     <TextInput
         disabled
-        required={false}
         label="A disabled text input"
         placeholder="Sorry but this field is disabled"
         type="text"
-        defaultValue=""
-        pattern=""
-        validationErrorMessage="Error message"
     />
 );
 
@@ -134,7 +104,6 @@ export const RequiredInForm: Story = () => {
     return (
         <form noValidate onSubmit={handleSubmit}>
             <TextInput
-                disabled={false}
                 required
                 label="Last Name (required)"
                 type="text"
