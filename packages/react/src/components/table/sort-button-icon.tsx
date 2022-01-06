a49552dea6ceaafc8f15b6ea877bbecef2439356
Maxime <maxime.gendron07@gmail.com>
2022-01-06 16:27:36 -0500

    refactor(Table): inverse sort icons

--- packages/react/src/components/table/sort-button-icon.tsx
+++ packages/react/src/components/table/sort-button-icon.tsx
@@ -13,9 +13,9 @@ export const SortButtonIcon: VoidFunctionComponent<SortButtonIconProps> = ({ sor
 
     switch (sort) {
         case 'ascending':
-            return <Icon name="arrowUp" size="16" color={theme.greys['dark-grey']} />;
-        case 'descending':
             return <Icon name="arrowDown" size="16" color={theme.greys['dark-grey']} />;
+        case 'descending':
+            return <Icon name="arrowUp" size="16" color={theme.greys['dark-grey']} />;
         default:
             return <Icon name="reorder" size="16" color={theme.greys['dark-grey']} />;
     }
