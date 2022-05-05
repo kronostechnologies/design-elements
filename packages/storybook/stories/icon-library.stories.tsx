5ea3ca4b7fb3342504e393e19e869882dccf2e4a
Pierre-Yves Lafleur <pierre-yves.lafleur@equisoft.com>
2022-05-05 11:20:50 -0400

    feat(IconGallery): use IconGallery to display icon components
    - Display all available icons in the library

--- packages/storybook/stories/icon-library.stories.tsx
+++ packages/storybook/stories/icon-library.stories.tsx
@@ -1,7 +1,6 @@
 import { Icon } from '@equisoft/design-elements-react';
 import { IconGallery, IconItem } from '@storybook/addon-docs';
 import { Story } from '@storybook/react';
-import { ComponentType } from 'react';
 
 export default {
     title: 'Icons/Library',
@@ -10,148 +9,151 @@ export default {
 
 export const All: Story = () => (
     <>
-        <Icon name="alertFilled" />
-        <Icon name="alertTriangle" />
-        <Icon name="arrowLeft" />
-        <Icon name="check" />
-        <Icon name="chevronDown" />
-        <Icon name="chevronUp" />
-        <Icon name="contracts" />
-        <Icon name="copy" />
-        <Icon name="bell" />
-        <Icon name="bento" />
-        <Icon name="edit" />
-        <Icon name="equisoft" />
-        <Icon name="files" />
-        <Icon name="helpCircle" />
-        <Icon name="history" />
-        <Icon name="home" />
-        <Icon name="info" />
-        <Icon name="mail" />
-        <Icon name="mapPin" />
-        <Icon name="menu" />
-        <Icon name="moreHorizontal" />
-        <Icon name="organization" />
-        <Icon name="open" />
-        <Icon name="phone" />
-        <Icon name="search" />
-        <Icon name="transfer" />
-        <Icon name="trash" />
-        <Icon name="user" />
-        <Icon name="users" />
-        <Icon name="warningFilled" />
-        <Icon name="x" />
+        <IconGallery>
+            <IconItem name="alertFilled">
+                <Icon name="alertFilled" />
+            </IconItem>
+            <IconItem name="alertTriangle">
+                <Icon name="alertTriangle" />
+            </IconItem>
+            <IconItem name="alertOctagon">
+                <Icon name="alertOctagon" />
+            </IconItem>
+            <IconItem name="arrowDown">
+                <Icon name="arrowDown" />
+            </IconItem>
+            <IconItem name="arrowLeft">
+                <Icon name="arrowLeft" />
+            </IconItem>
+            <IconItem name="arrowRight">
+                <Icon name="arrowRight" />
+            </IconItem>
+            <IconItem name="arrowUp">
+                <Icon name="arrowUp" />
+            </IconItem>
+            <IconItem name="bell">
+                <Icon name="bell" />
+            </IconItem>
+            <IconItem name="bento">
+                <Icon name="bento" />
+            </IconItem>
+            <IconItem name="calendar">
+                <Icon name="calendar" />
+            </IconItem>
+            <IconItem name="check">
+                <Icon name="check" />
+            </IconItem>
+            <IconItem name="chevronDown">
+                <Icon name="chevronDown" />
+            </IconItem>
+            <IconItem name="chevronLeft">
+                <Icon name="chevronLeft" />
+            </IconItem>
+            <IconItem name="chevronRight">
+                <Icon name="chevronRight" />
+            </IconItem>
+            <IconItem name="chevronUp">
+                <Icon name="chevronUp" />
+            </IconItem>
+            <IconItem name="chevronsLeft">
+                <Icon name="chevronsLeft" />
+            </IconItem>
+            <IconItem name="chevronsRight">
+                <Icon name="chevronsRight" />
+            </IconItem>
+            <IconItem name="contracts">
+                <Icon name="contracts" />
+            </IconItem>
+            <IconItem name="copy">
+                <Icon name="copy" />
+            </IconItem>
+            <IconItem name="edit">
+                <Icon name="edit" />
+            </IconItem>
+            <IconItem name="equisoft">
+                <Icon name="equisoft" />
+            </IconItem>
+            <IconItem name="externalLink">
+                <Icon name="externalLink" />
+            </IconItem>
+            <IconItem name="eye">
+                <Icon name="eye" />
+            </IconItem>
+            <IconItem name="files">
+                <Icon name="files" />
+            </IconItem>
+            <IconItem name="helpCircle">
+                <Icon name="helpCircle" />
+            </IconItem>
+            <IconItem name="history">
+                <Icon name="history" />
+            </IconItem>
+            <IconItem name="home">
+                <Icon name="home" />
+            </IconItem>
+            <IconItem name="info">
+                <Icon name="info" />
+            </IconItem>
+            <IconItem name="mail">
+                <Icon name="mail" />
+            </IconItem>
+            <IconItem name="mapPin">
+                <Icon name="mapPin" />
+            </IconItem>
+            <IconItem name="menu">
+                <Icon name="menu" />
+            </IconItem>
+            <IconItem name="minus">
+                <Icon name="minus" />
+            </IconItem>
+            <IconItem name="moreHorizontal">
+                <Icon name="moreHorizontal" />
+            </IconItem>
+            <IconItem name="moreVertical">
+                <Icon name="moreVertical" />
+            </IconItem>
+            <IconItem name="organization">
+                <Icon name="organization" />
+            </IconItem>
+            <IconItem name="open">
+                <Icon name="open" />
+            </IconItem>
+            <IconItem name="phone">
+                <Icon name="phone" />
+            </IconItem>
+            <IconItem name="plusSign">
+                <Icon name="plusSign" />
+            </IconItem>
+            <IconItem name="reorder">
+                <Icon name="reorder" />
+            </IconItem>
+            <IconItem name="search">
+                <Icon name="search" />
+            </IconItem>
+            <IconItem name="settings">
+                <Icon name="settings" />
+            </IconItem>
+            <IconItem name="star">
+                <Icon name="star" />
+            </IconItem>
+            <IconItem name="transfer">
+                <Icon name="transfer" />
+            </IconItem>
+            <IconItem name="trash">
+                <Icon name="trash" />
+            </IconItem>
+            <IconItem name="user">
+                <Icon name="user" />
+            </IconItem>
+            <IconItem name="users">
+                <Icon name="users" />
+            </IconItem>
+            <IconItem name="warningFilled">
+                <Icon name="warningFilled" />
+            </IconItem>
+            <IconItem name="x">
+                <Icon name="x" />
+            </IconItem>
+        </IconGallery>
     </>
-);
-
-All.decorators = [
-    (DecoratedStory: ComponentType) => (
-        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <DecoratedStory />
-        </div>
-    ),
-];
-
-export const AlertTriangle: Story = () => (
-    <Icon name="alertTriangle" />
-);
-
-export const ArrowLeft: Story = () => (
-    <Icon name="arrowLeft" />
-);
-
-export const Check: Story = () => (
-    <Icon name="check" />
-);
-
-export const ChevronDown: Story = () => (
-    <Icon name="chevronDown" />
-);
-
-export const ChevronUp: Story = () => (
-    <Icon name="chevronUp" />
-);
-
-export const Contracts: Story = () => (
-    <Icon name="contracts" />
-);
-
-export const Copy: Story = () => (
-    <Icon name="copy" />
-);
-
-export const Edit: Story = () => (
-    <Icon name="edit" />
-);
-
-export const Files: Story = () => (
-    <Icon name="files" />
-);
-
-export const HelpCircle: Story = () => (
-    <Icon name="helpCircle" />
-);
-
-export const History: Story = () => (
-    <Icon name="history" />
-);
-
-export const Home: Story = () => (
-    <Icon name="home" />
-);
-
-export const Info: Story = () => (
-    <Icon name="info" />
-);
-
-export const Mail: Story = () => (
-    <Icon name="mail" />
-);
-
-export const MapPin: Story = () => (
-    <Icon name="mapPin" />
-);
-
-export const Menu: Story = () => (
-    <Icon name="menu" />
-);
-
-export const MoreHorizontal: Story = () => (
-    <Icon name="moreHorizontal" />
-);
-
-export const Organization: Story = () => (
-    <Icon name="organization" />
-);
-
-export const Open: Story = () => (
-    <Icon name="open" />
-);
-
-export const Phone: Story = () => (
-    <Icon name="phone" />
-);
-
-export const Search: Story = () => (
-    <Icon name="search" />
-);
-
-export const Trash: Story = () => (
-    <Icon name="trash" />
-);
-
-export const Trasnfer: Story = () => (
-    <Icon name="transfer" />
-);
-
-export const User: Story = () => (
-    <Icon name="user" />
-);
-
-export const Users: Story = () => (
-    <Icon name="users" />
-);
-
-export const X: Story = () => (
-    <Icon name="x" />
 );
