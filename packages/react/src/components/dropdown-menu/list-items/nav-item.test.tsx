1c8252ec8ae557c715d51805f1eef3f1208b14be
Alexandre Brillant <alexandre.brillant@equisoft.com>
2022-10-03 11:01:26 -0400

    feat(NavLink): ensure NavLinkProps are available on StyledNavLink

--- packages/react/src/components/dropdown-menu/list-items/nav-item.test.tsx
+++ packages/react/src/components/dropdown-menu/list-items/nav-item.test.tsx
@@ -1,6 +1,8 @@
 import { shallow } from 'enzyme';
 import { getByTestId } from '../../../test-utils/enzyme-selectors';
-import { NavItem } from './nav-item';
+import { breakpoints } from '../../../tokens/breakpoints';
+import { DesignSystem } from '../../design-system';
+import { NavItem, StyledNavItem } from './nav-item';
 
 describe('NavItem', () => {
     it('displays screen-reader-only text when router link opens in a new tab (target="_blank")', () => {
@@ -13,5 +15,25 @@ describe('NavItem', () => {
         const wrapper = shallow(<NavItem value="test" isHtmlLink href="test" target="_blank" />);
 
         expect(getByTestId(wrapper, 'screen-reader-text').exists()).toBe(true);
+    });
+
+    it('styled nav item has access to NavLinkProps', () => {
+        shallow(
+            <DesignSystem>
+                <StyledNavItem
+                    $device={{
+                        device: 'desktop',
+                        isDesktop: true,
+                        isTablet: false,
+                        isMobile: false,
+                        breakpoints,
+                    }}
+                    to="some-route"
+                    title="some title"
+                    exact
+                    isActive={() => true}
+                />
+            </DesignSystem>,
+        );
     });
 });
