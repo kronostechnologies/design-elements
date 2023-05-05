f8cc11aa3220977cbc1f5db9e7b1594cf3fb0f8e
Pierre-Yves Lafleur <pierre-yves.lafleur@equisoft.com>
2023-05-05 12:07:45 -0400

    feat(ErrorSummary): update import for SB v7

--- packages/storybook/stories/error-summary.stories.tsx
+++ packages/storybook/stories/error-summary.stories.tsx
@@ -1,5 +1,5 @@
 import { ErrorMessage, ErrorSummary, TextInput } from '@equisoft/design-elements-react';
-import { Story } from '@storybook/react';
+import { StoryFn as Story } from '@storybook/react';
 import styled from 'styled-components';
 
 export default {
