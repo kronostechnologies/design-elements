fd0b28e9b089fbcb8aa871527e53fb9afb8930ca
kronostechnologies-build <na-qc@equisoft.com>
2023-05-08 18:22:15 +0000

    docs: update snapshot to 92ea9e67200961ee27329c06dbf8472321c7c6b3

---
+++ snapshot/skip-link-stories.7562675a.iframe.bundle.js
@@ -0,0 +1,18 @@
+"use strict";(self.webpackChunk_equisoft_design_elements_storybook=self.webpackChunk_equisoft_design_elements_storybook||[]).push([[4915],{"./stories/skip-link.stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Normal:()=>Normal,__namedExportsOrder:()=>__namedExportsOrder,default:()=>__WEBPACK_DEFAULT_EXPORT__});var _equisoft_design_elements_react__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("../../.yarn/__virtual__/@equisoft-design-elements-react-virtual-0c2f823c14/1/packages/react/dist/bundle.js"),styled_components__WEBPACK_IMPORTED_MODULE_2__=__webpack_require__("../../.yarn/__virtual__/styled-components-virtual-f4ef20953a/0/cache/styled-components-npm-5.3.10-601dcdbf2b-46dfea6c43.zip/node_modules/styled-components/dist/styled-components.browser.esm.js"),react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("../../.yarn/cache/react-npm-17.0.2-99ba37d931-b254cc17ce.zip/node_modules/react/jsx-runtime.js");const __WEBPACK_DEFAULT_EXPORT__={title:"Navigation/Skip Link",component:_equisoft_design_elements_react__WEBPACK_IMPORTED_MODULE_0__.SkipLink},Navigation=styled_components__WEBPACK_IMPORTED_MODULE_2__.default.nav`
+    align-items: center;
+    background-color: ${({theme})=>theme.greys.black};
+    border-radius: var(--border-radius) var(--border-radius) 0 0;
+    color: ${({theme})=>theme.greys.white};
+    display: flex;
+    height: 56px;
+    justify-content: center;
+    width: 100%;
+`,Main=styled_components__WEBPACK_IMPORTED_MODULE_2__.default.main`
+    align-items: center;
+    background-color: ${({theme})=>theme.greys.grey};
+    border-radius: 0 0 var(--border-radius) var(--border-radius);
+    display: flex;
+    height: 150px;
+    justify-content: center;
+    width: 100%;
+`,Normal=()=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.Fragment,{children:[(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_equisoft_design_elements_react__WEBPACK_IMPORTED_MODULE_0__.SkipLink,{href:"#main"}),(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(Navigation,{children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("p",{tabIndex:0,children:"Navigation"})}),(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(Main,{id:"main",children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("p",{tabIndex:0,children:"Main content"})})]});Normal.parameters={...Normal.parameters,docs:{...Normal.parameters?.docs,source:{originalSource:'() => <>\n        <SkipLink href="#main" />\n        <Navigation>\n            <p tabIndex={0}>Navigation</p>\n        </Navigation>\n        <Main id="main">\n            <p tabIndex={0}>Main content</p>\n        </Main>\n    </>',...Normal.parameters?.docs?.source}}};const __namedExportsOrder=["Normal"];try{Normal.displayName="Normal",Normal.__docgenInfo={description:"",displayName:"Normal",props:{}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["stories/skip-link.stories.tsx#Normal"]={docgenInfo:Normal.__docgenInfo,name:"Normal",path:"stories/skip-link.stories.tsx#Normal"})}catch(__react_docgen_typescript_loader_error){}}}]);
