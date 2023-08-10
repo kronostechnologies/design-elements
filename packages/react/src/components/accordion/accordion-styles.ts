1a72f9ba7c9c89985bad6a2753aeebbeb5384fea
carlafranca <carla.franca@equisoft.com>
2023-08-10 15:46:54 -0400

    feat: moved style to styles file for readability

--- packages/react/src/components/accordion/accordion-styles.ts
+++ packages/react/src/components/accordion/accordion-styles.ts
@@ -1,6 +1,45 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Theme } from '../../themes';
 
 export const StyledAccordionGroup = styled.div`
     display: flex;
     flex-direction: column;
+`;
+
+export const AccordionSectionStyled = (isExpanded: boolean, theme: Theme) => css`
+    min-height: 0;
+    overflow: hidden;
+    height: ${isExpanded ? 'auto' : '0'}; 
+    border: ${isExpanded ? `1px solid ${theme.greys.black}` : '0'};
+    border-top-width: 0;
+    margin-bottom: 8px;
+    border-radius: 0 0 4px 4px;
+    padding: ${isExpanded ? '16px 24px 24px 40px' : '0'};
+    background: ${theme.greys['colored-white']};
+    transition: height 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0ms; 
+    font-size: 0.75rem;
+    line-height: 1.5;
+    letter-spacing: 0.24px;
+    font-weight: 400;
+`;
+
+export const HeadingStyled = (isExpanded: boolean, theme: Theme) => css`
+    border: 1px solid ${theme.greys.black};
+    border-bottom-color: ${isExpanded ? theme.greys.grey : theme.greys.black};
+    border-radius: ${isExpanded ? '4px 4px 0px 0px' : '4px'};
+`;
+
+export const ButtonStyled = (isExpanded: boolean, theme: Theme) => css`
+    width: 100%;
+    border-radius: ${isExpanded ? '4px 4px 0px 0px' : '4px'};
+    padding: 8px 24px 8px 8px;
+    justify-content: start;
+    &[aria-expanded="true"]{
+        background: ${theme.greys.white};
+    }
+    > svg {
+        width: 16px;
+        height: 16px;
+        margin-right: 8px;
+    }
 `;
--- packages/react/src/components/accordion/accordion.tsx
+++ packages/react/src/components/accordion/accordion.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AccordionProps, AccordionItemProps  } from './accordion-types';
+import { AccordionSectionStyled,  HeadingStyled, ButtonStyled } from './accordion-styles';
 import { Button } from '../buttons/button';
 import { Icon } from '../icon/icon';
 import { Heading } from '../heading/heading';
@@ -35,51 +36,24 @@ export const Accordion: React.FC<AccordionProps> = ({
         );
 };
 
+
 const AccordionSection = styled.section<{ isExpanded: boolean }>`
-    min-height: 0;
-    overflow: hidden;
-    height: ${props => (props.isExpanded ? 'auto' : '0')}; 
-    border: ${props => (props.isExpanded ? `1px solid ${props.theme.greys.black}` : '0')};
-    border-top-width: 0;
-    margin-bottom: 8px;
-    border-radius: 0 0 4px 4px;
-    padding: ${props => (props.isExpanded ? '16px 24px 24px 40px' : '0')};
-    background: ${(props) => props.theme.greys['colored-white']};
-    transition: height 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0ms; 
-    font-size: 0.75rem;
-    line-height: 1.5;
-    letter-spacing: 0.24px;
-    font-weight: 400;
-`;
-
+  ${({ isExpanded, theme }) => AccordionSectionStyled(isExpanded, theme)};
+`
 const StyledHeading = styled(Heading)<{ isExpanded: boolean }>`
-    border: 1px solid ${(props) => props.theme.greys.black};
-    border-bottom-color: ${props => (props.isExpanded ? props.theme.greys.grey : props.theme.greys.black)};
-    border-radius: ${props => (props.isExpanded ? '4px 4px 0px 0px' : '4px')};
-`;
-
+  ${({ isExpanded, theme }) => HeadingStyled(isExpanded, theme)};
+`
 const StyledButton = styled(Button)<{ isExpanded: boolean }>`
-    width: 100%;
-    border-radius: ${props => (props.isExpanded ? '4px 4px 0px 0px' : '4px')};
-    padding: 8px 24px 8px 8px;
-    justify-content: start;
-    &[aria-expanded="true"]{
-        background: ${(props) => props.theme.greys.white};
-    }
-    > svg {
-        width: 16px;
-        height: 16px;
-        margin-right: 8px;
-    }
-`;
+${({ isExpanded, theme }) => ButtonStyled(isExpanded, theme)};
+`
 
 const AccordionItem: React.FC<AccordionItemProps> = ({
     headerId,
     panelId,
     title,
     type,
-    isExpanded = false, // Default to false if not provided
-    onToggle = () => {}, // Empty function if not provided
+    isExpanded = false, 
+    onToggle = () => {}, 
     children,
     ...props
   }) => {
