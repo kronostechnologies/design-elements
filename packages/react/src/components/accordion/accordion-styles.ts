12e6c75bef45810ccfd39d5df9d4abfe40769dbd
carlafranca <carla.franca@equisoft.com>
2023-08-18 16:36:17 -0400

    fix: style formatting

--- packages/react/src/components/accordion/accordion-styles.ts
+++ packages/react/src/components/accordion/accordion-styles.ts
@@ -7,23 +7,20 @@ export const StyledAccordionGroup = styled.div`
 `;
 
 export const AccordionSectionStyled = (isExpanded: boolean, theme: Theme) => css`
-   
     border: ${isExpanded ? `1px solid ${theme.greys['dark-grey']}` : '0'};
     border-top-width: 0;
-    margin-bottom: 8px;
     border-radius: 0 0 4px 4px;
     background: ${theme.greys['colored-white']};
-    
-    color: #1B1C1E;
+    color: #1b1c1e;
     font-size: 0.75rem;
     font-weight: 400;
-    line-height: 1.7;
+    height: ${isExpanded ? 'auto' : '0'}; 
     letter-spacing: 0.24px;
-   
-    padding: ${isExpanded ? '16px 24px 24px 40px' : '0'};
+    line-height: 1.7;
+    margin-bottom: 8px;
     min-height: 0;
     overflow: hidden;
-    height: ${isExpanded ? 'auto' : '0'}; 
+    padding: ${isExpanded ? '16px 24px 24px 40px' : '0'};
     transition: height 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0ms; 
 `;
 
@@ -34,22 +31,21 @@ export const HeadingStyled = (isExpanded: boolean, theme: Theme) => css`
 `;
 
 export const ButtonStyled = (isExpanded: boolean, theme: Theme) => css`
-    text-transform: none;
+    border-radius: ${ isExpanded ? '4px 4px 0px 0px' : '4px' };
     font-size: .875rem;
-    line-height: 24px;
+    text-transform: none;
     font-weight: 400;
-    letter-spacing: 0.24px;
-    width: 100%;
-    border-radius: ${isExpanded ? '4px 4px 0px 0px' : '4px'};
-    padding: 8px;
     justify-content: start;
-    &[aria-expanded="true"]{
+    letter-spacing: 0.24px;
+    line-height: 24px;
+    padding: 8px;
+    width: 100%;
+    &[aria-expanded="true"] {
         background: ${theme.greys.white};
     }
-
     > svg {
         color: ${theme.greys['dark-grey']};
-        padding: 4px;
         margin-right: 4px;
+        padding: 4px;
     }
 `;
