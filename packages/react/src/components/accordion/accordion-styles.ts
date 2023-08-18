e9201829f5ed682242b8263641be8f34a2084ea8
carlafranca <carla.franca@equisoft.com>
2023-08-18 17:27:36 -0400

    fix: style properties order alphabetically

--- packages/react/src/components/accordion/accordion-styles.ts
+++ packages/react/src/components/accordion/accordion-styles.ts
@@ -7,21 +7,21 @@ export const StyledAccordionGroup = styled.div`
 `;
 
 export const AccordionSectionStyled = (isExpanded: boolean, theme: Theme) => css`
-    border: ${isExpanded ? `1px solid ${theme.greys['dark-grey']}` : '0'};
-    border-top-width: 0;
-    border-radius: 0 0 4px 4px;
     background: ${theme.greys['colored-white']};
+    border: ${isExpanded ? `1px solid ${theme.greys['dark-grey']}` : '0'};
+    border-radius: 0 0 4px 4px;
+    border-top-width: 0;
     color: #1b1c1e;
     font-size: 0.75rem;
     font-weight: 400;
-    height: ${isExpanded ? 'auto' : '0'}; 
+    height: ${isExpanded ? 'auto' : '0'};
     letter-spacing: 0.24px;
     line-height: 1.7;
     margin-bottom: 8px;
     min-height: 0;
     overflow: hidden;
     padding: ${isExpanded ? '16px 24px 24px 40px' : '0'};
-    transition: height 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0ms; 
+    transition: height 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0ms;
 `;
 
 export const HeadingStyled = (isExpanded: boolean, theme: Theme) => css`
@@ -32,15 +32,15 @@ export const HeadingStyled = (isExpanded: boolean, theme: Theme) => css`
 
 export const ButtonStyled = (isExpanded: boolean, theme: Theme) => css`
     border-radius: ${ isExpanded ? '4px 4px 0px 0px' : '4px' };
-    font-size: .875rem;
-    text-transform: none;
+    font-size: 0.875rem;
     font-weight: 400;
     justify-content: start;
     letter-spacing: 0.24px;
     line-height: 24px;
     padding: 8px;
+    text-transform: none;
     width: 100%;
-    &[aria-expanded="true"] {
+    &[aria-expanded='true'] {
         background: ${theme.greys.white};
     }
     > svg {
