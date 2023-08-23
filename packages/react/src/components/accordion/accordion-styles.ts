72e911b26ffc4683ccc91d812f281ac19107f0cc
carlafranca <carla.franca@equisoft.com>
2023-08-23 11:25:21 -0400

    Fix: issues with styling and used variables

--- packages/react/src/components/accordion/accordion-styles.ts
+++ packages/react/src/components/accordion/accordion-styles.ts
@@ -9,43 +9,51 @@ export const StyledAccordionGroup = styled.div`
 export const AccordionSectionStyled = (isExpanded: boolean, theme: Theme) => css`
     background: ${theme.greys['colored-white']};
     border: ${isExpanded ? `1px solid ${theme.greys['dark-grey']}` : '0'};
-    border-radius: 0 0 4px 4px;
+    border-radius: 0 0 var(--border-radius) var(--border-radius);
     border-top-width: 0;
-    color: #1b1c1e;
+    color: ${theme.greys['neutral-100']};
     font-size: 0.75rem;
-    font-weight: 400;
+    font-weight: var(--font-normal);
     height: ${isExpanded ? 'auto' : '0'};
-    letter-spacing: 0.24px;
+    letter-spacing: 0.015rem;
     line-height: 1.7;
-    margin-bottom: 8px;
+    margin-bottom: var(--spacing-1x);
     min-height: 0;
     overflow: hidden;
-    padding: ${isExpanded ? '16px 24px 24px 40px' : '0'};
+    padding: ${isExpanded ? 'var(--spacing-2x) var(--spacing-3x) var(--spacing-3x) var(--spacing-5x)' : '0'};
     transition: height 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0ms;
 ` as any;
 
 export const HeadingStyled = (isExpanded: boolean, theme: Theme) => css`
     border: 1px solid ${theme.greys['dark-grey']};
     border-bottom-color: ${isExpanded ? theme.greys.grey : theme.greys['dark-grey']};
-    border-radius: ${isExpanded ? '4px 4px 0px 0px' : '4px'};
+    border-radius: ${isExpanded ? 'var(--border-radius) var(--border-radius) 0 0' : 'var(--border-radius)'};
+    position: relative;
 ` as any;
 
-export const ButtonStyled = (isExpanded: boolean, theme: Theme) => css`
-    border-radius: ${isExpanded ? '4px 4px 0px 0px' : '4px'};
+export const ButtonStyled = (isExpanded: boolean, theme: Theme, isMobile: boolean) => css`
+    align-items: flex-start;
+    border-radius: ${isExpanded ? 'var(--border-radius) var(--border-radius) 0 0' : 'var(--border-radius)'};
+    color: ${theme.greys['neutral-100']};
     font-size: 0.875rem;
-    font-weight: 400;
+    font-weight: var(--font-normal);
+    letter-spacing: 0.015rem;
+    line-height: ${isMobile ? 2 : 1.5 };
     justify-content: start;
-    letter-spacing: 0.24px;
-    line-height: 24px;
-    padding: 8px;
+    padding: ${isMobile ? 'var(--spacing-1x) var(--spacing-3x) var(--spacing-1x)  var(--spacing-5halfx)' : 'var(--spacing-1x) var(--spacing-1x) var(--spacing-1x) var(--spacing-4halfx)'}; 
+    text-align: left;
     text-transform: none;
     width: 100%;
+    &:hover,
     &[aria-expanded='true'] {
+        color: ${theme.greys['neutral-100']};
         background: ${theme.greys.white};
     }
     > svg {
-        color: ${theme.greys['dark-grey']};
-        margin-right: 4px;
-        padding: 4px;
+        color: ${theme.greys['neutral-100']};
+        left: var(--spacing-1x);
+        padding: var(--spacing-half);
+        position: absolute;
+        top: var(--spacing-1x);
     }
 ` as any;
--- packages/react/src/components/accordion/accordion.tsx
+++ packages/react/src/components/accordion/accordion.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { AccordionProps } from './accordion-types';
 import { AccordionSectionStyled, HeadingStyled, ButtonStyled } from './accordion-styles';
+import { useDeviceContext } from '../device-context-provider/device-context-provider';
 import { Button } from '../buttons/button';
 import { Icon } from '../icon/icon';
 import { Heading } from '../heading/heading';
@@ -14,8 +15,8 @@ const StyledHeading = styled(Heading)<{ isExpanded: boolean }>`
     ${({ isExpanded, theme }) => HeadingStyled(isExpanded, theme)};
 `;
 
-const StyledButton = styled(Button)<{ isExpanded: boolean }>`
-    ${({ isExpanded, theme }) => ButtonStyled(isExpanded, theme)};
+const StyledButton = styled(Button)<{ isExpanded: boolean, isMobile: boolean }>`
+    ${({ isExpanded, theme, isMobile }) => ButtonStyled(isExpanded, theme, isMobile)};
 `;
 
 export const Accordion: React.FC<AccordionProps> = ({
@@ -33,23 +34,25 @@ export const Accordion: React.FC<AccordionProps> = ({
     const headerId = `idHeader${id}`;
     const panelId = `idPanel${id}`;
     const { disabled: isDisabled = false } = {};
+    const { isMobile } = useDeviceContext();
+
     return (
         <>
-            <StyledHeading className="accordion-header" type={type} tag={tag} isExpanded={isExpanded}>
+            <StyledHeading className="accordion-header" type={type} tag={tag} isExpanded={isExpanded} noMargin>
                 <StyledButton
                     id={headerId}
                     className="accordion-button"
                     buttonType="tertiary"
                     label={title}
+                    isMobile={isMobile}
                     aria-expanded={isExpanded}
                     aria-controls={panelId}
                     onClick={() => onToggle()}
                     isExpanded={isExpanded}
                     disabled={disabled}
                     onKeyDown={onKeyDown}
-                    ref={buttonRef}
-                >
-                    <Icon name={isExpanded ? 'carretDown' : 'carretRight'} aria-hidden="true" />
+                    ref={buttonRef}>
+                        <Icon name={isExpanded ? 'carretDown' : 'carretRight'} aria-hidden="true" />
                 </StyledButton>
             </StyledHeading>
             <AccordionSection
