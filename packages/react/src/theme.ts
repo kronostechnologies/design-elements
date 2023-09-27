fca1e1df870c44159c785a773bf5f51ef47c3397
Carla Franca <infocarlafranca@gmail.com>
2023-09-26 22:54:15 -0400

    fix: add themes to ds

--- packages/react/src/theme.ts
+++ packages/react/src/theme.ts
@@ -1,2 +1,4 @@
+export { customTheme } from './themes/custom-theme';
 export { equisoftTheme } from './themes/equisoft';
 export { Theme } from './themes/theme';
+export { CustomTheme } from './themes/custom-types';
--- packages/react/src/themes/index.ts
+++ packages/react/src/themes/index.ts
@@ -1,2 +1,4 @@
+export * from './custom-theme';
 export * from './equisoft';
 export * from './theme';
+export * from './custom-types';
