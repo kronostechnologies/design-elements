d88242505f859798340360f1e6a421d77f68993c
Carla Franca <infocarlafranca@gmail.com>
2023-10-03 16:27:25 -0400

    fix: add addon-controls to storybook

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -30,6 +30,7 @@ const config: StorybookConfig = {
     ],
     addons: [
         '@storybook/addon-docs',
+        '@storybook/addon-controls'
     ],
     webpackFinal: async (config) => ({
         ...config,
