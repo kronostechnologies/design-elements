1cbfe988f258146dbebb58857c5cbd5767f0b17a
Alexandre Brillant <alexandre.brillant@equisoft.com>
2023-12-20 12:08:53 -0500

    feat(Button): add secondary-destructive variant (#689)
    * feat(Button): add secondary-destructive variant
    
    * feat(IconButton): add destructive-secondary stories

--- packages/react/src/components/buttons/abstract-button.tsx
+++ packages/react/src/components/buttons/abstract-button.tsx
@@ -75,7 +75,7 @@ export const AbstractButton = forwardRef<HTMLButtonElement, PropsWithChildren<Ab
 
 AbstractButton.displayName = 'AbstractButton';
 
-type ButtonType = 'primary' | 'secondary' | 'tertiary' | 'destructive';
+export type ButtonType = 'primary' | 'secondary' | 'tertiary' | 'destructive' | 'destructive-secondary';
 
 export interface ButtonTypeStyles {
     buttonType: ButtonType;
@@ -179,6 +179,36 @@ const getDestructiveButtonStyles: (props: ButtonTypeStyles) => FlattenInterpolat
     }
 `;
 
+const getDestructiveSecondaryButtonStyles: (props: ButtonTypeStyles) => FlattenInterpolation<ThemeProps<Theme>> = ({
+    inverted,
+    theme,
+}) => css`
+    background-color: ${inverted ? 'transparent' : theme.greys.white};
+    border-color: ${inverted ? theme.greys.white : theme.notifications['alert-2.1']};
+    color: ${inverted ? theme.greys.white : theme.notifications['alert-2.1']};
+
+    &:hover,
+    &[aria-expanded='true'] {
+        /* TODO change colors when updating thematization */
+        background-color: ${inverted ? '#7B1A15' : theme.greys.white};
+        border-color: ${inverted ? theme.greys.white : '#7B1A15'};
+        color: ${inverted ? theme.greys.white : '#7B1A15'};
+    }
+
+    &:disabled {
+        color: ${inverted ? theme.greys.white : '#F99D99'};
+
+        &,
+        &:focus,
+        &:hover {
+            /* TODO change colors when updating thematization */
+            background-color: ${inverted ? '#F99D99' : theme.greys.white};
+            border-color: ${inverted ? theme.greys.white : '#F99D99'};
+            color: ${inverted ? theme.greys.white : '#F99D99'};
+        }
+    }
+`;
+
 export const getButtonTypeStyles: (props: ButtonTypeStyles) => FlattenInterpolation<ThemeProps<Theme>> = (props) => css`
     ${focus(props, true)};
     ${() => {
@@ -191,6 +221,8 @@ export const getButtonTypeStyles: (props: ButtonTypeStyles) => FlattenInterpolat
                 return getTertiaryButtonStyles(props);
             case 'destructive':
                 return getDestructiveButtonStyles(props);
+            case 'destructive-secondary':
+                return getDestructiveSecondaryButtonStyles(props);
         }
     }}
 `;
