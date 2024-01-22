394d4c39ca1f837770b6495b1beacb7cba521da4
Savut Sang <savut.sang@equisoft.com>
2024-01-22 16:02:35 -0500

    chore: doc

--- packages/storybook/stories/table.stories.tsx
+++ packages/storybook/stories/table.stories.tsx
@@ -1101,12 +1101,12 @@ interface OptimizationData {
 
 /**
  * The table will always update the table with the new data and columns when React is re-rendering this component.
- * But the important key here is if you pass a different **columns** object, the Table will UNMOUNT and RE-MOUNT
+ * But the important key here is if you pass a different **columns** object, the table will UNMOUNT and RE-MOUNT
  * every cell (instead of the usual React update). So it is very important to memoize the columns object (ex: using
  * useMemo) and reuse it as much as possible.
  *
- * If your memoized columns need to be recreated because it has dependencies, you could pass these dependencies via
- * useRef instead. This way you will always retrieve the current value of that variable.
+ * If you are recreating the memoized columns just to update a dependency, you could pass these dependencies via useRef
+ * instead. This way you will always retrieve the current value of that variable.
  */
 export const Optimization: Story = () => {
     const [data, setData] = useState<OptimizationData[]>([
