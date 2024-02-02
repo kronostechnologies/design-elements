439077e3abbbbd045b03ccbb69f40b2d552fa6aa
Alexandre Brillant <alexandre.brillant@equisoft.com>
2024-02-02 13:54:14 -0500

    feat(Table): add aria-label warning for headers without text

--- packages/react/src/components/table/table-header.tsx
+++ packages/react/src/components/table/table-header.tsx
@@ -83,6 +83,12 @@ function getHeading<TData extends object, TValue>(
         sortState = 'descending';
     }
 
+    if (header.column.columnDef.header && !header.column.columnDef.headerAriaLabel) {
+        console.error(
+            `headerAriaLabel missing for column ${header.id}. aria label is required for headers without text.`
+        );
+    }
+
     if (!header.column.columnDef.sortable) {
         return (
             <StyledHeader
