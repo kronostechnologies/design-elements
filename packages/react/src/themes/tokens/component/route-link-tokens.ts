250dccfc551dae462032c319ba4f58bb8f659c5b
matique77 <mathieu.morin@equisoft.com>
2024-02-13 09:37:53 -0500

    feat(route-link,themes): add route-link design tokens

---
+++ packages/react/src/themes/tokens/component/route-link-tokens.ts
@@ -0,0 +1,19 @@
+import { AliasTokens } from '../alias-tokens';
+import { RefTokens } from '../ref-tokens';
+
+export type RouteLinkTokens =
+    | 'route-link-text-color'
+    | 'route-link-disabled-text-color'
+    | 'route-link-hover-text-color'
+
+export type RouteLinkTokenValue = AliasTokens | RefTokens;
+
+export type RouteLinkTokenMap = {
+    [Token in RouteLinkTokens]: RouteLinkTokenValue;
+};
+
+export const defaultRouteLinkTokens: RouteLinkTokenMap = {
+    'route-link-text-color': 'color-informative-50',
+    'route-link-disabled-text-color': 'color-informative-20',
+    'route-link-hover-text-color': 'color-informative-70',
+};
