df8987125845209e4de5b622e289fd299338c88b
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-02-26 09:37:24 -0500

    fix: removing unused focus border tokens

--- packages/react/src/themes/tokens/component/focus-tokens.ts
+++ packages/react/src/themes/tokens/component/focus-tokens.ts
@@ -5,12 +5,7 @@ export type FocusTokens =
     | 'focus-border-color'
     | 'focus-inverted-border-color'
     | 'focus-box-shadow-color'
-    | 'focus-inverted-box-shadow-color'
-    | 'focus-box-shadow-inset-color'
-    | 'focus-border-box-shadow-color-1'
-    | 'focus-border-box-shadow-color-2'
-    | 'focus-border-box-shadow-inset-color-1'
-    | 'focus-border-box-shadow-inset-color-2';
+    | 'focus-inverted-box-shadow-color';
 
 export type FocusTokenValue = AliasTokens | RefTokens;
 
@@ -23,9 +18,4 @@ export const defaultFocusTokens: FocusTokenMap = {
     'focus-inverted-border-color': 'color-brand-50',
     'focus-box-shadow-color': 'color-brand-50',
     'focus-inverted-box-shadow-color': 'color-brand-20',
-    'focus-box-shadow-inset-color': 'color-brand-20',
-    'focus-border-box-shadow-color-1': 'color-brand-50',
-    'focus-border-box-shadow-color-2': 'color-brand-20',
-    'focus-border-box-shadow-inset-color-1': 'color-brand-20',
-    'focus-border-box-shadow-inset-color-2': 'color-brand-50',
 };
