f78103f20c3f0b25ef5cf3bc933634f8d2ba2229
Alexandre Brillant <alexandre.brillant@equisoft.com>
2024-02-27 16:25:21 -0500

    feat(GlobalHeader): fix global-header-logo-title-separator-color token

--- packages/react/src/components/global-header/global-header.tsx
+++ packages/react/src/components/global-header/global-header.tsx
@@ -52,7 +52,7 @@ const HtmlLink = styled.a.attrs({ 'aria-label': 'Home' })`
 `;
 
 const StyledSpan = styled.span`
-    border-left: 1px solid ${({ theme }) => theme.component['global-header-logo-content-span-border-color']};
+    border-left: 1px solid ${({ theme }) => theme.component['global-header-logo-title-separator-color']};
     color: ${({ theme }) => theme.component['global-header-logo-content-text-color']};
     font-size: 1rem;
     font-weight: var(--font-normal);
