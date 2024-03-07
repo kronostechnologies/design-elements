459dccbb5dc83fb78e48bfb06941e3d7e406ac3c
Alexandre Brillant <alexandre.brillant@equisoft.com>
2024-03-07 17:10:13 -0500

    feat(ItemContent): add color tokens

---
+++ packages/react/src/themes/tokens/component/item-content-tokens.ts
@@ -0,0 +1,19 @@
+import { AliasTokens } from '../alias-tokens';
+import { RefTokens } from '../ref-tokens';
+
+export type ItemContentTokens =
+    | 'item-content-icon-background-color'
+    | 'item-content-icon-border-color'
+    | 'item-content-description-color';
+
+export type ItemContentTokenValue = AliasTokens | RefTokens;
+
+export type ItemContentTokenMap = {
+    [Token in ItemContentTokens]: ItemContentTokenValue;
+};
+
+export const defaultItemContentTokens: ItemContentTokenMap = {
+    'item-content-icon-background-color': 'color-neutral-05',
+    'item-content-icon-border-color': 'color-neutral-15',
+    'item-content-description-color': 'color-neutral-65'
+};
