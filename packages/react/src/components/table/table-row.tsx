75a2c5553c9464c7af653a8ce1f3b060155873c7
Alexandre Brillant <alexandre.brillant@equisoft.com>
2024-03-12 22:27:24 -0400

    feat(TableRow): rename table-error-row-border-color token

--- packages/react/src/components/table/table-row.tsx
+++ packages/react/src/components/table/table-row.tsx
@@ -103,7 +103,7 @@ const StyledTableRow = styled.tr<StyledTableRowProps>`
         position: relative;
 
         &::after {
-            box-shadow: inset 0 0 0 1px ${theme.component['table-row-shadow-color']};
+            box-shadow: inset 0 0 0 1px ${theme.component['table-error-row-border-color']};
             content: '';
             height: calc(100% + 1px);
             left: 0;
@@ -114,7 +114,7 @@ const StyledTableRow = styled.tr<StyledTableRowProps>`
         }
 
         td:first-child::after {
-            border-left: 1px solid ${theme.component['table-row-shadow-color']};
+            border-left: 1px solid ${theme.component['table-error-row-border-color']};
             content: '';
             height: 100%;
             left: 0;
