58da05e9d0ae2be5eaa838e416962791e11bd3ee
Alexandre Brillant <alexandre.brillant@equisoft.com>
2024-03-18 14:59:14 -0400

    feat(Table): re-order color tokens

--- packages/react/src/themes/tokens/component/table-tokens.ts
+++ packages/react/src/themes/tokens/component/table-tokens.ts
@@ -4,10 +4,10 @@ import { RefTokens } from '../ref-tokens';
 export type TableTokens =
     | 'table-text-color'
     | 'table-background-color'
+    | 'table-cell-hover-background-color'
     | 'table-cell-number-text-color'
     | 'table-row-selected-background-color'
     | 'table-row-error-background-color'
-    | 'table-cell-hover-background-color'
     | 'table-row-border-color'
     | 'table-row-odd-background-color'
     | 'table-row-error-border-color';
@@ -21,10 +21,10 @@ export type TableTokenMap = {
 export const defaultTableTokens: TableTokenMap = {
     'table-text-color': 'color-neutral-90',
     'table-background-color': 'color-white',
+    'table-cell-hover-background-color': 'color-neutral-15',
     'table-cell-number-text-color': 'color-neutral-65',
     'table-row-selected-background-color': 'color-brand-05',
     'table-row-error-background-color': 'color-alert-05',
-    'table-cell-hover-background-color': 'color-neutral-15',
     'table-row-border-color': 'color-neutral-15',
     'table-row-odd-background-color': 'color-neutral-02',
     'table-row-error-border-color': 'color-alert-50',
