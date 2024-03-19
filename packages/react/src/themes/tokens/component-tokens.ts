ff4e4720f05f99b34f30686e07254995370d8f3d
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-03-19 08:35:22 -0400

    feat(lozenge)!: update lozenge variants (#769)

--- packages/react/src/themes/tokens/component-tokens.ts
+++ packages/react/src/themes/tokens/component-tokens.ts
@@ -8,6 +8,7 @@ import { defaultFocusTokens, FocusTokens } from './component/focus-tokens';
 import { defaultHeadingTokens, HeadingTokens } from './component/heading-tokens';
 import { defaultLabelTokens, LabelTokens } from './component/label-tokens';
 import { defaultLegendTokens, LegendTokens } from './component/legend-tokens';
+import { defaultLozengeTokens, LozengeTokens } from './component/lozenge-tokens';
 import { defaultNavListTokens, NavListTokens } from './component/nav-list-tokens';
 import { defaultNavListItemTokens, NavListItemTokens } from './component/nav-list-item-tokens';
 import { defaultPaginationTokens, PaginationTokens } from './component/pagination-tokens';
@@ -61,7 +62,8 @@ export type ComponentTokens =
     | ModalTokens
     | TooltipTokens
     | ToggleTipTokens
-    | TabTokens;
+    | TabTokens
+    | LozengeTokens;
 
 export type ComponentTokenValue = AliasTokens | RefTokens;
 
@@ -101,6 +103,7 @@ export const defaultComponentTokens: ComponentTokenMap = {
     ...defaultTooltipTokens,
     ...defaultToggleTipTokens,
     ...defaultTabTokens,
+    ...defaultLozengeTokens,
 };
 
 export type ResolvedComponentTokenValue = RefTokenValue;
