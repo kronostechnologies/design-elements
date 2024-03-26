c10849a1ba29fcba6e208cfcf1955f838d8fee21
Genevieve Morency <genevieve.morency@equisoft.com>
2024-03-26 10:30:52 -0400

    docs(storybook): update lozenge

--- packages/storybook/stories/lozenge.stories.tsx
+++ packages/storybook/stories/lozenge.stories.tsx
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 import { MobileDecorator } from './utils/device-context-decorator';
 
 export default {
-    title: 'Components/Lozenge',
+    title: 'Components/Lozenge/Stories',
     component: Lozenge,
 };
 
-export const Normal: Story = () => (
+export const Default: Story = () => (
     <Lozenge>This is to highlight some text</Lozenge>
 );
 
@@ -23,47 +23,67 @@ const VariantsContainer = styled.div`
     gap: 1rem;
 `;
 
-export const Variant: Story = () => (
+export const Neutral: Story = () => (
     <>
         <VariantsContainer>
-            <Lozenge>default</Lozenge>
             <Lozenge variant="neutral">neutral</Lozenge>
-            <Lozenge variant="success">success</Lozenge>
-            <Lozenge variant="alert">alert</Lozenge>
-            <Lozenge variant="warning">warning</Lozenge>
-            <Lozenge variant="info">info</Lozenge>
-            <Lozenge variant="discovery">discovery</Lozenge>
-        </VariantsContainer>
-
-        <VariantsContainer>
-            <Lozenge subtle>default</Lozenge>
             <Lozenge subtle variant="neutral">neutral</Lozenge>
-            <Lozenge subtle variant="success">success</Lozenge>
-            <Lozenge subtle variant="alert">alert</Lozenge>
-            <Lozenge subtle variant="warning">warning</Lozenge>
-            <Lozenge subtle variant="info">info</Lozenge>
-            <Lozenge subtle variant="discovery">discovery</Lozenge>
+            <Lozenge icon="star" variant="neutral">neutral</Lozenge>
+            <Lozenge subtle icon="star" variant="neutral">neutral</Lozenge>
         </VariantsContainer>
     </>
 );
 
-export const WithIcon: Story = () => (
+export const Success: Story = () => (
     <>
         <VariantsContainer>
-            <Lozenge icon="star" variant="neutral">neutral</Lozenge>
+            <Lozenge variant="success">success</Lozenge>
+            <Lozenge subtle variant="success">success</Lozenge>
             <Lozenge icon="check" variant="success">success</Lozenge>
-            <Lozenge icon="alertOctagon" variant="alert">alert</Lozenge>
-            <Lozenge icon="alertTriangle" variant="warning">warning</Lozenge>
-            <Lozenge icon="info" variant="info">info</Lozenge>
-            <Lozenge icon="star" variant="discovery">discovery</Lozenge>
-        </VariantsContainer>
-
-        <VariantsContainer>
-            <Lozenge subtle icon="star" variant="neutral">neutral</Lozenge>
             <Lozenge subtle icon="check" variant="success">success</Lozenge>
+        </VariantsContainer>
+    </>
+);
+
+export const Alert: Story = () => (
+    <>
+        <VariantsContainer>
+            <Lozenge variant="alert">alert</Lozenge>
+            <Lozenge subtle variant="alert">alert</Lozenge>
+            <Lozenge icon="alertOctagon" variant="alert">alert</Lozenge>
             <Lozenge subtle icon="alertOctagon" variant="alert">alert</Lozenge>
+        </VariantsContainer>
+    </>
+);
+
+export const Warning: Story = () => (
+    <>
+        <VariantsContainer>
+            <Lozenge variant="warning">warning</Lozenge>
+            <Lozenge subtle variant="warning">warning</Lozenge>
+            <Lozenge icon="alertTriangle" variant="warning">warning</Lozenge>
             <Lozenge subtle icon="alertTriangle" variant="warning">warning</Lozenge>
+        </VariantsContainer>
+    </>
+);
+
+export const Informative: Story = () => (
+    <>
+        <VariantsContainer>
+            <Lozenge variant="info">info</Lozenge>
+            <Lozenge subtle variant="info">info</Lozenge>
+            <Lozenge icon="info" variant="info">info</Lozenge>
             <Lozenge subtle icon="info" variant="info">info</Lozenge>
+        </VariantsContainer>
+    </>
+);
+
+export const Discovery: Story = () => (
+    <>
+        <VariantsContainer>
+            <Lozenge variant="discovery">discovery</Lozenge>
+            <Lozenge subtle variant="discovery">discovery</Lozenge>
+            <Lozenge icon="star" variant="discovery">discovery</Lozenge>
             <Lozenge subtle icon="star" variant="discovery">discovery</Lozenge>
         </VariantsContainer>
     </>
