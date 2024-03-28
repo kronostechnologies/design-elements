3208b1b53a1b1efd61ba3abddccad7b944352c09
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-03-28 10:54:03 -0400

    fix(design-tokens): another draft of alias tokens

--- packages/react/src/themes/tokens/alias-tokens.ts
+++ packages/react/src/themes/tokens/alias-tokens.ts
@@ -15,19 +15,8 @@ export type AliasTokens =
     | 'color-action-inverse-hover'
     | 'color-action-inverse-disabled'
 /**
- * BACKGROUND
+ * FEEDBACK
  */
-    | 'color-bg'
-    | 'color-bg-overlay'
-    | 'color-bg-isolated'
-    | 'color-bg-disabled'
-    | 'color-bg-empty'
-    | 'color-bg-hover'
-    | 'color-bg-selected'
-    | 'color-bg-brand-subtle'
-    | 'color-bg-brand'
-    | 'color-bg-brand-bold'
-    | 'color-bg-indicator'
     | 'color-bg-neutral-subtlest'
     | 'color-bg-neutral-subtle'
     | 'color-bg-neutral-bold'
@@ -46,11 +35,55 @@ export type AliasTokens =
     | 'color-feedback-bg-warning-bold'
     | 'color-feedback-bg-discovery-bold'
     | 'color-feedback-bg-informative-bold'
+    | 'color-feedback-border-success'
+    | 'color-feedback-border-warning'
+    | 'color-feedback-border-alert'
+    | 'color-feedback-border-discovery'
+    | 'color-feedback-border-informative'
+    | 'color-feedback-content-success'
+    | 'color-feedback-content-warning'
+    | 'color-feedback-content-alert'
+    | 'color-feedback-content-discovery'
+    | 'color-feedback-content-informative'
+/**
+ * INPUT
+ */
     | 'color-input-bg'
     | 'color-input-bg-hover'
     | 'color-input-bg-disabled'
     | 'color-input-bg-checked'
     | 'color-input-bg-selected'
+    | 'color-input-border'
+    | 'color-input-border-hover'
+    | 'color-input-border-disabled'
+    | 'color-input-border-selected'
+    | 'color-input-border-checked'
+    | 'color-input-border-error'
+    | 'color-input-content'
+    | 'color-input-content-hover'
+    | 'color-input-content-disabled'
+    | 'color-input-content-error'
+    | 'color-input-content-checked'
+    | 'color-input-content-selected'
+/**
+ * BRAND
+ */
+    | 'color-bg-brand-subtle'
+    | 'color-bg-brand'
+    | 'color-bg-brand-bold'
+    | 'color-bg-indicator'
+    | 'color-border-brand'
+    | 'color-content-brand'
+/**
+ * BACKGROUND
+ */
+    | 'color-bg'
+    | 'color-bg-overlay'
+    | 'color-bg-isolated'
+    | 'color-bg-disabled'
+    | 'color-bg-empty'
+    | 'color-bg-hover'
+    | 'color-bg-selected'
     | 'color-blanket-bg'
 /**
  * BORDER
@@ -64,18 +97,6 @@ export type AliasTokens =
     | 'color-border-empty'
     | 'color-border-selected'
     | 'color-border-inverse'
-    | 'color-border-brand'
-    | 'color-feedback-border-success'
-    | 'color-feedback-border-warning'
-    | 'color-feedback-border-alert'
-    | 'color-feedback-border-discovery'
-    | 'color-feedback-border-informative'
-    | 'color-input-border'
-    | 'color-input-border-hover'
-    | 'color-input-border-disabled'
-    | 'color-input-border-selected'
-    | 'color-input-border-checked'
-    | 'color-input-border-error'
     | 'color-border-focus-outside'
     | 'color-border-focus-inside'
     | 'color-border-focus-outside-inverse'
@@ -89,18 +110,6 @@ export type AliasTokens =
     | 'color-content-disabled'
     | 'color-content-hover'
     | 'color-content-selected'
-    | 'color-content-brand'
-    | 'color-feedback-content-success'
-    | 'color-feedback-content-warning'
-    | 'color-feedback-content-alert'
-    | 'color-feedback-content-discovery'
-    | 'color-feedback-content-informative'
-    | 'color-input-content'
-    | 'color-input-content-hover'
-    | 'color-input-content-disabled'
-    | 'color-input-content-error'
-    | 'color-input-content-checked'
-    | 'color-input-content-selected'
     | 'color-link-content'
     | 'color-link-content-visited'
     | 'color-link-content-disabled'
@@ -117,7 +126,7 @@ export type AliasTokenMap = {
 export const defaultAliasTokens: AliasTokenMap = {
     /**
      * ACTION
-     * Usage: Buttons.
+     * Use for buttons.
      * Equisoft theme has brand colors.
      * Others themes might use accent colors.
      */
@@ -132,6 +141,132 @@ export const defaultAliasTokens: AliasTokenMap = {
     'color-action-inverse-disabled': 'color-brand-50',
 
     /**
+     * FEEDBACK
+     *
+     * background
+     *  subtlest: use for sectional banners.
+     *  subtle: use for subtle lozenges.
+     *  bold: vibrant option used for global-banners, lozenges, toasts, toggle-switch, tooltips, etc.
+     *
+     * border
+     *  use for success sectional banners.
+     *
+     * content (text & icons)
+     *  use for success sectional banners, lozenges, etc.
+     *
+     */
+    /**
+     * NEUTRAL
+     *  Use for default background of neutral UI elements,
+     */
+    'color-bg-neutral-subtlest': 'color-neutral-02',
+    'color-bg-neutral-subtle': 'color-neutral-05',
+    'color-bg-neutral-bold': 'color-neutral-65',
+    // 'color-bg-neutral-hover': 'color-neutral-70',
+    // 'color-bg-neutral-disabled': 'color-neutral-20',
+    /**
+     * SUCCESS
+     *  Use for UI elements communicating a favorable outcome and success messaging.
+     */
+    'color-feedback-bg-success-subtlest': 'color-success-02',
+    'color-feedback-bg-success-subtle': 'color-success-05',
+    'color-feedback-bg-success-bold': 'color-success-50',
+    // 'color-feedback-bg-success-hover': 'color-success-70',
+    // 'color-feedback-bg-success-disabled': 'color-success-20',
+    'color-feedback-border-success': 'color-success-50',
+    'color-feedback-content-success': 'color-success-70',
+    /**
+     * ALERT
+     *  Use for UI elements communicating critical information and error messaging.
+     */
+    'color-feedback-bg-alert-subtlest': 'color-alert-02',
+    'color-feedback-bg-alert-subtle': 'color-alert-05',
+    'color-feedback-bg-alert-bold': 'color-alert-50',
+    // 'color-feedback-bg-alert-hover': 'color-alert-70',
+    // 'color-feedback-bg-alert-disabled': 'color-alert-20',
+    'color-feedback-border-alert': 'color-alert-50',
+    'color-feedback-content-alert': 'color-alert-70',
+    /**
+     * WARNING
+     *  Use for UI elements communicating caution.
+     */
+    'color-feedback-bg-warning-subtlest': 'color-warning-02',
+    'color-feedback-bg-warning-subtle': 'color-warning-05',
+    'color-feedback-bg-warning-bold': 'color-warning-50',
+    // 'color-feedback-bg-warning-hover': 'color-warning-70',
+    // 'color-feedback-bg-warning-disabled': 'color-warning-20',
+    'color-feedback-border-warning': 'color-warning-50',
+    'color-feedback-content-warning': 'color-warning-80',
+    /**
+     * DISCOVERY
+     *  Use for UI elements communicating change, something new or onboarding spotlights.
+     */
+    'color-feedback-bg-discovery-subtlest': 'color-discovery-02',
+    'color-feedback-bg-discovery-subtle': 'color-discovery-05',
+    'color-feedback-bg-discovery-bold': 'color-discovery-50',
+    // 'color-feedback-bg-discovery-hover': 'color-discovery-70',
+    // 'color-feedback-bg-discovery-disabled': 'color-discovery-20',
+    'color-feedback-border-discovery': 'color-discovery-50',
+    'color-feedback-content-discovery': 'color-discovery-70',
+    /**
+     * INFORMATIVE
+     *  Use for UI elements communicating information or something in-progress.
+     */
+    'color-feedback-bg-informative-subtlest': 'color-informative-02',
+    'color-feedback-bg-informative-subtle': 'color-informative-05',
+    'color-feedback-bg-informative-bold': 'color-informative-50',
+    // 'color-feedback-bg-informative-hover': 'color-informative-70',
+    // 'color-feedback-bg-informative-disabled': 'color-informative-20',
+    'color-feedback-border-informative': 'color-informative-50',
+    'color-feedback-content-informative': 'color-informative-70',
+
+    /**
+     * INPUT
+     *  Use for form UI elements, such as inputs, checkboxes, radio buttons, etc.
+     */
+    'color-input-bg': 'color-white',
+    'color-input-bg-hover': 'color-neutral-15',
+    'color-input-bg-disabled': 'color-neutral-05',
+    'color-input-bg-checked': 'color-brand-50',
+    'color-input-bg-selected': 'color-brand-05',
+
+    'color-input-border': 'color-neutral-65',
+    'color-input-border-hover': 'color-neutral-90',
+    'color-input-border-disabled': 'color-neutral-30',
+    'color-input-border-selected': 'color-brand-50',
+    'color-input-border-checked': 'color-brand-50',
+    'color-input-border-error': 'color-alert-50',
+
+    'color-input-content': 'color-neutral-65',
+    'color-input-content-hover': 'color-neutral-90',
+    'color-input-content-disabled': 'color-neutral-30',
+    'color-input-content-error': 'color-alert-50',
+    'color-input-content-checked': 'color-white',
+    'color-input-content-selected': 'color-brand-70',
+    // color-input-content-placeholder?
+
+    /**
+     * BRAND
+     * Use for UI elements, like visual indicators to reinforce our brand, that need to stand out a lot.
+     *
+     * background
+     *  subtle: used for legends
+     *  bold: used for global headers.
+     *
+     * border
+     *  used for global headers.
+     *
+     * content (text & icons)
+     *  used for progress trackers
+     */
+    'color-bg-brand-subtle': 'color-brand-20',
+    'color-bg-brand': 'color-brand-50',
+    'color-bg-brand-bold': 'color-brand-80',
+    'color-bg-indicator': 'color-brand-50',
+    'color-border-brand': 'color-brand-70',
+    'color-content-brand': 'color-brand-50',
+
+    /**
      * BACKGROUND
      */
 
@@ -141,12 +276,12 @@ export const defaultAliasTokens: AliasTokenMap = {
     'color-bg': 'color-white',
     /**
      * Use for the elevation surface background of elements that sit on top of other UI elements,
-     * such as modals, menus, floating toolbar, toggletips, etc.
+     * such as modals, dropdown menus, listbox, nav-list, global navigation, toggletips, etc.
      */
     'color-bg-overlay': 'color-white',
     /**
      * A secondary elevation surface background for the UI commonly used for
-     * grouped or isolated items, such as sections, accordion panels, etc.
+     * grouped or isolated items, such as sections, accordion panels, table even rows, etc.
      */
     'color-bg-isolated': 'color-neutral-02',
 
@@ -155,108 +290,32 @@ export const defaultAliasTokens: AliasTokenMap = {
      */
     // 'color-bg-inverse': 'color-neutral-65',
     // 'color-bg-inverse-hover': 'color-neutral-80',
-
     /**
      * Use for the background of elements that appear to have no background in a resting state,
      * such as subtle buttons and menu items.
      */
     // 'color-bg-subtle': 'transparent-50',
+
     /**
      * Use for backgrounds of elements in a hovered state,
-     * such as menu items, navigation elements, etc.
+     * such as menu items, navigation elements, accordion headers, card links, table cells, clickable tags, etc.
      */
     'color-bg-hover': 'color-neutral-15',
     /**
      * Use for backgrounds of elements in a disabled state,
-     * such as menu items, form elements, etc.
+     * such as menu items, navigation elements like accordion, etc.
      */
     'color-bg-disabled': 'color-neutral-05',
+    /**
+     * Use for backgrounds of elements in an empty state,
+     * such as carousel dots, progress elements, etc.
+     */
     'color-bg-empty': 'color-neutral-15',
     /**
-     * Use for the background of elements in a selected state.
+     * Use for the background of elements in a selected state,
+     * such as naviaation elements like pagination pages or items like table rows.
      */
     'color-bg-selected': 'color-brand-05',
-    // color-bg-selected-hover?
-
-    /**
-     * Use for the background of action or visual indicator elements to reinforce our brand,
-     * that need to stand out a lot.
-     */
-    'color-bg-brand-subtle': 'color-brand-20',
-    'color-bg-brand': 'color-brand-50',
-    'color-bg-brand-bold': 'color-brand-80',
-    'color-bg-indicator': 'color-bg-brand',
-
-    /**
-     * Use for default background of neutral UI elements,
-     * subtle: use for default variants of lozenge, sectionnal banners.
-     * bold: vibrant option neutral elements, such as global-banners, lozenges, toasts.
-     */
-    'color-bg-neutral-subtlest': 'color-neutral-02',
-    'color-bg-neutral-subtle': 'color-neutral-05',
-    'color-bg-neutral-bold': 'color-neutral-65',
-    // 'color-bg-neutral-hover': 'color-neutral-70',
-    // 'color-bg-neutral-disabled': 'color-neutral-20',
-    /**
-     * Use for backgrounds communicating a favorable outcome.
-     * subtle: use for success section messages.
-     * bold: vibrant option for checked toggles.
-     */
-    'color-feedback-bg-success-subtlest': 'color-success-02',
-    'color-feedback-bg-success-subtle': 'color-success-05',
-    'color-feedback-bg-success-bold': 'color-success-50',
-    // 'color-feedback-bg-success-hover': 'color-success-70',
-    // 'color-feedback-bg-success-disabled': 'color-success-20',
-    /**
-     * Use for backgrounds communicating critical information.
-     * subtle: use for error section messages.
-     * bold: vibrant option for danger buttons and error banners.
-     */
-    'color-feedback-bg-alert-subtlest': 'color-alert-02',
-    'color-feedback-bg-alert-subtle': 'color-alert-05',
-    'color-feedback-bg-alert-bold': 'color-alert-50',
-    // 'color-feedback-bg-alert-hover': 'color-alert-70',
-    // 'color-feedback-bg-alert-disabled': 'color-alert-20',
-    /**
-     * Use for backgrounds communicating caution.
-     * subtle: use for warning buttons and warning banners.
-     * bold: vibrant option for danger buttons and error banners.
-     */
-    'color-feedback-bg-warning-subtlest': 'color-warning-02',
-    'color-feedback-bg-warning-subtle': 'color-warning-05',
-    'color-feedback-bg-warning-bold': 'color-warning-50',
-    // 'color-feedback-bg-warning-hover': 'color-warning-70',
-    // 'color-feedback-bg-warning-disabled': 'color-warning-20',
-    /**
-     * Use for backgrounds communicating change or something new
-     * subtle: use for discovery section messages.
-     * bold: vibrant option for onboarding spotlights.
-     */
-    'color-feedback-bg-discovery-subtlest': 'color-discovery-02',
-    'color-feedback-bg-discovery-subtle': 'color-discovery-05',
-    'color-feedback-bg-discovery-bold': 'color-discovery-50',
-    // 'color-feedback-bg-discovery-hover': 'color-discovery-70',
-    // 'color-feedback-bg-discovery-disabled': 'color-discovery-20',
-    /**
-     * Use for backgrounds communicating information or something in-progress.
-     * subtle: use for information section messages.
-     * bold: vibrant option.
-     */
-    'color-feedback-bg-informative-subtlest': 'color-informative-02',
-    'color-feedback-bg-informative-subtle': 'color-informative-05',
-    'color-feedback-bg-informative-bold': 'color-informative-50',
-    // 'color-feedback-bg-informative-hover': 'color-informative-70',
-    // 'color-feedback-bg-informative-disabled': 'color-informative-20',
-
-    /**
-     * Use for background of form UI elements, such as inputs, checkboxes, and radio buttons.
-     * Use selected for the backgrounds of inputs in a selected state, such as checkboxes and radio buttons.
-     */
-    'color-input-bg': 'color-white',
-    'color-input-bg-hover': 'color-neutral-15',
-    'color-input-bg-disabled': 'color-neutral-05',
-    'color-input-bg-checked': 'color-brand-50',
-    'color-input-bg-selected': 'color-brand-05',
 
     /**
      * Use for the screen overlay that appears with modal dialogs
@@ -289,50 +348,16 @@ export const defaultAliasTokens: AliasTokenMap = {
      * Use for borders of elements in a disabled state.
      */
     'color-border-disabled': 'color-neutral-15',
+    /**
+     * Use for borders of elements in an empty state,
+     * such as progress elements, etc.
+     */
     'color-border-empty': 'color-neutral-30',
     /**
      * Use for borders or visual indicators of elements in a selected, opened or active state,
-     * such as in tabs or menu items.
+     * such as navigation elements like tabs, pagination or menu items.
      */
     'color-border-selected': 'color-brand-50',
-    // 'color-border-selected-hover' ?
-
-    /**
-     * Use for borders or visual indicators of elements that reinforce our brand,
-     * such as global header or primary buttons.
-     */
-    'color-border-brand': 'color-brand-70',
-
-    /**
-     * Use for borders communicating a favorable outcome.
-     */
-    'color-feedback-border-success': 'color-success-50',
-    /**
-     * Use for borders communicating caution.
-     */
-    'color-feedback-border-warning': 'color-warning-50',
-    /**
-     * Use for borders communicating critical information, such as the borders on invalid inputs or table rows in error.
-     */
-    'color-feedback-border-alert': 'color-alert-50',
-    /**
-     * Use for borders communicating change or something new, such as the borders in onboarding spotlights.
-     */
-    'color-feedback-border-discovery': 'color-discovery-50',
-    /**
-     * Use for borders communicating information or something in-progress.
-     */
-    'color-feedback-border-informative': 'color-informative-50',
-
-    /**
-     * Use for borders of form UI elements, such as text fields, checkboxes, and radio buttons.
-     */
-    'color-input-border': 'color-neutral-65',
-    'color-input-border-hover': 'color-neutral-90',
-    'color-input-border-disabled': 'color-neutral-30',
-    'color-input-border-selected': 'color-brand-50',
-    'color-input-border-checked': 'color-brand-50',
-    'color-input-border-error': 'color-alert-50',
 
     /**
      * Use for focus rings of elements in a focus state.
@@ -347,11 +372,11 @@ export const defaultAliasTokens: AliasTokenMap = {
      */
 
     /**
-     * Use for primary content, such as body copy, sentence case headers, and buttons.
+     * Use for primary content, such as body copy, sentence case headers, labels, legends, etc.
      */
     'color-content': 'color-neutral-90',
     /**
-     * Use for secondary content, input field hints, subheadings, list items description.
+     * Use for secondary content, input field hints, subheadings, list items descriptions and captions.
      */
     'color-content-subtle': 'color-neutral-65',
     /**
@@ -367,47 +392,9 @@ export const defaultAliasTokens: AliasTokenMap = {
      */
     'color-content-disabled': 'color-neutral-30',
     /**
-     * Use for text in selected, active or opened states, such as choosers, toggles, pagination, etc.
+     * Use for content in selected, active or opened states, such as choosers, toggles, pagination, etc.
      */
     'color-content-selected': 'color-brand-70',
-    // 'color-content-selected-hover' ?
-
-    /**
-     * Use for content of elements that reinforce our brand, such as logos or primary buttons.
-     */
-    'color-content-brand': 'color-brand-50',
-
-    /**
-     * Use for content to communicate a favorable outcome, such as input field success messaging.
-     */
-    'color-feedback-content-success': 'color-success-70',
-    /**
-     * Use for critical text, such as input field error messaging.
-     */
-    'color-feedback-content-alert': 'color-alert-70',
-    /**
-     * Use for text to emphasize change or something new, such as in new lozenges.
-     */
-    'color-feedback-content-discovery': 'color-discovery-70',
-    /**
-     * Use for informative text or to communicate something is in progress, such as in-progress lozenges.
-     */
-    'color-feedback-content-informative': 'color-informative-70',
-    /**
-     * Use for text to emphasize caution, such as in moved lozenges.
-     */
-    'color-feedback-content-warning': 'color-warning-80',
-
-    /**
-     * Use for content of form UI elements, such as inputs.
-     */
-    'color-input-content': 'color-neutral-65',
-    'color-input-content-hover': 'color-neutral-90',
-    'color-input-content-disabled': 'color-neutral-30',
-    'color-input-content-error': 'color-alert-50',
-    'color-input-content-checked': 'color-white',
-    'color-input-content-selected': 'color-brand-70',
-    // color-input-content-placeholder?
 
     /**
      * Use for links in a default, disabled, hovered and visited state.
