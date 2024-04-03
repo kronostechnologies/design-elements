da1a753cdebfcc7d6756ee06a3f73e35fe3f3502
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-04-03 14:57:22 -0400

    fix(design-tokens): datepicker

--- packages/react/src/themes/tokens/component/datepicker-tokens.ts
+++ packages/react/src/themes/tokens/component/datepicker-tokens.ts
@@ -4,7 +4,9 @@ import { RefTokens } from '../ref-tokens';
 export type DatepickerTokens =
     | 'datepicker-border-color'
     | 'datepicker-box-shadow-color'
+    | 'datepicker-day-background-color'
     | 'datepicker-day-border-color'
+    | 'datepicker-day-text-color'
     | 'datepicker-day-hover-background-color'
     | 'datepicker-day-disabled-text-color'
     | 'datepicker-day-disabled-hover-background-color'
@@ -53,32 +55,41 @@ export type DatepickerTokenMap = {
 };
 
 export const defaultDatepickerTokens: DatepickerTokenMap = {
+    // NEW
     'datepicker-background-color': 'color-bg-overlay',
     'datepicker-border-color': 'color-border-overlay',
     'datepicker-box-shadow-color': 'color-box-shadow',
 
+    'datepicker-header-background-color': 'transparent-100',
+
+    'datepicker-day-background-color': 'transparent-100',
     'datepicker-day-border-color': 'transparent-100',
+    'datepicker-day-text-color': 'color-content',
+
     'datepicker-day-hover-background-color': 'color-bg-hover',
     'datepicker-day-disabled-text-color': 'color-content-disabled',
+
+    'datepicker-day-outside-month-text-color': 'color-content-subtle',
+    'datepicker-day-selected-outside-month-background-color': 'color-bg-selected',
+    'datepicker-day-selected-outside-month-border-color': 'color-border-selected',
+    'datepicker-day-selected-outside-month-text-color': 'color-content-selected',
+    'datepicker-day-selected-background-color': 'color-bg-selected',
+    'datepicker-day-selected-border-color': 'color-border-selected',
+    'datepicker-day-selected-text-color': 'color-content-selected',
+
+    'datepicker-day-today-text-color': 'color-content-selected',
+
+    // OLD
     'datepicker-day-disabled-hover-background-color': 'color-bg-disabled',
     'datepicker-day-focus-background-color': 'transparent-100',
     'datepicker-day-focus-text-color': 'color-content',
     'datepicker-day-focus-border-color': 'color-border-focus-inside',
     'datepicker-day-focus-box-shadow-color': 'color-border-focus-outside',
-    'datepicker-day-outside-month-text-color': 'color-content-subtle',
-    'datepicker-day-selected-background-color': 'color-bg-selected',
-    'datepicker-day-selected-outside-month-background-color': 'color-bg-selected',
-    'datepicker-day-selected-border-color': 'color-border-selected',
-    'datepicker-day-selected-outside-month-border-color': 'color-border-selected',
-    'datepicker-day-selected-text-color': 'color-content-selected',
-    'datepicker-day-selected-outside-month-text-color': 'color-content-selected',
+
     'datepicker-day-selected-focus-box-shadow-color': 'color-border-focus-inside',
     'datepicker-day-selected-hover-text-color': 'color-content-selected',
 
-    'datepicker-day-today-text-color': 'color-content-selected',
     'datepicker-day-today-selected-text-color': 'color-content-selected',
-
-    'datepicker-header-background-color': 'transparent-100',
     'datepicker-header-nav-button-background-color': 'transparent-100',
 
     'datepicker-input-background-color': 'color-input-bg',
