b14c6ec83650de43cb2ae2fba7101b178a1fd95b
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-10 07:37:08 -0400

    docs(storybook): update structure and variants

--- packages/storybook/stories/checkbox.stories.tsx
+++ packages/storybook/stories/checkbox.stories.tsx
@@ -2,17 +2,12 @@ import { Checkbox } from '@equisoft/design-elements-react';
 import { StoryFn as Story } from '@storybook/react';
 
 export default {
-    title: 'Components/Checkbox/Stories',
+    title: 'Components/Checkbox',
     component: Checkbox,
 };
 
-export const Normal: Story = () => (
+export const Default: Story = () => (
     <>
-        <Checkbox data-testid="some-checkbox-data-testid" name="checkbox1" value="no label" />
-        <Checkbox label="Normal" name="checkbox2" value="normal" />
-        <Checkbox label="Disabled" name="checkbox3" value="disabled" disabled />
-        <Checkbox label="Checked" name="checkbox4" value="checked" checked />
-        <Checkbox label="Default checked" name="checkbox5" value="defaultChecked" defaultChecked />
-        <Checkbox label="Indeterminate" name="checkbox6" value="indeterminate" indeterminate />
+        <Checkbox label="Send by email" name="checkbox2" value="normal" />
     </>
 );
