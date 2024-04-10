14668f474c5d8f1247618a1191620d897e44966c
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-10 08:24:09 -0400

    docs(storybook): update structure and variants

--- packages/storybook/stories/card.stories.tsx
+++ packages/storybook/stories/card.stories.tsx
@@ -2,10 +2,10 @@ import { Card } from '@equisoft/design-elements-react';
 import { StoryFn as Story } from '@storybook/react';
 
 export default {
-    title: 'Components/Card/Stories',
+    title: 'Components/Card',
     component: Card,
 };
 
-export const Normal: Story = () => (
+export const NonClickable: Story = () => (
     <Card>Hello, World!</Card>
 );
