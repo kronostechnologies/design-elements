157a01bc2ea1d67213351cdf7b1bae5824a924f1
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-10 08:50:32 -0400

    docs(storybook): update structure and variants

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -63,7 +63,7 @@ const config: StorybookConfig = {
     },
     core: {
         disableTelemetry: true,
-        defaultName: 'All Stories'
+        defaultName: 'Doc'
     },
 };
 
--- packages/storybook/stories/badge.stories.tsx
+++ packages/storybook/stories/badge.stories.tsx
@@ -3,7 +3,7 @@ import { StoryFn as Story } from '@storybook/react';
 import styled from 'styled-components';
 
 export default {
-    title: 'Components/Badge/Stories',
+    title: 'Components/Badge',
     component: Badge,
 };
 
@@ -15,47 +15,6 @@ export const Default: Story = () => (
 
 export const Dot: Story = () => (
     <Badge value={1} showValue={false}>
-        <Icon name="bell" />
-    </Badge>
-);
-
-export const Overflow: Story = () => (
-    <Badge value={10} maxValue={9}>
-        <Icon name="bell" />
-    </Badge>
-);
-
-const Spacer = styled.span`
-    margin-right: 4rem;
-`;
-
-export const Positions: Story = () => (
-    <>
-        <Spacer>
-            <Badge value={1} position="top-right">
-                <Icon name="bell" />
-            </Badge>
-        </Spacer>
-        <Spacer>
-            <Badge value={1} position="bottom-right">
-                <Icon name="bell" />
-            </Badge>
-        </Spacer>
-        <Spacer>
-            <Badge value={1} position="top-left">
-                <Icon name="bell" />
-            </Badge>
-        </Spacer>
-        <Spacer>
-            <Badge value={1} position="bottom-left">
-                <Icon name="bell" />
-            </Badge>
-        </Spacer>
-    </>
-);
-
-export const Animated: Story = () => (
-    <Badge value={1} animate>
         <Icon name="bell" />
     </Badge>
 );
