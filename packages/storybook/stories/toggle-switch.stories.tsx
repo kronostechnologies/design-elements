fb35fd408a3e30d3fd2b330bf3f573fc6a4294af
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-11 15:04:16 -0400

    docs(storybook): update doc and structure and variants

--- packages/storybook/stories/toggle-switch.stories.tsx
+++ packages/storybook/stories/toggle-switch.stories.tsx
@@ -9,26 +9,10 @@ export default {
     parameters: rawCodeParameters,
 };
 
-export const Normal: Story = () => {
+export const Default: Story = () => {
     const [toggled, setToggled] = useState(false);
 
     return (
-        <ToggleSwitch data-testid='some-data-testid' label="Switch" toggled={toggled} onToggle={setToggled} />
+        <ToggleSwitch data-testid='some-data-testid' label="Show children organizations" toggled={toggled} onToggle={setToggled} />
     );
 };
-
-export const Toggled: Story = () => {
-    const [toggled, setToggled] = useState(true);
-
-    return (
-        <ToggleSwitch label="Switch" toggled={toggled} onToggle={setToggled} />
-    );
-};
-
-export const Disabled: Story = () => (
-    <>
-        <ToggleSwitch label="Switch" disabled toggled={false} onToggle={() => console.info('Should not be called!')} />
-        <br />
-        <ToggleSwitch label="Switch" disabled toggled onToggle={() => console.info('Should not be called!')} />
-    </>
-);
