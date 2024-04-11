a0e98d2e078669d0b8089f341e153d6e2ce45f8a
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-11 10:53:06 -0400

    docs(storybook): update structure and variants

--- packages/storybook/stories/tabs.stories.tsx
+++ packages/storybook/stories/tabs.stories.tsx
@@ -1,10 +1,10 @@
-import { Card, Tab, Table, TableColumn, Tabs, TextArea } from '@equisoft/design-elements-react';
+import { Tab, Tabs } from '@equisoft/design-elements-react';
 import { StoryFn as Story } from '@storybook/react';
 import styled from 'styled-components';
 import { rawCodeParameters } from './utils/parameters';
 
 export default {
-    title: 'Components/Tabs/Stories',
+    title: 'Components/Tabs',
     component: Tabs,
     parameters: rawCodeParameters,
 };
@@ -13,83 +13,18 @@ const StyledDiv = styled.div`
     padding: var(--spacing-2x);
 `;
 
-interface Data {
-    column1: string;
-    column2: string;
-}
-
-export const Section: Story = () => {
-    const contactTableColumns: TableColumn<Data> = [
-        {
-            header: 'First Name',
-            accessorKey: 'column1',
-        },
-        {
-            header: 'Last Name',
-            accessorKey: 'column2',
-        },
-    ];
-
-    const contactTableData: Data[] = [
-        {
-            column1: 'First Name 1',
-            column2: 'First Name 2',
-        },
-        {
-            column1: 'Last Name 1',
-            column2: 'Last Name 2',
-        },
-    ];
-
-    const tabs: Tab[] = [
-        {
-            title: 'Contact',
-            panelContent: (
-                <StyledDiv>
-                    <Table columns={contactTableColumns} data={contactTableData} />
-                </StyledDiv>
-            ),
-        },
-        {
-            title: 'Calendar',
-            panelContent: (
-                <StyledDiv>
-                    <Card>Monday : Doing something meaningful</Card>
-                    <Card>Tuesday : Doing something else</Card>
-                </StyledDiv>
-            ),
-        },
-        {
-            title: 'Note',
-            panelContent: (
-                <StyledDiv>
-                    <TextArea label="Notes" />
-                </StyledDiv>
-            ),
-        },
-    ];
-
-    return (
-        <Tabs tabs={tabs} />
-    );
-};
-
 export const Global: Story = () => {
     const tabs: Tab[] = [
         {
-            title: 'First Button',
-            leftIcon: 'chevronUp',
+            title: 'Contact',
             panelContent: <StyledDiv>First tab content</StyledDiv>,
         },
         {
-            title: 'Second Button',
-            leftIcon: 'chevronLeft',
-            rightIcon: 'chevronRight',
+            title: 'Calendar',
             panelContent: <StyledDiv>Second tab content</StyledDiv>,
         },
         {
-            title: 'Third Button',
-            rightIcon: 'chevronDown',
+            title: 'Note',
             panelContent: <StyledDiv>Third tab content</StyledDiv>,
         },
     ];
@@ -99,96 +34,23 @@ export const Global: Story = () => {
     );
 };
 
-export const WithIcons: Story = () => {
+export const Section: Story = () => {
     const tabs: Tab[] = [
         {
-            title: 'First Button',
-            leftIcon: 'chevronUp',
+            title: 'Contact',
             panelContent: <StyledDiv>First tab content</StyledDiv>,
         },
         {
-            title: 'Second Button',
-            leftIcon: 'chevronLeft',
-            rightIcon: 'chevronRight',
+            title: 'Calendar',
             panelContent: <StyledDiv>Second tab content</StyledDiv>,
         },
         {
-            title: 'Third Button',
-            rightIcon: 'chevronDown',
+            title: 'Note',
             panelContent: <StyledDiv>Third tab content</StyledDiv>,
         },
     ];
 
     return (
         <Tabs tabs={tabs} />
-    );
-};
-
-export const WithForceRenderTabPanels: Story = () => {
-    const tabs: Tab[] = [
-        {
-            title: 'First Button',
-            panelContent: <StyledDiv>First tab content</StyledDiv>,
-        },
-        {
-            title: 'Second Button',
-            panelContent: <StyledDiv>Second tab content</StyledDiv>,
-        },
-        {
-            title: 'Third Button',
-            panelContent: <StyledDiv>Third tab content</StyledDiv>,
-        },
-    ];
-
-    return (
-        <Tabs tabs={tabs} forceRenderTabPanels />
-    );
-};
-
-export const Contained: Story = () => {
-    const tabs: Tab[] = [
-        {
-            title: 'First Button',
-            panelContent: <StyledDiv>First tab content</StyledDiv>,
-        },
-        {
-            title: 'Second Button',
-            panelContent: <StyledDiv>Second tab content</StyledDiv>,
-        },
-        {
-            title: 'Third Button',
-            panelContent: <StyledDiv>Third tab content</StyledDiv>,
-        },
-    ];
-
-    return (
-        <Tabs tabs={tabs} contained />
-    );
-};
-
-export const UnloadTabCallback: Story = () => {
-    const tabs: Tab[] = [
-        {
-            title: 'Tab that cannot change because onBeforeUnload resolves to false',
-            panelContent: <StyledDiv>First tab content</StyledDiv>,
-            onBeforeUnload: () => {
-                console.info('cannot change tab because onBeforeUnload promise resolves to false here');
-                return Promise.resolve(false);
-            },
-        },
-        {
-            title: 'Second Button',
-            panelContent: <StyledDiv>Second tab content</StyledDiv>,
-            onBeforeUnload: () => Promise.resolve(true),
-        },
-        {
-            title: 'Third Button',
-            panelContent: <StyledDiv>Third tab content</StyledDiv>,
-            onBeforeUnload: () => Promise.resolve(true),
-        },
-    ];
-
-    return (
-        <Tabs tabs={tabs} contained />
     );
 };
