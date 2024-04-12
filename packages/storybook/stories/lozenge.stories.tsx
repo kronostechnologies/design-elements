84898cce1ab45b6f3ca558f972f5370a78884def
Pierre-Yves Lafleur <pierre-yves.lafleur@equisoft.com>
2024-04-12 09:19:11 -0400

    docs(Lozenge): add controls

--- packages/storybook/stories/lozenge.stories.tsx
+++ packages/storybook/stories/lozenge.stories.tsx
@@ -1,72 +1,85 @@
 import { Lozenge } from '@equisoft/design-elements-react';
-import { StoryFn as Story } from '@storybook/react';
-import styled from 'styled-components';
+import { Meta, StoryObj } from '@storybook/react';
 
-export default {
+const meta: Meta<typeof Lozenge> = {
     title: 'Components/Lozenge',
     component: Lozenge,
 };
 
-export const Default: Story = () => (
-    <Lozenge>status</Lozenge>
-);
+export default meta;
 
-const VariantsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 1rem;
-`;
+type Story = StoryObj<typeof Lozenge>;
 
-export const Neutral: Story = () => (
-    <VariantsContainer>
-        <Lozenge variant="neutral">neutral</Lozenge>
-        <Lozenge subtle variant="neutral">neutral</Lozenge>
-        <Lozenge icon="star" variant="neutral">neutral</Lozenge>
-        <Lozenge subtle icon="star" variant="neutral">neutral</Lozenge>
-    </VariantsContainer>
-);
+export const Default: Story = {
+    render: (args) => (
+        <Lozenge {...args /* eslint-disable-line react/jsx-props-no-spreading */}>
+            status
+        </Lozenge>
+    ),
+};
 
-export const Success: Story = () => (
-    <VariantsContainer>
-        <Lozenge variant="success">success</Lozenge>
-        <Lozenge subtle variant="success">success</Lozenge>
-        <Lozenge icon="check" variant="success">success</Lozenge>
-        <Lozenge subtle icon="check" variant="success">success</Lozenge>
-    </VariantsContainer>
-);
+export const Neutral: Story = {
+    args: {
+        variant: 'neutral',
+    },
+    render: (args) => (
+        <Lozenge {...args /* eslint-disable-line react/jsx-props-no-spreading */}>
+            neutral
+        </Lozenge>
+    ),
+};
 
-export const Alert: Story = () => (
-    <VariantsContainer>
-        <Lozenge variant="alert">alert</Lozenge>
-        <Lozenge subtle variant="alert">alert</Lozenge>
-        <Lozenge icon="alertOctagon" variant="alert">alert</Lozenge>
-        <Lozenge subtle icon="alertOctagon" variant="alert">alert</Lozenge>
-    </VariantsContainer>
-);
+export const Success: Story = {
+    args: {
+        variant: 'success',
+    },
+    render: (args) => (
+        <Lozenge {...args /* eslint-disable-line react/jsx-props-no-spreading */}>
+            success
+        </Lozenge>
+    ),
+};
 
-export const Warning: Story = () => (
-    <VariantsContainer>
-        <Lozenge variant="warning">warning</Lozenge>
-        <Lozenge subtle variant="warning">warning</Lozenge>
-        <Lozenge icon="alertTriangle" variant="warning">warning</Lozenge>
-        <Lozenge subtle icon="alertTriangle" variant="warning">warning</Lozenge>
-    </VariantsContainer>
-);
+export const Alert: Story = {
+    args: {
+        variant: 'alert',
+    },
+    render: (args) => (
+        <Lozenge {...args /* eslint-disable-line react/jsx-props-no-spreading */}>
+            alert
+        </Lozenge>
+    ),
+};
 
-export const Informative: Story = () => (
-    <VariantsContainer>
-        <Lozenge variant="info">info</Lozenge>
-        <Lozenge subtle variant="info">info</Lozenge>
-        <Lozenge icon="info" variant="info">info</Lozenge>
-        <Lozenge subtle icon="info" variant="info">info</Lozenge>
-    </VariantsContainer>
-);
+export const Warning: Story = {
+    args: {
+        variant: 'warning',
+    },
+    render: (args) => (
+        <Lozenge {...args /* eslint-disable-line react/jsx-props-no-spreading */}>
+            warning
+        </Lozenge>
+    ),
+};
 
-export const Discovery: Story = () => (
-    <VariantsContainer>
-        <Lozenge variant="discovery">discovery</Lozenge>
-        <Lozenge subtle variant="discovery">discovery</Lozenge>
-        <Lozenge icon="star" variant="discovery">discovery</Lozenge>
-        <Lozenge subtle icon="star" variant="discovery">discovery</Lozenge>
-    </VariantsContainer>
-);
+export const Informative: Story = {
+    args: {
+        variant: 'info',
+    },
+    render: (args) => (
+        <Lozenge {...args /* eslint-disable-line react/jsx-props-no-spreading */}>
+            info
+        </Lozenge>
+    ),
+};
+
+export const Discovery: Story = {
+    args: {
+        variant: 'discovery',
+    },
+    render: (args) => (
+        <Lozenge {...args /* eslint-disable-line react/jsx-props-no-spreading */}>
+            discovery
+        </Lozenge>
+    ),
+};
