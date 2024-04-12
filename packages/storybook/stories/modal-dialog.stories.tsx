7bd1c0ae1d264f801c9fbc4a3ccd09e6b163adbd
Pierre-Yves Lafleur <pierre-yves.lafleur@equisoft.com>
2024-04-12 10:54:22 -0400

    docs(ModalDialog): add controls

--- packages/storybook/stories/modal-dialog.stories.tsx
+++ packages/storybook/stories/modal-dialog.stories.tsx
@@ -1,26 +1,45 @@
 import { Button, ModalDialog, useModal } from '@equisoft/design-elements-react';
-import { StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
-export default {
+const meta: Meta<typeof ModalDialog> = {
     title: 'Components/Modal',
     component: ModalDialog,
+    argTypes: {
+        appElement: {
+            control: { type: null },
+        },
+        isOpen: {
+            control: { type: null },
+        },
+        onRequestClose: {
+            control: { type: null },
+        },
+        parentSelector: {
+            control: { type: null },
+        },
+    },
 };
+
+export default meta;
 
 type Story = StoryObj<typeof ModalDialog>;
 
 export const Default: Story = {
-    render: () => {
+    args: {
+        ariaDescribedby: 'story-description',
+        title: 'Heading 5',
+        subtitle: 'Subtitle 1',
+    },
+    render: (args) => {
         const { isModalOpen, closeModal, openModal } = useModal();
         return (
             <>
                 <Button label="Open Modal" buttonType="primary" onClick={openModal} />
                 <ModalDialog
+                    {...args /* eslint-disable-line react/jsx-props-no-spreading */}
                     appElement="#storybook-root"
-                    ariaDescribedby="story-description"
                     isOpen={isModalOpen}
                     onRequestClose={closeModal}
-                    title="Heading 5"
-                    subtitle="Subtitle 1"
                 >
                     <p style={{ margin: 0 }} id="story-description">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -33,21 +52,23 @@ export const Default: Story = {
 };
 
 export const AlertModal: Story = {
-    render: () => {
+    args: {
+        ariaDescribedby: 'story-description',
+        dialogType: 'alert',
+        confirmButton: { label: 'Delete' },
+        title: 'Alert Modal',
+        subtitle: 'Subtitle 1',
+    },
+    render: (args) => {
         const { isModalOpen, closeModal, openModal } = useModal();
         return (
             <>
                 <Button label="Open Modal" buttonType="destructive-primary" onClick={openModal} />
                 <ModalDialog
+                    {...args /* eslint-disable-line react/jsx-props-no-spreading */}
                     appElement="#storybook-root"
-                    ariaDescribedby="story-description"
-                    confirmButton={{
-                        label: 'Delete',
-                    }}
                     isOpen={isModalOpen}
                     onRequestClose={closeModal}
-                    title="Alert Modal"
-                    dialogType="alert"
                 >
                     <p style={{ margin: 0 }} id="story-description">
                         This modal has a destructive button. It is used to alert the user of something.
