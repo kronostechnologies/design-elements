d455d1454809b017cda7e3dc2f4db22cc816dd11
Pierre-Yves Lafleur <pierre-yves.lafleur@equisoft.com>
2024-04-12 10:03:34 -0400

    docs(Tooltip): add controls

--- packages/storybook/stories/tooltip.stories.tsx
+++ packages/storybook/stories/tooltip.stories.tsx
@@ -1,48 +1,44 @@
-import {
-    IconButton,
-    Tooltip,
-} from '@equisoft/design-elements-react';
-import { StoryFn as Story } from '@storybook/react';
+import { IconButton, Tooltip } from '@equisoft/design-elements-react';
+import { Meta, StoryObj } from '@storybook/react';
 import styled from 'styled-components';
 
 const Container = styled.div`
     height: 100px;
 `;
 
-export default {
+const meta: Meta<typeof Tooltip> = {
     title: 'Components/Tooltip',
     component: Tooltip,
 };
 
-const DarkDiv = styled.div`
-    align-items: center;
-    background-color: ${({ theme }) => theme.main['primary-1.1']};
-    display: flex;
-    height: 50px;
-    justify-content: center;
-    width: 100%;
-`;
+export default meta;
 
-export const Default: Story = () => (
-    <Tooltip label="Tooltip Content" />
-);
+type Story = StoryObj<typeof Tooltip>;
+
+export const Default: Story = {
+    args: {
+        label: 'Tooltip content',
+    },
+};
 
 const CodeContainer = styled.div`
     display: flex;
 `;
-export const Confirmation: Story = () => {
-    const code = 'JBSW Y3DP EHPK 3PXP';
 
-    return (
+const code = 'JBSW Y3DP EHPK 3PXP';
+
+export const Confirmation: Story = {
+    args: {
+        label: 'label',
+        confirmationLabel: 'label confirmation',
+        desktopPlacement: 'bottom',
+        mode: 'confirm',
+    },
+    render: (args) => (
         <Container>
             <CodeContainer>
                 <span>{code}</span>
-                <Tooltip
-                    label="label"
-                    confirmationLabel="label confirmation"
-                    desktopPlacement="bottom"
-                    mode='confirm'
-                >
+                <Tooltip {...args /* eslint-disable-line react/jsx-props-no-spreading */}>
                     <IconButton
                         buttonType='tertiary'
                         type='button'
@@ -54,5 +50,5 @@ export const Confirmation: Story = () => {
                 </Tooltip>
             </CodeContainer>
         </Container>
-    );
+    ),
 };
