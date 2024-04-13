f6d621280c6eb554d3c77f16deabacffe5af4afe
Pierre-Yves Lafleur <pierre-yves.lafleur@equisoft.com>
2024-04-13 19:47:29 -0400

    docs(ToggleSwitch): add controls

--- packages/storybook/stories/toggle-switch.stories.tsx
+++ packages/storybook/stories/toggle-switch.stories.tsx
@@ -1,18 +1,52 @@
 import { ToggleSwitch } from '@equisoft/design-elements-react';
-import { StoryFn as Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
-import { rawCodeParameters } from './utils/parameters';
 
-export default {
+const meta: Meta<typeof ToggleSwitch> = {
     title: 'Components/Toggle Switch',
     component: ToggleSwitch,
-    parameters: rawCodeParameters,
+    argTypes: {
+        onToggle: {
+            control: { type: null },
+        },
+        toggled: {
+            control: { type: null },
+        },
+    },
 };
 
-export const Default: Story = () => {
-    const [toggled, setToggled] = useState(false);
+export default meta;
 
-    return (
-        <ToggleSwitch data-testid='some-data-testid' label="Show children organizations" toggled={toggled} onToggle={setToggled} />
-    );
+type Story = StoryObj<typeof ToggleSwitch>;
+
+export const Default: Story = {
+    args: {
+        label: 'Show children organizations',
+    },
+    render: (args) => {
+        const [toggled, setToggled] = useState(false);
+        return (
+            <ToggleSwitch
+                {...args /* eslint-disable-line react/jsx-props-no-spreading */}
+                toggled={toggled}
+                onToggle={setToggled}
+            />
+        );
+    },
+    parameters: {
+        docs: {
+            source: {
+                code: `
+const [toggled, setToggled] = useState(false);
+return (
+    <ToggleSwitch
+        label="Show children organizations"
+        toggled={toggled}
+        onToggle={setToggled}
+    />
+);                
+                `,
+            },
+        },
+    },
 };
