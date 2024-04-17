300cf4f00b3825461b2d472f8ab0d9fc2a023daa
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-17 08:57:56 -0400

    docs(storybook): update stepper input

--- packages/storybook/stories/stepper-input.stories.tsx
+++ packages/storybook/stories/stepper-input.stories.tsx
@@ -1,6 +1,5 @@
 import { StepperInput } from '@equisoft/design-elements-react';
 import { StoryFn as Story } from '@storybook/react';
-import { useState } from 'react';
 import { rawCodeParameters } from './utils/parameters';
 
 export default {
@@ -8,46 +7,8 @@ export default {
     component: StepperInput,
 };
 
-export const Normal: Story = () => (
+export const Default: Story = () => (
     <StepperInput label="Stepper input" />
-);
-
-export const WithTooltip: Story = () => (
-    <StepperInput label="Stepper input" tooltip={{ label: 'Tooltip text content' }} />
-);
-
-type Value = number | undefined | null;
-export const Controlled: Story = () => {
-    const [inputValue, setInputValue] = useState<Value>(null);
-
-    return (
-        <StepperInput label="Stepper input" onChange={setInputValue} value={inputValue} />
-    );
-};
-Controlled.parameters = rawCodeParameters;
-
-export const WithDefaultValue: Story = () => (
-    <StepperInput label="Stepper input" defaultValue={0} />
-);
-
-export const WithCustomStep: Story = () => (
-    <StepperInput label="Stepper input" step={5} />
-);
-
-export const WithMaxValue: Story = () => (
-    <StepperInput label="Stepper input" max={5} />
-);
-
-export const WithMinValue: Story = () => (
-    <StepperInput label="Stepper input" min={0} />
-);
-
-export const Invalid: Story = () => (
-    <StepperInput valid={false} label="Stepper input" />
-);
-
-export const Disabled: Story = () => (
-    <StepperInput label="Stepper input" disabled />
 );
 
 export const WithOnChangeCallback: Story = () => (
