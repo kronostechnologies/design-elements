558dffffa707a79085f7fde5ccf4096c074648ba
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-17 13:49:41 -0400

    docs(storybook): update phone input

--- packages/storybook/stories/phone-input.stories.tsx
+++ packages/storybook/stories/phone-input.stories.tsx
@@ -7,7 +7,7 @@ export default {
     component: PhoneInput,
 };
 
-export const Normal: Story = () => (
+export const Default: Story = () => (
     <PhoneInput
         data-testid='custom-data-test-id'
         pattern='(___) ___-____'
@@ -15,16 +15,7 @@ export const Normal: Story = () => (
         label='Label'
     />
 );
-Normal.decorators = [DesktopDecorator];
+Default.decorators = [DesktopDecorator];
 
 export const Mobile: Story = () => <PhoneInput pattern='(___) ___-____' />;
 Mobile.decorators = [MobileDecorator];
-
-export const WithDefaultValue: Story = () => <PhoneInput pattern='(___) ___-____' defaultValue='1234567890' />;
-WithDefaultValue.decorators = [DesktopDecorator];
-
-export const Required: Story = () => <PhoneInput pattern='(___) ___-____' required />;
-Required.decorators = [DesktopDecorator];
-
-export const Disabled: Story = () => <PhoneInput pattern='(___) ___-____' disabled />;
-Disabled.decorators = [DesktopDecorator];
