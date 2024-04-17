39c6314a0fd0d4d39531001eba1134013ecbe4f1
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-17 14:47:08 -0400

    docs(storybook): update link

---
+++ packages/storybook/stories/link.stories.tsx
@@ -0,0 +1,16 @@
+import { RouteLink } from '@equisoft/design-elements-react';
+import { StoryFn as Story } from '@storybook/react';
+import { NavLink } from 'react-router-dom';
+import { RouterDecorator } from './utils/router-decorator';
+
+export default {
+    title: 'Components/Link',
+    component: RouteLink,
+    decorators: [RouterDecorator],
+};
+
+export const Default: Story = () => (
+    <>
+        <RouteLink routerLink={NavLink} href="/story1" label="Route Link" />
+    </>
+);
