6d53d54341bd277985c212fd6eb5674b2b8e6bcf
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-04-17 09:21:52 -0400

    fix(design-tokens, menu): replace orphan color

--- packages/react/src/themes/tokens/component/menu-tokens.ts
+++ packages/react/src/themes/tokens/component/menu-tokens.ts
@@ -9,9 +9,12 @@ export type MenuTokens =
     | 'menu-submenu-border-color'
     | 'menu-submenu-box-shadow-color'
     | 'menu-item-text-color'
+    | 'menu-item-hover-text-color'
+    | 'menu-item-icon-color'
+    | 'menu-item-hover-icon-color'
     | 'menu-item-hover-background-color'
-    | 'menu-item-focus-outline-color'
     | 'menu-item-disabled-text-color'
+    | 'menu-item-focus-outline-color'
     | 'menu-group-text-color';
 
 export type MenuTokenValue = AliasTokens | RefTokens;
@@ -26,11 +29,14 @@ export const defaultMenuTokens: MenuTokenMap = {
     'menu-submenu-background-color': 'color-menu-bg',
     'menu-submenu-border-color': 'color-menu-border',
 
-    'menu-item-hover-background-color': 'color-menu-item-bg-hover',
     'menu-group-text-color': 'color-menu-item-subcontent',
 
     'menu-item-text-color': 'color-menu-item-content',
+    'menu-item-hover-text-color': 'color-menu-item-content-hover',
+    'menu-item-icon-color': 'color-menu-item-content',
+    'menu-item-hover-icon-color': 'color-menu-item-content-hover',
     'menu-item-disabled-text-color': 'color-menu-item-content-disabled',
+    'menu-item-hover-background-color': 'color-menu-item-bg-hover',
 
     'menu-submenu-box-shadow-color': 'color-box-shadow',
     'menu-box-shadow-color': 'color-box-shadow',
