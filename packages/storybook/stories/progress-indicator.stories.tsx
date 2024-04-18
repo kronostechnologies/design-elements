382fac57fe66bbd1ed4485968af931a180d09933
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-18 15:42:06 -0400

    docs(storybook): update progress indicator

--- packages/storybook/stories/progress-indicator.stories.tsx
+++ packages/storybook/stories/progress-indicator.stories.tsx
@@ -1,24 +1,11 @@
-import { ProgressIndicator } from '@equisoft/design-elements-react';
+import { Spinner } from '@equisoft/design-elements-react';
 import { StoryFn as Story } from '@storybook/react';
 
 export default {
     title: 'Components/Progress Indicator',
-    component: ProgressIndicator,
+    component: Spinner,
 };
 
-export const Normal: Story = () => (
-    <>
-        <ProgressIndicator
-            descriptionLabel="You"
-            resultLabel="50k - 100k $"
-            percent={60}
-            color="#0080A5"
-        />
-        <ProgressIndicator
-            descriptionLabel="Equisoft"
-            resultLabel="150k - 250k $"
-            percent={100}
-            color="#012639"
-        />
-    </>
+export const Default: Story = () => (
+    <Spinner />
 );
