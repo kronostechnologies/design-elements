f8ff36b79c3e9ec1f285f2932be0db06ddcb5e92
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-04-22 12:26:39 -0400

    chore(design-tokens): update radio card tokens

--- packages/react/src/themes/tokens/component/radio-card-tokens.ts
+++ packages/react/src/themes/tokens/component/radio-card-tokens.ts
@@ -5,8 +5,8 @@ export type RadioCardTokens =
     | 'radio-card-background-color'
     | 'radio-card-border-color'
     | 'radio-card-text-color'
-    | 'radio-card-checked-background-color'
-    | 'radio-card-checked-border-color'
+    | 'radio-card-selected-background-color'
+    | 'radio-card-selected-border-color'
     | 'radio-card-hover-background-color'
     | 'radio-card-hover-border-color'
     | 'radio-card-hover-text-color'
@@ -23,17 +23,21 @@ export type RadioCardTokenMap = {
 };
 
 export const defaultRadioCardTokens: RadioCardTokenMap = {
-    'radio-card-button-checked-background-color': 'color-brand-50',
-    'radio-card-button-checked-border-color': 'color-brand-50',
+    'radio-card-background-color': 'color-white',
+    'radio-card-text-color': 'color-black',
     'radio-card-border-color': 'color-black',
-    'radio-card-checked-background-color': 'color-brand-05',
-    'radio-card-checked-border-color': 'color-brand-50',
-    'radio-card-disabled-background-color': 'color-neutral-05',
-    'radio-card-disabled-border-color': 'color-neutral-15',
-    'radio-card-disabled-text-color': 'color-neutral-30',
+
     'radio-card-hover-background-color': 'color-neutral-15',
     'radio-card-hover-border-color': 'color-black',
     'radio-card-hover-text-color': 'color-black',
-    'radio-card-text-color': 'color-black',
-    'radio-card-background-color': 'color-white',
+
+    'radio-card-disabled-background-color': 'color-neutral-05',
+    'radio-card-disabled-border-color': 'color-neutral-15',
+    'radio-card-disabled-text-color': 'color-neutral-30',
+
+    'radio-card-selected-background-color': 'color-brand-05',
+    'radio-card-selected-border-color': 'color-brand-50',
+
+    'radio-card-button-checked-background-color': 'color-brand-50',
+    'radio-card-button-checked-border-color': 'color-brand-50',
 };
