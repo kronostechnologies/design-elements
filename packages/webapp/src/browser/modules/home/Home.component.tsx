64b8acbbc1a81fc833d7499a115b701e0f8fda6a
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2024-04-23 15:29:39 -0400

    docs(webapp): ajouter une webapp

---
+++ packages/webapp/src/browser/modules/home/Home.component.tsx
@@ -0,0 +1,21 @@
+import { Button, SectionalBanner } from '@equisoft/design-elements-react';
+import { FunctionComponent } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const HomePage: FunctionComponent = () => {
+    const { t, i18n } = useTranslation();
+    const language: string = i18n.language;
+    const nextLanguage = language === 'fr' ? 'en' : 'fr';
+    const rotateLanguage: () => void = () => i18n.changeLanguage(nextLanguage);
+
+    return (
+        <>
+            <SectionalBanner type="info">
+                {t('helloWorld')}
+            </SectionalBanner>
+            <Button buttonType="primary" onClick={rotateLanguage}>
+                {t('nav:changeLanguage', { lng: language, nextLanguage })}
+            </Button>
+        </>
+    );
+};
