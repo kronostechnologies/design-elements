123640a0fa19f79639eb1d5d2b1a0a296b713ba4
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-04-29 09:14:00 -0400

    chore(design-tokens): update listbox tokens

--- packages/react/src/themes/tokens/component/listbox-tokens.ts
+++ packages/react/src/themes/tokens/component/listbox-tokens.ts
@@ -4,17 +4,17 @@ import { RefTokens } from '../ref-tokens';
 export type ListboxTokens =
     | 'listbox-background-color'
     | 'listbox-border-color'
-    | 'listbox-box-shadow-frame-color'
-    | 'listbox-box-shadow-depth-color'
     | 'listbox-item-text-color'
+    | 'listbox-item-disabled-background-color'
     | 'listbox-item-disabled-text-color'
     | 'listbox-item-hover-background-color'
-    | 'listbox-item-hover-disabled-background-color'
     | 'listbox-item-selected-background-color'
     | 'listbox-item-selected-border-color'
     | 'listbox-item-selected-icon-color'
     | 'listbox-item-caption-text-color'
-    | 'listbox-item-caption-disabled-text-color';
+    | 'listbox-item-caption-disabled-text-color'
+    | 'listbox-box-shadow-frame-color'
+    | 'listbox-box-shadow-depth-color';
 
 export type ListboxTokenValue = AliasTokens | RefTokens;
 
@@ -32,10 +32,11 @@ export const defaultListboxTokens: ListboxTokenMap = {
     'listbox-item-disabled-background-color': 'color-white',
     'listbox-item-disabled-text-color': 'color-neutral-30',
     'listbox-item-caption-disabled-text-color': 'color-neutral-15',
-    'listbox-item-caption-text-color': 'color-neutral-65',
-    'listbox-item-hover-background-color': 'color-neutral-15',
-    'listbox-item-hover-disabled-background-color': 'color-white',
+
     'listbox-item-selected-background-color': 'color-brand-50',
     'listbox-item-selected-border-color': 'color-brand-50',
     'listbox-item-selected-icon-color': 'color-white',
+
+    'listbox-box-shadow-frame-color': 'color-neutral-15',
+    'listbox-box-shadow-depth-color': 'transparent-dark-20',
 };
