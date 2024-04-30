2b81c80be3b13bac8e241a7b47c458b17348e7be
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-30 10:17:24 -0400

    docs(storybook): update dropdown-list

--- packages/storybook/stories/dropdown-list.stories.tsx
+++ packages/storybook/stories/dropdown-list.stories.tsx
@@ -32,7 +32,7 @@ export default {
     decorators: [decorateWith(Container)],
 };
 
-export const Normal: Story = () => (
+export const Default: Story = () => (
     <DropdownList
         data-testid="some-data-test-id"
         label="Select an option"
@@ -41,30 +41,13 @@ export const Normal: Story = () => (
     />
 );
 
-export const WithTooltip: Story = () => (
+export const MultiSelect: Story = () => (
     <DropdownList
-        label="Select an option"
-        tooltip={{ label: 'Tooltip text content' }}
+        label="Select one or more options"
         hint="Hint"
         options={provinces}
+        multiselect
     />
-);
-
-export const InsideShadowDom: Story = () => (
-    <DropdownList label="Select an option" hint="Hint" options={provinces} />
-);
-InsideShadowDom.decorators = [ShadowDomDecorator];
-
-export const Disabled: Story = () => (
-    <DropdownList label="Select an option" options={provinces} disabled />
-);
-
-export const Invalid: Story = () => (
-    <DropdownList label="Select an option" options={provinces} valid={false} />
-);
-
-export const Required: Story = () => (
-    <DropdownList required label="Select an option" options={provinces} />
 );
 
 export const WithCallback: Story = () => (
@@ -75,80 +58,3 @@ export const WithCallback: Story = () => (
     />
 );
 WithCallback.parameters = rawCodeParameters;
-
-export const WithDefaultValue: Story = () => (
-    <DropdownList label="Select an option" options={provinces} defaultValue="qc" />
-);
-
-export const WithControlledValue: Story = () => {
-    const [value, setValue] = useState<string | undefined>(undefined);
-
-    function handleChange(option: DropdownListOption): void {
-        setValue(option.value);
-    }
-
-    return (
-        <>
-            <DropdownList label="Select an option" options={provinces} onChange={handleChange} value={value} />
-            <Button buttonType="primary" onClick={() => setValue('qc')}>Set value to Quebec</Button>
-        </>
-    );
-};
-
-export const WithoutLabel: Story = () => (
-    <DropdownList options={provinces} />
-);
-
-export const WithDisabledOptions: Story = () => {
-    const disabledOptions = [
-        { value: 'option1', label: 'Option 1', disabled: true },
-        { value: 'option2', label: 'Option 2' },
-        { value: 'option3', label: 'Option 3' },
-        { value: 'option4', label: 'Option 4', disabled: true },
-    ];
-
-    return <DropdownList label="Select an option" options={disabledOptions} />;
-};
-
-export const WithDisabledDefaultOption: Story = () => {
-    const [value, setValue] = useState<string>('');
-
-    function handleChange(option: DropdownListOption): void {
-        setValue(option.value);
-    }
-
-    const options = [
-        { value: '', label: '', disabled: true },
-        { value: 'optionA', label: 'Option A' },
-        { value: 'optionB', label: 'Option B' },
-        { value: 'optionC', label: 'Option C' },
-    ];
-
-    return (
-        <>
-            <DropdownList label="Select an option" options={options} value={value} onChange={handleChange} />
-            <Button buttonType="primary" onClick={() => setValue('')}>Re-select empty option</Button>
-        </>
-    );
-};
-
-export const WithOverflowingText: Story = () => {
-    const longLabel = 'This is option 1, with a very long label that should allow some wrapping in the listbox, '
-        + 'while showcasing an ellipsis in the textbox when selected (you may have to resize your browser window)';
-
-    const options = [
-        { value: 'option1', label: longLabel },
-        { value: 'option2', label: 'This is option 2' },
-    ];
-
-    return <DropdownList label="Select an option" options={options} defaultValue="option1" />;
-};
-
-export const WithMultiselect: Story = () => (
-    <DropdownList
-        label="Select one or more options"
-        hint="Hint"
-        options={provinces}
-        multiselect
-    />
-);
