5b272ab6d8675924d6e440fe0c069c242a842b5a
Genevieve Morency <genevieve.morency@equisoft.com>
2024-04-30 10:48:53 -0400

    docs(storybook): update side drawer

--- packages/storybook/stories/side-drawer.stories.tsx
+++ packages/storybook/stories/side-drawer.stories.tsx
@@ -1,19 +1,7 @@
 import { Button, SideDrawer } from '@equisoft/design-elements-react';
 import { StoryFn as Story } from '@storybook/react';
 import { useState } from 'react';
-import styled from 'styled-components';
 import { rawCodeParameters } from './utils/parameters';
-
-const Box = styled.div`
-    background-color: #094c6c;
-    border-radius: 8px;
-    height: 20vh;
-    margin-bottom: 16px;
-
-    &:nth-child(2n) {
-        background-color: #012639;
-    }
-`;
 
 export default {
     title: 'Components/Side Drawer',
@@ -21,7 +9,7 @@ export default {
     parameters: rawCodeParameters,
 };
 
-export const Normal: Story = () => {
+export const Default: Story = () => {
     const [isDrawerOpen, setDrawerOpen] = useState(false);
 
     return (
@@ -31,80 +19,6 @@ export const Normal: Story = () => {
                 <Button label="Close drawer" buttonType="primary" onClick={() => setDrawerOpen(false)} />
             </SideDrawer>
             <Button label="Click to open side-drawer" buttonType="primary" onClick={() => setDrawerOpen(true)} />
-        </>
-    );
-};
-
-export const WithNestedDrawer: Story = () => {
-    const [isDrawerOpen, setDrawerOpen] = useState(false);
-    const [isNestedDrawerOpen, setNestedDrawerOpen] = useState(false);
-
-    return (
-        <>
-            <SideDrawer open={isDrawerOpen}>
-                <h3>Drawer Content</h3>
-                <SideDrawer open={isNestedDrawerOpen} nested>
-                    <h3>Nested Drawer Content</h3>
-                    <Button label="Close drawer" buttonType="primary" onClick={() => setNestedDrawerOpen(false)} />
-                </SideDrawer>
-                <Button label="Open nested drawer" buttonType="primary" onClick={() => setNestedDrawerOpen(true)} />
-                <br />
-                <Button label="Close drawer" buttonType="primary" onClick={() => setDrawerOpen(false)} />
-            </SideDrawer>
-            <Button
-                label="Click to open side-drawer"
-                buttonType="primary"
-                onClick={() => setDrawerOpen(true)}
-            />
-        </>
-    );
-};
-
-export const Scrollable: Story = () => {
-    const [isDrawerOpen, setDrawerOpen] = useState(false);
-
-    return (
-        <>
-            <SideDrawer open={isDrawerOpen}>
-                <h3>Drawer with scrollable content</h3>
-                <div>
-                    {[...Array(6).keys()].map((el) => <Box key={el} />)}
-                </div>
-                <Button label="Close drawer" buttonType="primary" onClick={() => setDrawerOpen(false)} />
-            </SideDrawer>
-            <Button label="Click to open side-drawer" buttonType="primary" onClick={() => setDrawerOpen(true)} />
-        </>
-    );
-};
-
-export const LeftOrigin: Story = () => {
-    const [isDrawerOpen, setDrawerOpen] = useState(false);
-
-    return (
-        <>
-            <SideDrawer open={isDrawerOpen} drawerOrigin="left">
-                <h3>Drawer Content</h3>
-                <Button label="Close drawer" buttonType="primary" onClick={() => setDrawerOpen(false)} />
-            </SideDrawer>
-            <Button label="Click to open side-drawer" buttonType="primary" onClick={() => setDrawerOpen(true)} />
-        </>
-    );
-};
-
-export const VariableWidth: Story = () => {
-    const [isDrawerOpen, setDrawerOpen] = useState(false);
-
-    return (
-        <>
-            <SideDrawer open={isDrawerOpen} width="50%">
-                <h3>Drawer content</h3>
-                <Button label="Close drawer" buttonType="primary" onClick={() => setDrawerOpen(false)} />
-            </SideDrawer>
-            <Button
-                label="Click to open side-drawer"
-                buttonType="primary"
-                onClick={() => setDrawerOpen(true)}
-            />
         </>
     );
 };
