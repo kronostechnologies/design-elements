fd80ce4e0295da08212e4a9bf536c2623b5a0d1e
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-05-03 10:47:49 -0400

    chore(link): adds controls

--- packages/storybook/stories/link.stories.tsx
+++ packages/storybook/stories/link.stories.tsx
@@ -1,14 +1,26 @@
 import { RouteLink } from '@equisoft/design-elements-react';
-import { StoryFn as Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { NavLink } from 'react-router-dom';
 import { RouterDecorator } from './utils/router-decorator';
 
-export default {
-    title: 'Components/Link',
+const RouteLinkMeta: Meta<typeof RouteLink> = {
+    title: 'Components/Links/Route Link',
     component: RouteLink,
-    decorators: [RouterDecorator],
+    render: (args) => (
+        <RouteLink
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...args}
+            routerLink={NavLink}
+            href="/story1"
+            label="Route Link"
+        />
+    ),
 };
 
-export const Default: Story = () => (
-    <RouteLink routerLink={NavLink} href="/story1" label="Route Link" />
-);
+export default RouteLinkMeta;
+type Story = StoryObj<typeof RouteLink>;
+
+export const Default: Story = {
+    ...RouteLinkMeta,
+};
+Default.decorators = [RouterDecorator];
