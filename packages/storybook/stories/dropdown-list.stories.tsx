881fad83e644ac2c9d39e3abfca41c86cbda3d0c
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-05-03 14:10:18 -0400

    chore(dropdown-list): add controls

--- packages/storybook/stories/dropdown-list.stories.tsx
+++ packages/storybook/stories/dropdown-list.stories.tsx
@@ -1,5 +1,5 @@
 import { DropdownList, DropdownListOption } from '@equisoft/design-elements-react';
-import { StoryFn as Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import styled from 'styled-components';
 import { decorateWith } from './utils/decorator';
 import { rawCodeParameters } from './utils/parameters';
@@ -7,6 +7,17 @@ import { rawCodeParameters } from './utils/parameters';
 const Container = styled.div`
     height: 260px;
 `;
+
+const longLabel = 'This is option 1, with a very long label that should allow some wrapping in the listbox, '
+    + 'while showcasing an ellipsis in the textbox when selected (you may have to resize your browser window)'
+    + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
 const provinces = [
     { value: 'ab', label: 'Alberta' },
@@ -22,37 +33,59 @@ const provinces = [
     { value: 'qc', label: 'Quebec' },
     { value: 'sk', label: 'Saskatchewan' },
     { value: 'yt', label: 'Yukon' },
+    { value: 'long', label: longLabel },
 ];
 
-export default {
+const DropdownListMeta: Meta<typeof DropdownList> = {
     title: 'Components/Dropdown List',
     component: DropdownList,
     decorators: [decorateWith(Container)],
+    args: {
+        label: 'Select an option',
+        hint: 'Hint',
+        tooltip: { label: 'This is a tooltip.' },
+    },
+    argTypes: {
+        options: {
+            control: { type: null },
+        },
+        onChange: {
+            control: { type: null },
+        },
+        tooltip: {
+            control: { type: null },
+        },
+    },
+    render: (args) => (
+        <DropdownList
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...args}
+            data-testid="some-data-test-id"
+            options={provinces}
+        />
+    ),
 };
 
-export const Default: Story = () => (
-    <DropdownList
-        data-testid="some-data-test-id"
-        label="Select an option"
-        hint="Hint"
-        options={provinces}
-    />
-);
+export default DropdownListMeta;
+type Story = StoryObj<typeof DropdownList>;
 
-export const MultiSelect: Story = () => (
-    <DropdownList
-        label="Select one or more options"
-        hint="Hint"
-        options={provinces}
-        multiselect
-    />
-);
+export const Default: Story = {
+    ...DropdownListMeta,
+};
 
-export const WithCallback: Story = () => (
-    <DropdownList
-        label="Select an option"
-        options={provinces}
-        onChange={(option: DropdownListOption) => console.info(`Label: ${option.label} | Value: ${option.value}`)}
-    />
-);
+export const MultiSelect: Story = {
+    ...DropdownListMeta,
+    args: {
+        label: 'Select multiple options',
+        multiselect: true,
+    },
+};
+
+export const WithCallback: Story = {
+    ...DropdownListMeta,
+    args: {
+        label: 'Select an option',
+        onChange: (option: DropdownListOption) => console.info(`Label: ${option.label} | Value: ${option.value}`),
+    },
+};
 WithCallback.parameters = rawCodeParameters;
