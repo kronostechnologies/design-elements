f2976ac10026a4c9346c060ce220b1bd97ee14e6
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-05-09 09:16:28 -0400

    chore(alias-tokens): link radio card tokens

--- packages/react/src/themes/tokens/component/radio-card-tokens.ts
+++ packages/react/src/themes/tokens/component/radio-card-tokens.ts
@@ -21,18 +21,18 @@ export type RadioCardTokenMap = {
 };
 
 export const defaultRadioCardTokens: RadioCardTokenMap = {
-    'radio-card-background-color': 'color-white',
-    'radio-card-text-color': 'color-black',
-    'radio-card-border-color': 'color-black',
+    'radio-card-background-color': 'color-input-bg',
+    'radio-card-border-color': 'color-input-border',
+    'radio-card-text-color': 'color-content',
 
-    'radio-card-hover-background-color': 'color-neutral-15',
-    'radio-card-hover-border-color': 'color-black',
-    'radio-card-hover-text-color': 'color-black',
+    'radio-card-hover-background-color': 'color-input-bg-hover',
+    'radio-card-hover-border-color': 'color-input-border-hover',
+    'radio-card-hover-text-color': 'color-content-hover',
 
-    'radio-card-disabled-background-color': 'color-neutral-05',
-    'radio-card-disabled-border-color': 'color-neutral-15',
-    'radio-card-disabled-text-color': 'color-neutral-30',
+    'radio-card-disabled-background-color': 'color-input-bg-disabled',
+    'radio-card-disabled-border-color': 'color-input-border-disabled',
+    'radio-card-disabled-text-color': 'color-input-content-disabled',
 
-    'radio-card-selected-background-color': 'color-brand-05',
-    'radio-card-selected-border-color': 'color-brand-50',
+    'radio-card-selected-background-color': 'color-input-bg-selected',
+    'radio-card-selected-border-color': 'color-input-border-selected',
 };
