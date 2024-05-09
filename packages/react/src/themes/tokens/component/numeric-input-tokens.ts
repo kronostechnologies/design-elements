fc1f87b87408fe2dcf6b4ca85eb546f06a663a73
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-05-09 09:06:35 -0400

    chore(alias-tokens): link numeric tokens

--- packages/react/src/themes/tokens/component/numeric-input-tokens.ts
+++ packages/react/src/themes/tokens/component/numeric-input-tokens.ts
@@ -16,10 +16,11 @@ export type NumericInputTokenMap = {
 };
 
 export const defaultNumericInputTokens: NumericInputTokenMap = {
-    'numeric-input-background-color': 'color-white',
-    'numeric-input-border-color': 'color-neutral-65',
-    'numeric-input-disabled-adornment-text-color': 'color-neutral-30',
-    'numeric-input-disabled-background-color': 'color-neutral-05',
-    'numeric-input-disabled-border-color': 'color-neutral-15',
-    'numeric-input-error-border-color': 'color-alert-50',
+    'numeric-input-background-color': 'color-input-bg',
+    'numeric-input-border-color': 'color-input-border',
+    'numeric-input-error-border-color': 'color-input-border-error',
+
+    'numeric-input-disabled-adornment-text-color': 'color-input-content-disabled',
+    'numeric-input-disabled-background-color': 'color-input-bg-disabled',
+    'numeric-input-disabled-border-color': 'color-input-border-disabled',
 };
