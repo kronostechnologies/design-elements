823b2eb2101d32f18075fb0db050b42bb0fe5169
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-05-09 09:58:57 -0400

    chore(alias-tokens): link toggle switch tokens

--- packages/react/src/themes/tokens/component/toggle-switch-tokens.ts
+++ packages/react/src/themes/tokens/component/toggle-switch-tokens.ts
@@ -20,14 +20,16 @@ export type ToggleSwitchTokenMap = {
 };
 
 export const defaultToggleSwitchTokens: ToggleSwitchTokenMap = {
-    'toggle-switch-background-color': 'color-neutral-50',
-    'toggle-switch-border-color': 'color-neutral-30',
-    'toggle-switch-disabled-background-color': 'color-neutral-15',
-    'toggle-switch-disabled-border-color': 'color-neutral-15',
-    'toggle-switch-disabled-toggled-background-color': 'color-success-20',
-    'toggle-switch-disabled-toggled-border-color': 'color-success-20',
-    'toggle-switch-toggled-background-color': 'color-success-50',
-    'toggle-switch-toggled-border-color': 'color-success-50',
-    'toggle-switch-knob-background-color': 'color-white',
-    'toggle-switch-label-text-color': 'color-black',
+    'toggle-switch-background-color': 'color-bg-neutral-bold',
+    'toggle-switch-border-color': 'color-bg-neutral-bold',
+    'toggle-switch-label-text-color': 'color-content',
+    'toggle-switch-knob-background-color': 'color-input-bg',
+
+    'toggle-switch-toggled-background-color': 'color-feedback-bg-success-bold',
+    'toggle-switch-toggled-border-color': 'color-feedback-bg-success-bold',
+
+    'toggle-switch-disabled-background-color': 'color-bg-neutral-bold-disabled',
+    'toggle-switch-disabled-border-color': 'color-bg-neutral-bold-disabled',
+    'toggle-switch-disabled-toggled-background-color': 'color-feedback-bg-success-bold-disabled',
+    'toggle-switch-disabled-toggled-border-color': 'color-feedback-bg-success-bold-disabled',
 };
