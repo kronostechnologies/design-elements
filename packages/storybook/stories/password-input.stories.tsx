97f5059e4117462c57d62d1bda414031434889aa
matique77 <mathieu.morin@equisoft.com>
2024-05-09 09:14:21 -0400

    feat(password-input,storybook): add controls to password input

--- packages/storybook/stories/password-input.stories.tsx
+++ packages/storybook/stories/password-input.stories.tsx
@@ -1,18 +1,31 @@
-import { StoryFn as Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { PasswordInput } from '@equisoft/design-elements-react';
 
-export default {
+const PasswordInputMeta: Meta<typeof PasswordInput> = {
     title: 'Components/Password Input',
     component: PasswordInput,
-};
-
-export const Default: Story = () => (
-    <PasswordInput
-        label="Password Label"
-        defaultValue="Pass123"
-        onChange={(password, event) => {
+    args: {
+        label: 'Password Label',
+        defaultValue: 'Pass123',
+        onChange: (password, event) => {
             console.info(password);
             console.info(event);
-        }}
-    />
-);
+        },
+    },
+    argTypes: {
+        onBlur: {
+            control: { type: null },
+        },
+        onFocus: {
+            control: { type: null },
+        },
+        onChange: {
+            control: { type: null },
+        },
+    },
+};
+
+export default PasswordInputMeta;
+type Story = StoryObj<typeof PasswordInput>;
+
+export const Default: Story = { ...PasswordInputMeta };
