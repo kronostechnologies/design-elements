3470f280676c620c65b6a9a41ca26cd926674ebe
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-05-09 08:51:56 -0400

    chore(alias-tokens): link global banner tokens

--- packages/react/src/themes/tokens/component/global-banner-tokens.ts
+++ packages/react/src/themes/tokens/component/global-banner-tokens.ts
@@ -46,40 +46,55 @@ export type GlobalBannerTokenMap = {
 };
 
 export const defaultGlobalBannerTokens: GlobalBannerTokenMap = {
-    'global-banner-neutral-background-color': 'color-neutral-65',
-    'global-banner-neutral-text-color': 'color-white',
-    'global-banner-neutral-action-button-border-color': 'color-white',
-    'global-banner-neutral-action-button-text-color': 'color-white',
-    'global-banner-neutral-action-button-hover-border-color': 'color-neutral-30',
-    'global-banner-neutral-action-button-hover-text-color': 'color-neutral-30',
+    /**
+     * neutral
+     */
+    'global-banner-neutral-background-color': 'color-bg-neutral-bold',
     'global-banner-neutral-dismiss-button-background-color': 'transparent-100',
-    'global-banner-neutral-dismiss-button-text-color': 'color-white',
-    'global-banner-neutral-dismiss-button-hover-background-color': 'color-neutral-80',
-    'global-banner-neutral-dismiss-button-hover-text-color': 'color-white',
-    'global-banner-alert-background-color': 'color-alert-50',
-    'global-banner-alert-text-color': 'color-white',
-    'global-banner-alert-action-button-border-color': 'color-white',
-    'global-banner-alert-action-button-text-color': 'color-white',
-    'global-banner-alert-action-button-hover-border-color': 'color-alert-20',
-    'global-banner-alert-action-button-hover-text-color': 'color-alert-20',
-    'global-banner-discovery-background-color': 'color-discovery-50',
-    'global-banner-discovery-text-color': 'color-white',
-    'global-banner-discovery-action-button-border-color': 'color-white',
-    'global-banner-discovery-action-button-text-color': 'color-white',
-    'global-banner-discovery-action-button-hover-border-color': 'color-discovery-20',
-    'global-banner-discovery-action-button-hover-text-color': 'color-discovery-20',
+    'global-banner-neutral-dismiss-button-hover-background-color': 'transparent-dark-50',
+    'global-banner-neutral-text-color': 'color-content-inverse',
+    'global-banner-neutral-action-button-text-color': 'color-content-inverse',
+    'global-banner-neutral-dismiss-button-text-color': 'color-content-inverse',
+    'global-banner-neutral-dismiss-button-hover-text-color': 'color-content-inverse',
+    'global-banner-neutral-action-button-border-color': 'color-border-inverse',
+    'global-banner-neutral-action-button-hover-border-color': 'transparent-light-50',
+    'global-banner-neutral-action-button-hover-text-color': 'transparent-light-50',
+
+    /**
+     * discovery
+     */
+    'global-banner-discovery-background-color': 'color-feedback-bg-discovery-bold',
     'global-banner-discovery-dismiss-button-background-color': 'transparent-100',
-    'global-banner-discovery-dismiss-button-text-color': 'color-white',
-    'global-banner-discovery-dismiss-button-hover-background-color': 'color-discovery-70',
-    'global-banner-discovery-dismiss-button-hover-text-color': 'color-white',
-    'global-banner-warning-background-color': 'color-warning-50',
-    'global-banner-warning-text-color': 'color-neutral-90',
-    'global-banner-warning-action-button-border-color': 'color-neutral-90',
-    'global-banner-warning-action-button-text-color': 'color-neutral-90',
-    'global-banner-warning-action-button-hover-border-color': 'color-warning-70',
-    'global-banner-warning-action-button-hover-text-color': 'color-warning-70',
+    'global-banner-discovery-dismiss-button-hover-background-color': 'transparent-dark-50',
+    'global-banner-discovery-text-color': 'color-content-inverse',
+    'global-banner-discovery-action-button-text-color': 'color-content-inverse',
+    'global-banner-discovery-dismiss-button-text-color': 'color-content-inverse',
+    'global-banner-discovery-dismiss-button-hover-text-color': 'color-content-inverse',
+    'global-banner-discovery-action-button-border-color': 'color-border-inverse',
+    'global-banner-discovery-action-button-hover-border-color': 'transparent-light-50',
+    'global-banner-discovery-action-button-hover-text-color': 'transparent-light-50',
+
+    /**
+     * warning
+     */
+    'global-banner-warning-background-color': 'color-feedback-bg-warning-bold',
     'global-banner-warning-dismiss-button-background-color': 'transparent-100',
-    'global-banner-warning-dismiss-button-text-color': 'color-neutral-90',
-    'global-banner-warning-dismiss-button-hover-background-color': 'color-warning-60',
-    'global-banner-warning-dismiss-button-hover-text-color': 'color-neutral-90',
+    'global-banner-warning-dismiss-button-hover-background-color': 'transparent-dark-30',
+    'global-banner-warning-text-color': 'color-content',
+    'global-banner-warning-action-button-text-color': 'color-content',
+    'global-banner-warning-dismiss-button-hover-text-color': 'color-content',
+    'global-banner-warning-dismiss-button-text-color': 'color-content',
+    'global-banner-warning-action-button-border-color': 'color-content',
+    'global-banner-warning-action-button-hover-border-color': 'transparent-dark-60',
+    'global-banner-warning-action-button-hover-text-color': 'transparent-dark-60',
+
+    /**
+     * alert
+     */
+    'global-banner-alert-background-color': 'color-feedback-bg-alert-bold',
+    'global-banner-alert-text-color': 'color-content-inverse',
+    'global-banner-alert-action-button-text-color': 'color-content-inverse',
+    'global-banner-alert-action-button-border-color': 'color-border-inverse',
+    'global-banner-alert-action-button-hover-border-color': 'transparent-light-50',
+    'global-banner-alert-action-button-hover-text-color': 'transparent-light-50',
 };
