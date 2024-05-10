010daf6ddfcff069faed6edc293daa3230b6f116
Genevieve Morency <genevieve.morency@equisoft.com>
2024-05-10 10:22:14 -0400

    docs(storybook): update combobox

--- packages/storybook/stories/combobox.stories.tsx
+++ packages/storybook/stories/combobox.stories.tsx
@@ -4,7 +4,6 @@ import { StoryFn as Story } from '@storybook/react';
 import styled from 'styled-components';
 import { decorateWith } from './utils/decorator';
 import { rawCodeParameters } from './utils/parameters';
-import { ShadowDomDecorator } from './utils/shadow-dom-decorator';
 
 const Container = styled.div`
     height: 240px;
@@ -32,7 +31,7 @@ export default {
     decorators: [decorateWith(Container)],
 };
 
-export const Normal: Story = () => (
+export const Default: Story = () => (
     <Combobox
         data-testid="some-data-test-id"
         label="Select an option"
@@ -42,16 +41,7 @@ export const Normal: Story = () => (
     />
 );
 
-export const WithInlineAutocomplete: Story = () => (
-    <Combobox
-        label="Select an option"
-        hint="Hint"
-        inlineAutoComplete
-        options={provinces}
-    />
-);
-
-export const AllowCustomValue: Story = () => (
+export const CustomValue: Story = () => (
     <Combobox
         allowCustomValue
         label="Select an option"
@@ -59,42 +49,6 @@ export const AllowCustomValue: Story = () => (
         options={provinces}
     />
 );
-
-export const WithTooltip: Story = () => (
-    <Combobox
-        label="Select an option"
-        tooltip={{ label: 'Tooltip text content' }}
-        hint="Hint"
-        options={provinces}
-    />
-);
-
-export const InsideShadowDom: Story = () => (
-    <Combobox label="Select an option" hint="Hint" options={provinces} />
-);
-InsideShadowDom.decorators = [ShadowDomDecorator];
-
-export const Disabled: Story = () => (
-    <Combobox label="Select an option" options={provinces} disabled />
-);
-
-export const RequiredWithValidationError: Story = () => {
-    const [value, setValue] = useState<string | undefined>(undefined);
-    const [valid, setValid] = useState(true);
-
-    return (
-        <>
-            <Combobox
-                label="Select an option"
-                options={provinces}
-                onChange={(newValue) => { setValid(true); setValue(newValue); }}
-                required
-                valid={valid}
-            />
-            <Button buttonType="primary" onClick={() => setValid(!!value)}>Submit</Button>
-        </>
-    );
-};
 
 export const WithCallback: Story = () => {
     const [output, setOutput] = useState('');
@@ -114,41 +68,6 @@ export const WithCallback: Story = () => {
     );
 };
 WithCallback.parameters = rawCodeParameters;
-
-export const WithDefaultValue: Story = () => (
-    <Combobox label="Select an option" options={provinces} defaultValue="Quebec" />
-);
-
-export const WithControlledValue: Story = () => {
-    const [value, setValue] = useState<string | undefined>(undefined);
-
-    function handleChange(newValue: string): void {
-        setValue(newValue);
-    }
-
-    return (
-        <>
-            <Combobox
-                label="Select an option"
-                options={provinces}
-                onChange={handleChange}
-                value={value}
-            />
-            <Button buttonType="primary" onClick={() => setValue('Quebec')}>Set value to Quebec</Button>
-        </>
-    );
-};
-
-export const WithDisabledOptions: Story = () => {
-    const disabledOptions = [
-        { value: 'option1', label: 'Option 1', disabled: true },
-        { value: 'option2', label: 'Option 2' },
-        { value: 'option3', label: 'Option 3' },
-        { value: 'option4', label: 'Option 4', disabled: true },
-    ];
-
-    return <Combobox label="Select an option" options={disabledOptions} />;
-};
 
 export const UsingAsyncDataSource: Story = () => {
     const [options, setOptions] = useState<ComboboxOption[]>([]);
