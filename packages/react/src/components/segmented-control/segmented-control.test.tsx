4a5d8818640d345088a417cc69ce6325380acd82
matique77 <mathieu.morin@equisoft.com>
2024-05-14 14:39:46 -0400

    chore(segmented-control): rename toggle button group to segmented control

--- packages/react/src/components/segmented-control/segmented-control.test.tsx
+++ packages/react/src/components/segmented-control/segmented-control.test.tsx
@@ -1,7 +1,7 @@
 import { shallow } from 'enzyme';
 import { getByTestId } from '../../test-utils/enzyme-selectors';
 import { renderWithProviders } from '../../test-utils/renderer';
-import { ToggleButtonGroup } from './toggle-button-group';
+import { SegmentedControl } from './segmented-control';
 
 const buttonGroup = [
     { label: 'Option 1', value: 'option1' },
@@ -10,24 +10,24 @@ const buttonGroup = [
     { label: 'Option 4', value: 'option4' },
 ];
 
-describe('ToggleButtonGroup', () => {
+describe('SegmentedControl', () => {
     test('onClick callback is called when clicked', () => {
         const callback = jest.fn();
-        const wrapper = shallow(<ToggleButtonGroup onClick={callback} buttonGroup={buttonGroup} groupName="Test1" />);
+        const wrapper = shallow(<SegmentedControl onClick={callback} buttonGroup={buttonGroup} groupName="Test1" />);
 
         getByTestId(wrapper, 'test-toggle-button-2').simulate('click', { currentTarget: { value: 'test' } });
         expect(callback).toHaveBeenCalled();
     });
 
     test('Is default pressed', () => {
-        const wrapper = shallow(<ToggleButtonGroup buttonGroup={buttonGroup} groupName="Test2" />);
+        const wrapper = shallow(<SegmentedControl buttonGroup={buttonGroup} groupName="Test2" />);
 
         expect(getByTestId(wrapper, 'test-toggle-button-1').prop('pressed')).toBe(true);
     });
 
     test('Matches snapshot (desktop)', () => {
         const tree = renderWithProviders(
-            <ToggleButtonGroup buttonGroup={buttonGroup} groupName="Test4" />,
+            <SegmentedControl buttonGroup={buttonGroup} groupName="Test4" />,
             'desktop',
         );
 
@@ -36,7 +36,7 @@ describe('ToggleButtonGroup', () => {
 
     test('Matches snapshot (mobile)', () => {
         const tree = renderWithProviders(
-            <ToggleButtonGroup buttonGroup={buttonGroup} groupName="Test4" />,
+            <SegmentedControl buttonGroup={buttonGroup} groupName="Test4" />,
             'mobile',
         );
 
--- packages/storybook/stories/segmented-control.stories.tsx
+++ packages/storybook/stories/segmented-control.stories.tsx
@@ -1,44 +1,44 @@
-import { ToggleButtonGroup } from '@equisoft/design-elements-react';
-import { StoryFn as Story } from '@storybook/react';
+import { SegmentedControl } from '@equisoft/design-elements-react';
+import { Meta, StoryObj } from '@storybook/react';
 import { MouseEvent } from 'react';
 import { rawCodeParameters } from './utils/parameters';
 
-const buttonGroup = [
-    { label: 'Option 1', value: 'option1' },
-    { label: 'Option 2', value: 'option2' },
-    { label: 'Option 3', value: 'option3' },
-    { label: 'Option 4', value: 'option4' },
-];
-
-const defaultCheckedGroup = [
-    { label: 'Option 1', value: 'option1' },
-    { label: 'Option 2', value: 'option2', defaultPressed: true },
-    { label: 'Option 3', value: 'option3' },
-    { label: 'Option 4', value: 'option4' },
-];
-
-const disabledGroup = [
-    { label: 'Option 1', value: 'option1', disabled: true },
-    { label: 'Option 2', value: 'option2' },
-    { label: 'Option 3', value: 'option3', disabled: true },
-    { label: 'Option 4', value: 'option4' },
-];
-
-export default {
+const SegmentedControlMeta: Meta<typeof SegmentedControl> = {
     title: 'Components/Segmented Control',
-    component: ToggleButtonGroup,
-    tags: ['autodocs'],
+    component: SegmentedControl,
+    argTypes: {
+        onClick: {
+            control: { disable: true },
+        },
+    },
 };
 
-export const Default: Story = () => (
-    <ToggleButtonGroup groupName="Story2" buttonGroup={defaultCheckedGroup} />
-);
+export default SegmentedControlMeta;
 
-export const WithCallback: Story = () => (
-    <ToggleButtonGroup
-        groupName="Test5"
-        buttonGroup={buttonGroup}
-        onClick={(event: MouseEvent<HTMLButtonElement>) => console.info(`Toggled button value: ${event.currentTarget.value}`)}
-    />
-);
-WithCallback.parameters = rawCodeParameters;
+type Story = StoryObj<typeof SegmentedControl>;
+
+export const Default: Story = {
+    args: {
+        buttonGroup: [
+            { label: 'Option 1', value: 'option1' },
+            { label: 'Option 2', value: 'option2' },
+            { label: 'Option 3', value: 'option3' },
+            { label: 'Option 4', value: 'option4' },
+        ],
+        groupName: 'Story1',
+    },
+};
+
+export const WithCallback: Story = {
+    parameters: rawCodeParameters,
+    args: {
+        buttonGroup: [
+            { label: 'Option 1', value: 'option1' },
+            { label: 'Option 2', value: 'option2', defaultPressed: true },
+            { label: 'Option 3', value: 'option3' },
+            { label: 'Option 4', value: 'option4' },
+        ],
+        groupName: 'Story2',
+        onClick: (event: MouseEvent<HTMLButtonElement>) => console.info(`Toggled button value: ${event.currentTarget.value}`),
+    },
+};
