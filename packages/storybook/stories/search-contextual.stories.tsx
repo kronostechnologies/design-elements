40d3f64be095d71875b12c95e89feb017addef5b
matique77 <mathieu.morin@equisoft.com>
2024-05-14 13:48:56 -0400

    feat(search-contextual,storybook): add controls

--- packages/storybook/stories/search-contextual.stories.tsx
+++ packages/storybook/stories/search-contextual.stories.tsx
@@ -1,23 +1,36 @@
 import { SearchContextual } from '@equisoft/design-elements-react';
-import { StoryFn as Story } from '@storybook/react';
-import { useState } from 'react';
+import { Meta, StoryObj } from '@storybook/react';
 import { rawCodeParameters } from './utils/parameters';
 
-export default {
+const SearchContextualMeta: Meta<typeof SearchContextual> = {
     title: 'Components/Search Contextual',
     component: SearchContextual,
+    argTypes: {
+        onChange: {
+            control: { type: null },
+        },
+        onInputFocus: {
+            control: { type: null },
+        },
+        onReset: {
+            control: { type: null },
+        },
+        onSearch: {
+            control: { type: null },
+        },
+    },
 };
 
-export const Default: Story = () => (
-    <SearchContextual />
-);
+export default SearchContextualMeta;
+type Story = StoryObj<typeof SearchContextual>;
 
-export const EventCallbacks: Story = () => (
-    <SearchContextual
-        onChange={(value) => console.info(`New value is : ${value}`)}
-        onSearch={(value) => console.info(`Searching for: ${value}`)}
-        onInputFocus={() => console.info('Input focused')}
-    />
-);
-EventCallbacks.parameters = rawCodeParameters;
+export const Default: Story = { ...SearchContextualMeta };
 
+export const EventCallbacks: Story = {
+    parameters: rawCodeParameters,
+    args: {
+        onChange: (value) => console.info(`New value is : ${value}`),
+        onSearch: (value) => console.info(`Searching for: ${value}`),
+        onInputFocus: () => console.info('Input focused'),
+    },
+};
