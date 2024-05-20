3a38799f093e0eb6e457491113eca6b14b175e7b
matique77 <mathieu.morin@equisoft.com>
2024-05-20 13:35:21 -0400

    fix(search-contextual,storybook): remove spreading

--- packages/storybook/stories/search-contextual.stories.tsx
+++ packages/storybook/stories/search-contextual.stories.tsx
@@ -24,7 +24,7 @@ const SearchContextualMeta: Meta<typeof SearchContextual> = {
 export default SearchContextualMeta;
 type Story = StoryObj<typeof SearchContextual>;
 
-export const Default: Story = { ...SearchContextualMeta };
+export const Default: Story = {};
 
 export const EventCallbacks: Story = {
     parameters: rawCodeParameters,
