4f949e9b29a69ec6f6baf62e25efc8e9dfdf41e1
Savut Sang <savut.sang@equisoft.com>
2024-07-04 11:53:36 -0400

    fix: update tokens

--- packages/react/src/themes/tokens/alias-tokens.ts
+++ packages/react/src/themes/tokens/alias-tokens.ts
@@ -88,6 +88,7 @@ export type AliasTokens =
     | 'color-background-brand-subtle'
     | 'color-background-brand'
     | 'color-background-brand-bold'
+    | 'color-background-indicator-disabled'
     | 'color-background-indicator-selected'
     | 'color-background-indicator-active'
     | 'color-border-brand'
@@ -288,6 +289,7 @@ export const defaultAliasTokens: AliasTokenMap = {
     'color-background-brand-subtle': 'color-brand-20',
     'color-background-brand': 'color-brand-50',
     'color-background-brand-bold': 'color-brand-80',
+    'color-background-indicator-disabled': 'color-neutral-30',
     'color-background-indicator-selected': 'color-brand-50',
     'color-background-indicator-active': 'color-brand-80',
     'color-border-brand': 'color-brand-70',
--- packages/react/src/themes/tokens/component/slider-tokens.ts
+++ packages/react/src/themes/tokens/component/slider-tokens.ts
@@ -21,15 +21,15 @@ export type SliderTokenMap = {
 };
 
 export const defaultSliderTokens: SliderTokenMap = {
-    'slider-rail-background-color': 'color-bg-empty',
-    'slider-rail-disabled-background-color': 'color-bg-neutral-subtle',
-    'slider-rail-mark-background-color': 'color-bg-neutral-bold',
-    'slider-rail-mark-disabled-background-color': 'color-neutral-30', // color-background-indicator-disabled
-    'slider-track-background-color': 'color-bg-brand',
-    'slider-track-disabled-background-color': 'color-bg-neutral-bold-disabled',
-    'slider-track-mark-background-color': 'color-white', // color-control-background
-    'slider-track-mark-disabled-background-color': 'color-white', // color-control-background
-    'slider-thumb-background-color': 'color-white', // color-control-background
+    'slider-rail-background-color': 'color-background-empty',
+    'slider-rail-disabled-background-color': 'color-background-neutral-subtle',
+    'slider-rail-mark-background-color': 'color-background-neutral-bold',
+    'slider-rail-mark-disabled-background-color': 'color-background-indicator-disabled',
+    'slider-track-background-color': 'color-background-brand',
+    'slider-track-disabled-background-color': 'color-background-neutral-bold-disabled',
+    'slider-track-mark-background-color': 'color-control-background',
+    'slider-track-mark-disabled-background-color': 'color-control-background',
+    'slider-thumb-background-color': 'color-control-background',
     'slider-thumb-border-color': 'color-border-brand',
-    'slider-thumb-disabled-border-color': 'color-neutral-30', // color-control-border-disabled
+    'slider-thumb-disabled-border-color': 'color-control-border-disabled',
 };
