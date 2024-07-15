bd6c47ebbfc42ae234cef322f4c7be90b56bb340
Alexandre Brillant <alexandre.brillant@equisoft.com>
2024-07-15 16:53:33 -0400

    feat(Disclosure): remove height css from storybook

--- packages/storybook/stories/disclosure.stories.tsx
+++ packages/storybook/stories/disclosure.stories.tsx
@@ -1,17 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Disclosure } from '@equisoft/design-elements-react';
-import styled from 'styled-components';
-import { decorateWith } from './utils/decorator';
 import { rawCodeParameters } from './utils/parameters';
-
-const Container = styled.div`
-    height: 240px;
-`;
 
 const disclosureMeta: Meta<typeof Disclosure> = {
     title: 'Components/Disclosure',
     component: Disclosure,
-    decorators: [decorateWith(Container)],
     parameters: rawCodeParameters,
 };
 
