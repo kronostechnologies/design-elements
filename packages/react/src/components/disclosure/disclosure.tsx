8232a62219a3e858a625e5914c89a4f92fe6c357
Alexandre Brillant <alexandre.brillant@equisoft.com>
2024-07-18 13:45:17 -0400

    feat(Disclosure): implement disclosure component (#931)
    * feat(Disclosure): implement disclosure component
    
    * feat(Disclosure): add disclosure component tokens and styles
    
    * feat(Disclosure): add z-index 700 to container
    
    * feat(Disclosure): fix content container border
    
    * feat(Disclosure): use disclosure component tokens
    
    * feat(Disclosure): always render children content in dom for aria-controls
    
    * feat(Disclosure): fixup unit tests naming
    
    * feat(Disclosure): fix stylelint
    
    * feat(Disclosure): support IconButton
    
    * feat(Disclosure): support IconButton
    
    * feat(Disclosure): use display none when container is collapsed
    
    * feat(Disclosure): fix stylelint
    
    * feat(Disclosure): remove width and list-style-type in container
    
    * feat(Disclosure): remove height css from storybook
    
    * feat(Disclosure): collapse disclosure on blur
    
    * feat(Disclosure): add snapshot tests

---
+++ packages/react/src/components/disclosure/disclosure.tsx
@@ -0,0 +1,73 @@
+import { FunctionComponent, PropsWithChildren, useState } from 'react';
+import styled from 'styled-components';
+import { useId } from '../../hooks/use-id';
+import { Button, ButtonProps, Size } from '../buttons/button';
+import { IconButton } from '../buttons/icon-button';
+import { IconName } from '../icon/icon';
+
+export type ButtonPropsWithoutOnClick = Omit<ButtonProps, 'onClick'> & {
+    iconName?: IconName;
+    size?: Size;
+}
+
+interface DisclosureWidgetProps {
+    idContent?: string;
+    buttonProps: ButtonPropsWithoutOnClick;
+}
+
+export const Container = styled.div<{ $expanded: boolean; }>`
+    background-color: ${({ theme }) => theme.component['disclosure-background-color']};
+    border:
+        ${({
+            $expanded,
+            theme,
+        }) => (
+            $expanded ? `1px solid ${theme.component['disclosure-border-color']}` : 0
+        )};
+    border-radius: var(--border-radius);
+    box-shadow: 0 10px 20px 0 ${({ theme }) => theme.component['disclosure-box-shadow-color']};
+    color: ${({ theme }) => theme.component['disclosure-text-color']};
+    display: ${({ $expanded }) => ($expanded ? 'block' : 'none')};
+    position: absolute;
+    z-index: 700;
+`;
+
+const DisclosureContainer = styled.div`
+    position: relative;
+`;
+
+export const Disclosure: FunctionComponent<PropsWithChildren<DisclosureWidgetProps>> = ({
+    idContent: providedIdContent,
+    buttonProps,
+    children,
+}) => {
+    const [expanded, setExpanded] = useState<boolean>(false);
+    const idContent = useId(providedIdContent);
+
+    return (
+        <DisclosureContainer>
+            {!buttonProps.iconName && (
+                <Button
+                    {...buttonProps /* eslint-disable-line react/jsx-props-no-spreading */}
+                    onClick={() => setExpanded(!expanded)}
+                    onBlur={() => setExpanded(false)}
+                    aria-expanded={expanded}
+                    aria-controls={idContent}
+                />
+            )}
+            {buttonProps.iconName && (
+                <IconButton
+                    {...buttonProps /* eslint-disable-line react/jsx-props-no-spreading */}
+                    iconName={buttonProps.iconName}
+                    onClick={() => setExpanded(!expanded)}
+                    onBlur={() => setExpanded(false)}
+                    aria-expanded={expanded}
+                    aria-controls={idContent}
+                />
+            )}
+            <Container $expanded={expanded} id={idContent}>
+                {children}
+            </Container>
+        </DisclosureContainer>
+    );
+};
