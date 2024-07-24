a8fa56bcb8f741cef538098adb3e49f522c461de
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-07-23 20:41:21 -0400

    fix(fieldset): bring back legacy legend

---
+++ packages/storybook/stories/legend.stories.tsx
@@ -0,0 +1,29 @@
+import { Legend, LegendItem } from '@equisoft/design-elements-react';
+import { StoryFn as Story } from '@storybook/react';
+
+const legendItems: LegendItem[] = [
+    {
+        name: 'You',
+        description: 'Data from your answers',
+    },
+    {
+        name: 'Equisoft Peers',
+        description: 'Private Equisoft data',
+        color: '#012639',
+    },
+    {
+        name: 'General Peers',
+        description: 'Publicly accessible data',
+        color: '#094C6C',
+    },
+];
+
+export default {
+    title: 'Deprecated/Legend (deprecated)',
+    component: Legend,
+    tags: ['autodocs'],
+};
+
+export const Normal: Story = () => (
+    <Legend items={legendItems} />
+);
