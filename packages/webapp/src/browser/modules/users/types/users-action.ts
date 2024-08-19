9566b3fbdcba98db775086ba862cc79cad55eeb3
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-08-19 11:42:45 -0400

    feat(webapp, users): adds delete action (#968)
    * feat(webapp, users): adds delete action
    
    * fix(webapp): stylelint

--- packages/webapp/src/browser/modules/users/types/users-action.ts
+++ packages/webapp/src/browser/modules/users/types/users-action.ts
@@ -4,10 +4,12 @@ import { TableContextKeys, TableContextProps } from './users-context';
 export enum UsersAction {
     LOAD_USERS = 'LOAD_USERS',
     UPDATE_TABLE = 'UPDATE_TABLE',
+    DELETE = 'DELETE',
 }
 
 export interface UsersActionProps {
     type: string,
+    id?: User['id'],
     users?: User[],
     key?: TableContextKeys,
     value?: TableContextProps[TableContextKeys],
