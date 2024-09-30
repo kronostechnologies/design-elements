a51938192d5adfd2dad2b11b789ec01449069f3a
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-09-30 15:34:51 -0400

    feat(Fieldset): implement fieldset component (#952)
    * feat(fieldset): first iteration
    
    * feat(fieldset): second iteration
    
    * fix(fieldset): stylelint
    
    * fix(fieldset): snapshots
    
    * feat(fieldset): remove deprecated legend component
    
    * fix(fieldset): html validator
    
    * fix(fieldset): bring back legacy legend
    
    * fix(fieldset): bring back legacy legend
    
    * fix(fieldset): bring back legacy legend
    
    * fix(fieldset): bring back legacy legend
    
    * fix(Fieldset): post review changes
    
    * fix(fieldset): remove redundant tests
    
    * fix(Fieldset): post review changes
    
    * fix(Fieldset): update snapshots
    
    * fix(Fieldset): post review changes
    
    * fix(Fieldset): update snapshots
    
    * fix(Legend): remove display table css

---
+++ packages/react/src/components/fieldset/fieldset.tsx
@@ -0,0 +1,40 @@
+import { forwardRef, ReactElement, Ref } from 'react';
+import { useId } from '../../hooks/use-id';
+import { Legend } from './legend';
+import { StyledFieldset } from './styled';
+import { FieldsetProps } from './types';
+
+export const Fieldset = forwardRef(({
+    id: providedId,
+    children,
+    orientation = 'vertical',
+    legend,
+    disabled,
+    ...props
+}: FieldsetProps, ref: Ref<HTMLFieldSetElement>): ReactElement => {
+    const id = useId(providedId);
+    const { text, ...legendProps } = legend || {};
+
+    return (
+        <StyledFieldset
+            data-testid="fieldset"
+            id={id}
+            $orientation={orientation}
+            data-orientation={orientation}
+            aria-disabled={disabled}
+            disabled={disabled}
+            ref={ref}
+            {...props /* eslint-disable-line react/jsx-props-no-spreading */}
+        >
+            {legend && (
+                <Legend
+                    {...legendProps /* eslint-disable-line react/jsx-props-no-spreading */}
+                    id={`${id}-legend`}
+                >
+                    {text}
+                </Legend>
+            )}
+            {children}
+        </StyledFieldset>
+    );
+});
