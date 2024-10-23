4b634c1cc891e62daac311776751e1bf87f27341
LarryMatte <matte.larry@gmail.com>
2024-10-23 16:42:58 -0400

    feat(TextInput, PhoneInput, MoneyInput)!: add readonly state

--- packages/react/src/components/text-input/text-input.tsx
+++ packages/react/src/components/text-input/text-input.tsx
@@ -245,6 +245,7 @@ export const TextInput = forwardRef(({
             noMargin={noMargin}
             fieldId={fieldId}
             label={label}
+            readOnly={readOnly}
             required={required}
             tooltip={tooltip}
             toggletip={toggletip}
