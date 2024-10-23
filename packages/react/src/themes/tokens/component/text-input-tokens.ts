c00bdcdfff732f439f7e8a8b788252c70c104ad3
LarryMatte <matte.larry@gmail.com>
2024-10-23 16:42:58 -0400

    feat(TextInput, PhoneInput, MoneyInput)!: add readonly state

--- packages/react/src/themes/tokens/component/text-input-tokens.ts
+++ packages/react/src/themes/tokens/component/text-input-tokens.ts
@@ -11,18 +11,31 @@ export type TextInputToken =
     | 'text-input-disabled-text-color'
     | 'text-input-placeholder-disabled-text-color'
     | 'text-input-disabled-adornment-text-color'
-    | 'text-input-adornment-color';
+    | 'text-input-adornment-color'
+    | 'text-input-readonly-background-color'
+    | 'text-input-readonly-border-color'
+    | 'text-input-readonly-text-color'
+    | 'text-input-disabled-adornment-color'
+    | 'text-input-readonly-adornment-color';
 
 export const defaultTextInputTokens: ComponentTokenMap<TextInputToken> = {
     'text-input-background-color': 'color-control-background',
     'text-input-disabled-background-color': 'color-control-background-disabled',
+    'text-input-readonly-background-color': 'color-control-background-readonly',
+
     'text-input-border-color': 'color-control-border',
     'text-input-disabled-border-color': 'color-control-border-disabled',
+    'text-input-readonly-border-color': 'color-control-border-readonly',
+
     'text-input-error-border-color': 'color-control-border-error',
     'text-input-text-color': 'color-control-value',
     'text-input-disabled-text-color': 'color-control-value-disabled',
+    'text-input-readonly-text-color': 'color-control-value-readonly',
+
     'text-input-placeholder-text-color': 'color-control-auxiliary',
     'text-input-placeholder-disabled-text-color': 'color-control-auxiliary-disabled',
-    'text-input-disabled-adornment-text-color': 'color-control-auxiliary-disabled',
+
     'text-input-adornment-color': 'color-control-auxiliary',
+    'text-input-disabled-adornment-color': 'color-control-auxiliary-disabled',
+    'text-input-readonly-adornment-color': 'color-control-auxiliary-readonly',
 };
