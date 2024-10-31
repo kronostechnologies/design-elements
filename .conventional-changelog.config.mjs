1b974b21d830341706bdd75b8e1e406843026364
Maxime Meriouma-Caron <meriouma@users.noreply.github.com>
2024-10-31 17:08:02 -0400

    chore: release v9 (#1020)

--- .conventional-changelog.config.mjs
+++ .conventional-changelog.config.mjs
@@ -6,6 +6,7 @@ export default config({
     types: [
         { type: 'feat', section: 'Features', hidden: false },
         { type: 'fix', section: 'Bug Fixes', hidden: false },
+        { type: 'deps', section: 'Dependencies', hidden: false },
         { type: 'docs', section: 'Docs', hidden: false },
         { type: 'chore', section: 'Other', hidden: true },
         { type: 'style', section: 'Other', hidden: true },
