952dc4fb096f381ecd4b9ee2df4fbb7d29bccfdc
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2024-11-20 10:29:02 -0500

    fix(Webapp): post review changes

--- packages/webapp/src/browser/modules/users/components/table/toolbar/CreateUser.component.tsx
+++ packages/webapp/src/browser/modules/users/components/table/toolbar/CreateUser.component.tsx
@@ -1,6 +1,7 @@
 import { Icon, Link } from '@equisoft/design-elements-react';
 import { FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
 import { UserMode } from '../../../types';
 
 export const CreateUser: FunctionComponent = () => {
@@ -9,6 +10,7 @@ export const CreateUser: FunctionComponent = () => {
         <Link
             button={{ buttonType: 'secondary' }}
             href={`/users/${UserMode.CREATE}`}
+            routerLink={NavLink}
         >
             <Icon name="plusSign" />
             {t('create')}
