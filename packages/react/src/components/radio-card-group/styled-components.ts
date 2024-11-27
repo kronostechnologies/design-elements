cc2aa2043481f4dad3e310899b21101bf62d976d
LarryMatte <matte.larry@gmail.com>
2024-11-27 18:05:45 -0500

    fix(RadioCard): fix conflict

--- packages/react/src/components/radio-card-group/styled-components.ts
+++ packages/react/src/components/radio-card-group/styled-components.ts
@@ -71,7 +71,6 @@ export const Label = styled.label<CardProps>`
     background-color: ${getLabelBackgroundColor};
     border: 1px solid ${getLabelBorderColor};
     border-radius: var(--border-radius-2x);
-    box-sizing: border-box;
     color: ${getContentColor};
     display: flex;
     gap: var(--spacing-2x);
