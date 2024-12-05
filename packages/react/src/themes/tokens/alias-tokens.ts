7d2ccdb4a117a4c7610799b4d80776595b15dc6e
Pierre-Yves Lafleur <pierre-yves.lafleur@equisoft.com>
2024-12-05 09:30:17 -0500

    feat(Theme): add text tokens

--- packages/react/src/themes/tokens/alias-tokens.ts
+++ packages/react/src/themes/tokens/alias-tokens.ts
@@ -1,8 +1,11 @@
 import { ColorAliasToken, defaultColorAliasTokens } from './alias/color-tokens';
+import { defaultTextAliasTokens, TextAliasToken } from './alias/text-tokens';
 
 export type AliasToken =
-    | ColorAliasToken;
+    | ColorAliasToken
+    | TextAliasToken;
 
 export const defaultAliasTokens = {
     ...defaultColorAliasTokens,
+    ...defaultTextAliasTokens,
 };
