702c8baf3b6386f1afaaab2f79c65a6a81c546c1
LarryMatte <matte.larry@gmail.com>
2025-02-03 11:10:11 -0500

    fix(Fieldset): fix lint

--- packages/react/src/components/fieldset/styled.ts
+++ packages/react/src/components/fieldset/styled.ts
@@ -2,8 +2,20 @@ import styled from 'styled-components';
 import { FieldsetProps, LegendProps } from './types';
 
 function getFontSize({ $isMobile, $size = 'medium' }: { $isMobile: boolean, $size?: LegendProps['size'] }): string {
-    const mobileSizes = { xxsmall: '0.75rem', xsmall: '0.875rem', small: '1rem', medium: '1.25rem', large: '1.5rem' };
-    const desktopSizes = { xxsmall: '0.75rem', xsmall: '0.875rem', small: '1rem', medium: '1.25rem', large: '1.5rem' };
+    const mobileSizes = { 
+        xxsmall: '0.75rem',
+        xsmall: '0.875rem',
+        small: '1rem',
+        medium: '1.25rem',
+        large: '1.5rem'
+    };
+    const desktopSizes = { 
+        xxsmall: '0.75rem',
+        xsmall: '0.875rem',
+        small: '1rem',
+        medium: '1.25rem',
+        large: '1.5rem'
+    };
     const sizes = $isMobile ? mobileSizes : desktopSizes;
     return sizes[$size] || sizes.medium;
 }
