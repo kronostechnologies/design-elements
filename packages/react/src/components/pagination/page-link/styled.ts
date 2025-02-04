c5743660ad6c292b8e1b88c4576dbce311f7d928
Henri Bernard <132286421+hebernardEquisoft@users.noreply.github.com>
2025-02-04 07:41:13 -0500

    fix(Pagination): stylelint

--- packages/react/src/components/pagination/page-link/styled.ts
+++ packages/react/src/components/pagination/page-link/styled.ts
@@ -8,21 +8,18 @@ function getSelectionSuffix(isSelected: boolean): SelectionSuffix {
 }
 
 export const Page = styled.div<{ isSelected: boolean, isMobile: boolean }>`
+    align-items: center;
+    background-color: ${({ isSelected, theme }) => theme.component[`pagination-page${getSelectionSuffix(isSelected)}-background-color`]};
+    border: ${({ isSelected, theme }) => (isSelected ? `1px solid ${theme.component['pagination-page-selected-border-color']}` : 'none')};
+    border-radius: var(--border-radius-4x);
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    padding: 0.125rem;
     gap: 0;
-
     height: ${({ isMobile }) => (isMobile ? 'var(--size-2x)' : 'var(--size-1halfx)')};
+    justify-content: center;
     min-width: ${({ isMobile }) => (isMobile ? 'var(--size-2x)' : 'var(--size-1halfx)')};
+    padding: 0.125rem;
     width: inherit;
-    
-    background-color: ${({ isSelected, theme }) => theme.component[`pagination-page${getSelectionSuffix(isSelected)}-background-color`]};
-    border-radius: var(--border-radius-4x);
-    border: ${({ isSelected, theme }) => (isSelected ? `1px solid ${theme.component['pagination-page-selected-border-color']}` : 'none')};
     
     ${focus};
 
