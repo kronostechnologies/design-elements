5c17aad1bf71b45701b743063250c3ab1c2369e9
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-02-27 14:26:41 -0500

    fix(NumericInput): impossible de coller du contenu en mode controlled

--- packages/react/src/components/numeric-input/utils.test.ts
+++ packages/react/src/components/numeric-input/utils.test.ts
@@ -1,4 +1,5 @@
-import { cleanIncompleteNumber, isValidValueForInput } from './utils';
+import { type ClipboardEvent } from 'react';
+import { cleanIncompleteNumber, isValidValueForInput, replacePastedValue } from './utils';
 
 describe('NumericInput utils', () => {
     describe('cleanIncompleteNumber', () => {
@@ -19,6 +20,61 @@ describe('NumericInput utils', () => {
             'should match expected (value: $value)',
             ({ value, expected }) => {
                 const result = cleanIncompleteNumber(value);
+                expect(result).toEqual(expected);
+            },
+        );
+    });
+
+    describe('replacePastedValue', () => {
+        const tests = [
+            {
+                description: 'should replace selected text with pasted value',
+                inputValue: '123.45',
+                selectionStart: 1,
+                selectionEnd: 5,
+                pastedValue: '98.7',
+                expected: '198.75',
+            },
+            {
+                description: 'should insert pasted value at cursor position when no text is selected',
+                inputValue: '12345',
+                selectionStart: 2,
+                selectionEnd: 2,
+                pastedValue: '987',
+                expected: '12987345',
+            },
+            {
+                description: 'should replace entire value when all text is selected',
+                inputValue: '12345',
+                selectionStart: 0,
+                selectionEnd: 5,
+                pastedValue: '987',
+                expected: '987',
+            },
+        ];
+
+        test.each(tests)(
+            '$description',
+            ({
+                inputValue,
+                selectionStart,
+                selectionEnd,
+                pastedValue,
+                expected,
+            }) => {
+                const event = {
+                    clipboardData: {
+                        getData: jest.fn().mockReturnValue(pastedValue),
+                    },
+                    currentTarget: {
+                        value: inputValue,
+                        selectionStart,
+                        selectionEnd,
+                    },
+                } as unknown as ClipboardEvent<HTMLInputElement>;
+
+                const result = replacePastedValue(event);
+
                 expect(result).toEqual(expected);
             },
         );
