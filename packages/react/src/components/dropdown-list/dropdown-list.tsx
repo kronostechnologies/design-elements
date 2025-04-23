9be597c630d549cb988253963553f2a62af3c330
ogermain <olivier.germain@equisoft.com>
2025-04-23 18:23:21 -0400

    feat(dropdownlist): add onclose event function for dropdownlist (#1135)

--- packages/react/src/components/dropdown-list/dropdown-list.tsx
+++ packages/react/src/components/dropdown-list/dropdown-list.tsx
@@ -174,6 +174,11 @@ export interface DropdownListProps<M extends boolean | undefined> {
     iconName?: IconName;
 
     /**
+     * OnClose callback function, called when the dropdown is closed
+     */
+    onClose?(): void;
+
+    /**
      * OnChange callback function, invoked when options are selected
      */
     onChange?(option: M extends true ? DropdownListOption[] : DropdownListOption): void;
@@ -192,6 +197,7 @@ export const DropdownList: VoidFunctionComponent<DropdownListProps<boolean | und
     id: providedId,
     label,
     onChange,
+    onClose,
     options,
     name,
     required,
@@ -286,7 +292,8 @@ export const DropdownList: VoidFunctionComponent<DropdownListProps<boolean | und
 
     const closeListbox: () => void = useCallback(() => {
         setOpen(false);
-    }, []);
+        onClose?.();
+    }, [onClose]);
 
     const selectOption: (option: DropdownListOption) => void = useCallback((option) => {
         setSelectedOptions([option]);
