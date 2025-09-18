24fabb489e9add9a2778a2ee7a5f029f5302627d
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-09-18 15:57:35 -0400

    chore: fix deprecations

--- packages/react/src/utils/user.ts
+++ packages/react/src/utils/user.ts
@@ -6,7 +6,7 @@ export function getInitialsFromUsername(username: string): string {
 
     const nameParts: string[] = usernameWithoutNonAlphaCharacters.split(/\s/);
     if (nameParts.length === 1 && usernameWithoutNonAlphaCharacters.length > 0) {
-        return usernameWithoutNonAlphaCharacters.substr(0, 2).toUpperCase();
+        return usernameWithoutNonAlphaCharacters.substring(0, 2).toUpperCase();
     }
 
     return nameParts[0].charAt(0).toUpperCase() + nameParts[nameParts.length - 1].charAt(0).toUpperCase();
