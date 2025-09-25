33f7155b00bdc1299d1230897ea23d91c0f88f47
Maxime Meriouma-Caron <meriouma@users.noreply.github.com>
2025-09-24 22:00:58 -0400

    feat(Icon): added circle-check icon to icon library (#1253) (#1254)
    Co-authored-by: Jean-Simon Garneau <jean-simon.garneau@equisoft.com>

--- packages/react/src/components/icon/icon.tsx
+++ packages/react/src/components/icon/icon.tsx
@@ -8,6 +8,7 @@ import ArrowUp from 'feather-icons/dist/icons/arrow-up.svg';
 import Bell from 'feather-icons/dist/icons/bell.svg';
 import Calendar from 'feather-icons/dist/icons/calendar.svg';
 import Check from 'feather-icons/dist/icons/check.svg';
+import CheckCircle from 'feather-icons/dist/icons/check-circle.svg';
 import ChevronDown from 'feather-icons/dist/icons/chevron-down.svg';
 import ChevronLeft from 'feather-icons/dist/icons/chevron-left.svg';
 import ChevronRight from 'feather-icons/dist/icons/chevron-right.svg';
@@ -87,6 +88,7 @@ const iconMapping = {
     caretDown: CaretDown,
     caretRight: CaretRight,
     check: Check,
+    checkCircle: CheckCircle,
     chevronDown: ChevronDown,
     chevronLeft: ChevronLeft,
     chevronRight: ChevronRight,
