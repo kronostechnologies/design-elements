85873ed79697889d4471ebe4ae38b8f70bffb494
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-10-27 23:00:52 -0400

    feat(Filters): add multi-select filter

--- packages/react/src/components/filters/index.ts
+++ packages/react/src/components/filters/index.ts
@@ -1,3 +1,5 @@
 export { FilterSingle } from './filter-single';
 export type { FilterSingleProps } from './filter-single';
+export { FilterMulti } from './filter-multi';
+export type { FilterMultiProps } from './filter-multi';
 export type { FilterOption } from './filter-option';
