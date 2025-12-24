35e3ce41ac4adde9123eaf283469bc80c676b148
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-24 16:26:27 -0500

    test(Button): rewrite tests with RTL

--- packages/react/src/components/buttons/button.test.tsx
+++ packages/react/src/components/buttons/button.test.tsx
@@ -1,9 +1,11 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { doNothing } from '../../test-utils/callbacks';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { Button } from './button';
 
 describe('Button', () => {
-    test('has primary disabled styles', () => {
+    it('has primary disabled styles', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="primary" disabled label="Primary Button" />,
         );
@@ -11,7 +13,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has primary styles', () => {
+    it('has primary styles', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="primary" label="Primary Button" />,
         );
@@ -19,7 +21,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has primary styles (inverted)', () => {
+    it('has primary styles (inverted)', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="primary" label="Primary Button" inverted />,
         );
@@ -27,7 +29,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has secondary styles', () => {
+    it('has secondary styles', () => {
         const { container } = renderWithProviders(
             <Button
                 onClick={doNothing}
@@ -39,7 +41,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has secondary styles (inverted)', () => {
+    it('has secondary styles (inverted)', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="secondary" label="Secondary Button" inverted />,
         );
@@ -47,7 +49,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has tertiary styles', () => {
+    it('has tertiary styles', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="tertiary" label="Tertiary Button" />,
         );
@@ -55,7 +57,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has tertiary styles (inverted)', () => {
+    it('has tertiary styles (inverted)', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="tertiary" label="Tertiary Button" inverted />,
         );
@@ -63,7 +65,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has destructive styles', () => {
+    it('has destructive styles', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="destructive-primary" label="Destructive Button" />,
         );
@@ -71,7 +73,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has destructive styles (inverted)', () => {
+    it('has destructive styles (inverted)', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="destructive-primary" label="Destructive Button" inverted />,
         );
@@ -79,7 +81,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has destructive-secondary styles', () => {
+    it('has destructive-secondary styles', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="destructive-primary" label="Destructive Button" />,
         );
@@ -87,7 +89,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has destructive-secondary styles (inverted)', () => {
+    it('has destructive-secondary styles (inverted)', () => {
         const { container } = renderWithProviders(
             <Button
                 onClick={doNothing}
@@ -100,7 +102,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has small styles', () => {
+    it('has small styles', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="primary" label="Small Primary Button" size="small" />,
         );
@@ -108,7 +110,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has mobile styles', () => {
+    it('has mobile styles', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="primary" label="Primary Button" />,
             'mobile',
@@ -117,7 +119,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has small styles on mobile', () => {
+    it('has small styles on mobile', () => {
         const { container } = renderWithProviders(
             <Button onClick={doNothing} buttonType="primary" label="Small Primary Button" size="small" />,
             'mobile',
@@ -126,7 +128,7 @@ describe('Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has left and right icons', () => {
+    it('has left and right icons', () => {
         const { container } = renderWithProviders(
             <Button
                 buttonType="primary"
@@ -138,5 +140,97 @@ describe('Button', () => {
         );
 
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('calls onClick callback when clicked', async () => {
+        const callback = jest.fn();
+        renderWithProviders(<Button onClick={callback} buttonType="primary" label="Primary Button" />);
+
+        await userEvent.click(screen.getByRole('button', { name: 'Primary Button' }));
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not call onClick callback when disabled', async () => {
+        const callback = jest.fn();
+        renderWithProviders(
+            <Button
+                onClick={callback}
+                buttonType="primary"
+                disabled
+                label="Primary Button"
+            />,
+        );
+
+        await expect(userEvent.click(screen.getByRole('button', { name: 'Primary Button' }))).toReject();
+        expect(callback).toHaveBeenCalledTimes(0);
+    });
+
+    it('has no tabIndex prop when focusable', () => {
+        renderWithProviders(<Button buttonType="primary" label="Primary Button" />);
+
+        const button = screen.getByRole('button', { name: 'Primary Button' });
+        expect(button).not.toHaveAttribute('tabIndex');
+    });
+
+    it('has tabIndex=-1 when non-focusable', () => {
+        renderWithProviders(<Button buttonType="primary" label="Primary Button" focusable={false} />);
+
+        const button = screen.getByText('Primary Button');
+        expect(button).toHaveAttribute('tabIndex', '-1');
+    });
+
+    it('has focus styles when focusable', () => {
+        renderWithProviders(<Button buttonType="primary" label="Primary Button" />);
+
+        const button = screen.getByRole('button', { name: 'Primary Button' });
+        expect(button).toMatchSnapshot();
+    });
+
+    it('does not have focus styles when non-focusable', () => {
+        renderWithProviders(<Button buttonType="primary" label="Primary Button" focusable={false} />);
+
+        const button = screen.getByText('Primary Button');
+        expect(button).toMatchSnapshot();
+    });
+
+    it('renders icons on left and right', () => {
+        renderWithProviders(
+            <Button
+                buttonType="primary"
+                label="Primary Button"
+                leftIconName="chevronLeft"
+                rightIconName="chevronRight"
+            />,
+        );
+
+        expect(screen.getByTestId('left-icon')).toBeInTheDocument();
+        expect(screen.getByTestId('right-icon')).toBeInTheDocument();
+    });
+
+    it('renders icon on left side only', () => {
+        renderWithProviders(
+            <Button
+                buttonType="primary"
+                label="Primary Button"
+                leftIconName="chevronRight"
+            />,
+        );
+
+        expect(screen.queryByTestId('right-icon')).not.toBeInTheDocument();
+        expect(screen.getByTestId('left-icon')).toBeInTheDocument();
+    });
+
+    it('renders icon on right side only', () => {
+        renderWithProviders(
+            <Button
+                buttonType="primary"
+                label="Primary Button"
+                rightIconName="chevronRight"
+            />,
+        );
+
+        expect(screen.queryByTestId('left-icon')).not.toBeInTheDocument();
+        expect(screen.getByTestId('right-icon')).toBeInTheDocument();
     });
 });
