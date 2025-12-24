7ec59abcaac2dd3c30ba45c56d1cc6f01e36493b
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-24 11:24:29 -0500

    test(Avatar): rewrite tests with RTL

--- packages/react/src/components/avatar/avatar.test.tsx
+++ packages/react/src/components/avatar/avatar.test.tsx
@@ -1,112 +1,119 @@
+import { screen } from '@testing-library/react';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { Avatar } from './avatar';
 
 describe('Avatar', () => {
-    test('Matches Snapshot', () => {
+    it('Matches Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches small avatar Snapshot', () => {
+    it('Matches small avatar Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="small" />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches medium avatar Snapshot', () => {
+    it('Matches medium avatar Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="medium" />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches large avatar Snapshot', () => {
+    it('Matches large avatar Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="large" />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches mobile Snapshot', () => {
+    it('Matches mobile Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches mobile small avatar Snapshot', () => {
+    it('Matches mobile small avatar Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="small" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches mobile medium avatar Snapshot', () => {
+    it('Matches mobile medium avatar Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="medium" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches mobile large avatar Snapshot', () => {
+    it('Matches mobile large avatar Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="large" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches small avatar with image Snapshot', () => {
+    it('Matches small avatar with image Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="small" imgSrc="anImage" />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches medium avatar with image Snapshot', () => {
+    it('Matches medium avatar with image Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="medium" imgSrc="anImage" />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches large avatar with image Snapshot', () => {
+    it('Matches large avatar with image Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="large" imgSrc="anImage" />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches mobile avatar with image Snapshot', () => {
+    it('Matches mobile avatar with image Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" imgSrc="anImage" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches mobile small avatar with image Snapshot', () => {
+    it('Matches mobile small avatar with image Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="small" imgSrc="anImage" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches mobile medium avatar with image Snapshot', () => {
+    it('Matches mobile medium avatar with image Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="medium" imgSrc="anImage" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches mobile large avatar with image Snapshot', () => {
+    it('Matches mobile large avatar with image Snapshot', () => {
         const { container } = renderWithProviders(<Avatar username="AB" size="large" imgSrc="anImage" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Should use user icon when username is empty on desktop', () => {
+    it('Should use user icon when username is empty on desktop', () => {
         const { container } = renderWithProviders(<Avatar username="" />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Should use bigger user icon when username is empty on mobile', () => {
+    it('Should use bigger user icon when username is empty on mobile', () => {
         const { container } = renderWithProviders(<Avatar username="" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Should use user icon when username is undefined', () => {
+    it('Should use user icon when username is undefined', () => {
         const { container } = renderWithProviders(<Avatar />);
 
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('should display expected username initials', () => {
+        renderWithProviders(<Avatar username="John Doe" />);
+
+        expect(screen.getByTestId('avatar-initials')).toHaveTextContent('JD');
     });
 });
