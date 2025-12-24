0c3d2de32d5c26282ac02acfad0a5402d92057fb
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-24 16:57:08 -0500

    test(Card): rewrite tests with RTL

--- packages/react/src/components/card/card.test.tsx
+++ packages/react/src/components/card/card.test.tsx
@@ -1,4 +1,19 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../test-utils/renderer';
+import { Card } from './card';
+
 describe('Card', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('adds data-testid', () => {
+        const testId = 'some-test-id';
+
+        renderWithProviders(<Card data-testid={testId}>Test</Card>);
+
+        expect(screen.getByTestId(testId)).toBeInTheDocument();
+    });
+
+    it('Matches the snapshot', () => {
+        const { container } = renderWithProviders(<Card>Hello World</Card>);
+
+        expect(container).toMatchSnapshot();
     });
 });
