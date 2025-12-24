ada10debd1dbaf29a5da4b6a82e20b9858a5f487
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-24 16:47:19 -0500

    test(IconButton): rewrite tests with RTL

--- packages/react/src/components/buttons/icon-button.test.tsx
+++ packages/react/src/components/buttons/icon-button.test.tsx
@@ -1,8 +1,10 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { IconButton } from './icon-button';
 
 describe('Icon Button', () => {
-    test('Has disabled styles', () => {
+    it('Has disabled styles', () => {
         const { container } = renderWithProviders(
             <IconButton
                 label="home"
@@ -15,7 +17,7 @@ describe('Icon Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Has primary styles', () => {
+    it('Has primary styles', () => {
         const { container } = renderWithProviders(
             <IconButton
                 label="home"
@@ -27,7 +29,7 @@ describe('Icon Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Has secondary styles', () => {
+    it('Has secondary styles', () => {
         const { container } = renderWithProviders(
             <IconButton
                 label="home"
@@ -39,7 +41,7 @@ describe('Icon Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Has tertiary styles', () => {
+    it('Has tertiary styles', () => {
         const { container } = renderWithProviders(
             <IconButton
                 label="home"
@@ -51,7 +53,7 @@ describe('Icon Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Has destructive-secondary styles', () => {
+    it('Has destructive-secondary styles', () => {
         const { container } = renderWithProviders(
             <IconButton
                 label="home"
@@ -63,7 +65,7 @@ describe('Icon Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Has small styles', () => {
+    it('Has small styles', () => {
         const { container } = renderWithProviders(
             <IconButton
                 label="home"
@@ -76,7 +78,7 @@ describe('Icon Button', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Has mobile styles', () => {
+    it('Has mobile styles', () => {
         const { container } = renderWithProviders(
             <IconButton
                 label="home"
@@ -87,5 +89,62 @@ describe('Icon Button', () => {
         );
 
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('onClick callback is called when clicked', async () => {
+        const callback = jest.fn();
+        renderWithProviders(
+            <IconButton label="home" iconName="home" buttonType="primary" onClick={callback} />,
+        );
+
+        await userEvent.click(screen.getByRole('button', { name: 'home' }));
+
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it('onClick callback cannot be called when disabled', async () => {
+        const callback = jest.fn();
+        renderWithProviders(
+            <IconButton label="home" iconName="home" onClick={callback} buttonType="primary" disabled />,
+        );
+
+        await expect(userEvent.click(screen.getByRole('button', { name: 'home' }))).toReject();
+        expect(callback).not.toHaveBeenCalled();
+    });
+
+    it('focusable button has no tabIndex prop', () => {
+        renderWithProviders(<IconButton iconName="home" buttonType="primary" label="home" />);
+
+        expect(screen.getByRole('button', { name: 'home' })).not.toHaveAttribute('tabIndex');
+    });
+
+    it('non-focusable button has tabIndex=-1', () => {
+        renderWithProviders(<IconButton iconName="home" buttonType="primary" label="home" focusable={false} />);
+
+        expect(screen.getByRole('button', { name: 'home' })).toHaveAttribute('tabIndex', '-1');
+    });
+
+    it('focusable button has focus styles', async () => {
+        renderWithProviders(<IconButton iconName="home" buttonType="primary" label="home" />);
+
+        const button = screen.getByRole('button', { name: 'home' });
+        await userEvent.tab();
+
+        expect(button).toHaveFocus();
+        expect(button).toHaveStyleRule('outline', '2px solid #84C6EA', {
+            modifier: ':focus-visible',
+        });
+    });
+
+    it('non-focusable button does not have focus styles', async () => {
+        renderWithProviders(<IconButton iconName="home" buttonType="primary" label="home" focusable={false} />);
+
+        const button = screen.getByRole('button', { name: 'home' });
+        await userEvent.tab();
+
+        expect(button).not.toHaveFocus();
+        expect(button).not.toHaveStyleRule('outline', 'none', {
+            modifier: ':focus',
+        });
     });
 });
