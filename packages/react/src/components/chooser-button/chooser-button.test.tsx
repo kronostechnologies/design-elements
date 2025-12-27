ac7be79ebaf2c34d229c30e5d69eacdf282561e2
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-26 23:17:00 -0500

    test(ChooserButton): rewrite tests with RTL

--- packages/react/src/components/chooser-button/chooser-button.test.tsx
+++ packages/react/src/components/chooser-button/chooser-button.test.tsx
@@ -1,4 +1,42 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithProviders } from '../../test-utils/renderer';
+import { ChooserButton } from './chooser-button';
+
 describe('Chooser Button', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('onChange Callback is called when changed', async () => {
+        const callback = jest.fn();
+        const user = userEvent.setup();
+
+        renderWithProviders(
+            <ChooserButton
+                groupName="maritalStatus"
+                onChange={callback}
+                type="radio"
+                value="test value"
+            >
+                Children
+            </ChooserButton>,
+        );
+
+        await user.click(screen.getByText('Children'));
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('Matches the snapshot', () => {
+        const { asFragment } = renderWithProviders(
+            <ChooserButton
+                groupName="maritalStatus"
+                onChange={jest.fn()}
+                type="radio"
+                value="test value"
+                defaultChecked
+            >
+                Children
+            </ChooserButton>,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
     });
 });
