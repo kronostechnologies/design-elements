3d41e9abf7a9e72e5574c50a34991cdaef926bf3
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-29 18:36:29 -0500

    test(DropdownMenuButton): rewrite tests with RTL

--- packages/react/src/components/dropdown-menu-button/dropdown-menu-button.test.tsx
+++ packages/react/src/components/dropdown-menu-button/dropdown-menu-button.test.tsx
@@ -1,4 +1,5 @@
-import { fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { ReactElement } from 'react';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { type GroupItemProps } from '../dropdown-menu';
@@ -23,21 +24,85 @@ const TestGroups = (): ReactElement<GroupItemProps>[] | ReactElement<GroupItemPr
 );
 
 describe('DropdownMenuButton', () => {
-    test('Matches Snapshot', () => {
-        const { baseElement, getByTestId } = renderWithProviders(
+    it('matches Snapshot', async () => {
+        const user = userEvent.setup();
+        const { baseElement } = renderWithProviders(
             <DropdownMenuButton render={TestGroups} />,
         );
-        const element = getByTestId('menu-button');
-        fireEvent.click(element);
+        const element = screen.getByTestId('menu-button');
+        await user.click(element);
 
         expect(baseElement).toMatchSnapshot();
     });
 
-    test('Matches Snapshot (defaultOpen)', () => {
+    it('matches Snapshot (defaultOpen)', () => {
         const { baseElement } = renderWithProviders(
             <DropdownMenuButton defaultOpen render={TestGroups} />,
         );
 
         expect(baseElement).toMatchSnapshot();
+    });
+
+    it('adds aria-label to menu-button-wrapper when ariaLabel is defined', () => {
+        const ariaLabel = 'test-aria-label';
+        renderWithProviders(<DropdownMenuButton tag="nav" ariaLabel={ariaLabel} render={TestGroups} />);
+
+        expect(screen.getByTestId('dropdown-container')).toHaveAttribute('aria-label', ariaLabel);
+    });
+
+    it('adds aria-label to menu-button when buttonAriaLabel is defined', () => {
+        const ariaLabel = 'test-aria-label';
+        renderWithProviders(<DropdownMenuButton buttonAriaLabel={ariaLabel} render={TestGroups} />);
+
+        expect(screen.getByTestId('menu-button')).toHaveAttribute('aria-label', ariaLabel);
+    });
+
+    it('dropdown-menu is open when defaultOpen prop is set to true', () => {
+        renderWithProviders(<DropdownMenuButton defaultOpen render={TestGroups} />);
+
+        expect(screen.getByTestId('menu-dropdownMenu')).toBeInTheDocument();
+    });
+
+    it('opens dropdown-menu when menu-button is clicked', async () => {
+        const user = userEvent.setup();
+        renderWithProviders(<DropdownMenuButton render={() => <div />} />);
+
+        await user.click(screen.getByTestId('menu-button'));
+
+        expect(screen.getByTestId('menu-dropdownMenu')).toBeInTheDocument();
+    });
+
+    it('should close nav-menu when escape key is pressed in dropdown-menu', async () => {
+        const user = userEvent.setup();
+        renderWithProviders(<DropdownMenuButton defaultOpen render={TestGroups} />);
+
+        const item = screen.getByTestId('listitem-optionA');
+        item.focus();
+        await user.keyboard('{Escape}');
+
+        expect(screen.queryByTestId('menu-dropdownMenu')).not.toBeInTheDocument();
+    });
+
+    it('focuses menu-button when escape key is pressed in dropdown-menu', async () => {
+        const user = userEvent.setup();
+        renderWithProviders(<DropdownMenuButton defaultOpen render={TestGroups} />);
+
+        const item = screen.getByTestId('listitem-optionA');
+        item.focus();
+        await user.keyboard('{Escape}');
+
+        expect(screen.getByTestId('menu-button')).toHaveFocus();
+    });
+
+    it('renders nav container tag when "tag" prop is set to nav', () => {
+        renderWithProviders(<DropdownMenuButton tag="nav" render={TestGroups} />);
+
+        expect(screen.getByTestId('dropdown-container').tagName).toBe('NAV');
+    });
+
+    it('renders div container tag when "tag" prop is set to div', () => {
+        renderWithProviders(<DropdownMenuButton tag="div" render={TestGroups} />);
+
+        expect(screen.getByTestId('dropdown-container').tagName).toBe('DIV');
     });
 });
