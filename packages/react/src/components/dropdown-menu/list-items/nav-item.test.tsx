425620d56808b5a85f31de7809bab68b5064861e
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-29 18:26:54 -0500

    test(DropdownMenu): rewrite tests with RTL

--- packages/react/src/components/dropdown-menu/list-items/nav-item.test.tsx
+++ packages/react/src/components/dropdown-menu/list-items/nav-item.test.tsx
@@ -1,4 +1,17 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../../test-utils/testing-library';
+import { NavItem } from './nav-item';
+
 describe('NavItem', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('displays screen-reader-only text when router link opens in a new tab (target="_blank")', () => {
+        renderWithProviders(<NavItem value="test" href="test" target="_blank" />);
+
+        expect(screen.getByTestId('screen-reader-text')).toBeInTheDocument();
+    });
+
+    it('displays screen-reader-only text when html link opens in a new tab (target="_blank")', () => {
+        renderWithProviders(<NavItem value="test" isHtmlLink href="test" target="_blank" />);
+
+        expect(screen.getByTestId('screen-reader-text')).toBeInTheDocument();
     });
 });
