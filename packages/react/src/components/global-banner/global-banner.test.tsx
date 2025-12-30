2a9ff1c14c5834c1bff57f991c4f7378f026de5f
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-29 21:15:25 -0500

    test(GlobalBanner): rewrite tests with RTL

--- packages/react/src/components/global-banner/global-banner.test.tsx
+++ packages/react/src/components/global-banner/global-banner.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { type ActionButton, GlobalBanner, type GlobalBannerType } from './global-banner';
 
@@ -10,7 +12,7 @@ const bannerTypesArray: GlobalBannerType[] = ['alert', 'warning', 'discovery', '
 
 describe('GlobalBanner', () => {
     bannerTypesArray.forEach((type) => {
-        test(`matches snapshot (desktop, ${type})`, () => {
+        it(`matches snapshot (desktop, ${type})`, () => {
             const { container } = renderWithProviders(
                 <GlobalBanner actionButton={defaultActionButton} dismissable label={type} type={type}>
                     Test content
@@ -21,7 +23,7 @@ describe('GlobalBanner', () => {
             expect(container.firstChild).toMatchSnapshot();
         });
 
-        test(`matches snapshot (mobile, ${type})`, () => {
+        it(`matches snapshot (mobile, ${type})`, () => {
             const { container } = renderWithProviders(
                 <GlobalBanner actionButton={defaultActionButton} dismissable label={type} type={type}>
                     Test content
@@ -30,6 +32,134 @@ describe('GlobalBanner', () => {
             );
 
             expect(container.firstChild).toMatchSnapshot();
+        });
+    });
+
+    it('calls action-button onClick callback when action-button is clicked', async () => {
+        const callback = jest.fn();
+        renderWithProviders(
+            <GlobalBanner
+                actionButton={{
+                    label: 'Test button',
+                    onClick: callback,
+                }}
+                label="Test"
+            >
+                Test
+            </GlobalBanner>,
+        );
+
+        await userEvent.click(screen.getByTestId('action-button'));
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls secondary-action-button onClick callback when secondary-action-button is clicked', async () => {
+        const callback = jest.fn();
+        renderWithProviders(
+            <GlobalBanner
+                secondaryActionButton={{
+                    label: 'Test button',
+                    onClick: callback,
+                }}
+                label="Test"
+            >
+                Test
+            </GlobalBanner>,
+        );
+
+        await userEvent.click(screen.getByTestId('secondary-action-button'));
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('hides the banner when dismiss-button is clicked', async () => {
+        renderWithProviders(
+            <GlobalBanner
+                actionButton={defaultActionButton}
+                label="Test"
+                dismissable
+            >
+                WARNING! test test
+            </GlobalBanner>,
+        );
+
+        await userEvent.click(screen.getByTestId('dismiss-button'));
+
+        expect(screen.queryByTestId('container')).not.toBeInTheDocument();
+    });
+
+    it('calls onDismiss when dismiss-button is clicked', async () => {
+        const onDismiss = jest.fn();
+        renderWithProviders(
+            <GlobalBanner
+                actionButton={defaultActionButton}
+                label="Test"
+                onDismiss={onDismiss}
+                dismissable
+            >
+                WARNING! test test
+            </GlobalBanner>,
+        );
+
+        await userEvent.click(screen.getByTestId('dismiss-button'));
+
+        expect(onDismiss).toHaveBeenCalled();
+    });
+
+    it('does not have dismiss-button when type is alert', () => {
+        renderWithProviders(
+            <GlobalBanner
+                label="Test"
+                type="alert"
+                dismissable
+            >
+                Test content
+            </GlobalBanner>,
+        );
+
+        expect(screen.queryByTestId('dismiss-button')).not.toBeInTheDocument();
+    });
+
+    it('does not have dismiss-button when dismissable is set to false', () => {
+        renderWithProviders(
+            <GlobalBanner
+                label="Test"
+                dismissable={false}
+            >
+                Test content
+            </GlobalBanner>,
+        );
+
+        expect(screen.queryByTestId('dismiss-button')).not.toBeInTheDocument();
+    });
+
+    describe('hidden property', () => {
+        it('hides the component', () => {
+            renderWithProviders(
+                <GlobalBanner
+                    actionButton={defaultActionButton}
+                    label="Test"
+                    hidden
+                >
+                    WARNING! test test
+                </GlobalBanner>,
+            );
+
+            expect(screen.queryByTestId('container')).not.toBeInTheDocument();
+        });
+
+        it('does not hide by default', () => {
+            renderWithProviders(
+                <GlobalBanner
+                    actionButton={defaultActionButton}
+                    label="Test"
+                >
+                    WARNING! test test
+                </GlobalBanner>,
+            );
+
+            expect(screen.getByTestId('container')).toBeInTheDocument();
         });
     });
 });
