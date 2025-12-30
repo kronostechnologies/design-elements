d49fdae1c53bb51e1efa0449481cb51d380c45a5
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-29 22:59:14 -0500

    test(Lozenge): rewrite tests with RTL

--- packages/react/src/components/lozenge/lozenge.test.tsx
+++ packages/react/src/components/lozenge/lozenge.test.tsx
@@ -1,4 +1,47 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../test-utils/renderer';
+import { Lozenge } from './lozenge';
+
 describe('Lozenge', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('has icon when icon prop is specified', () => {
+        renderWithProviders(<Lozenge icon="home">Test</Lozenge>);
+
+        expect(screen.getByTestId('lozenge-icon')).toBeInTheDocument();
+    });
+
+    it('matches the snapshot', () => {
+        const { asFragment } = renderWithProviders(<Lozenge>Hello World</Lozenge>);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('default matches the snapshot', () => {
+        const { asFragment } = renderWithProviders(<Lozenge>default</Lozenge>);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('success matches the snapshot', () => {
+        const { asFragment } = renderWithProviders(<Lozenge variant="success">success</Lozenge>);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('warning matches the snapshot', () => {
+        const { asFragment } = renderWithProviders(<Lozenge variant="warning">warning</Lozenge>);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('info matches the snapshot', () => {
+        const { asFragment } = renderWithProviders(<Lozenge variant="info">info</Lozenge>);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('alert matches the snapshot', () => {
+        const { asFragment } = renderWithProviders(<Lozenge variant="alert">alert</Lozenge>);
+
+        expect(asFragment()).toMatchSnapshot();
     });
 });
