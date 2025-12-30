b04b58db68ec38d0a1f42cad48412a9a9dd84f47
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2025-12-30 00:12:57 -0500

    test(NavList): rewrite tests with RTL

--- packages/react/src/components/nav-list/nav-list-item.test.tsx
+++ packages/react/src/components/nav-list/nav-list-item.test.tsx
@@ -1,4 +1,91 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { renderWithProviders } from '../../test-utils/testing-library';
+import { ListOption, NavListItem } from './nav-list-item';
+
 describe('NavListItem', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    function givenOptionA(override: Omit<Partial<ListOption>, 'label' | 'value'> = {}): ListOption {
+        return {
+            id: 'an-id',
+            focusIndex: 0,
+            ref: React.createRef(),
+            label: 'Option A',
+            value: 'optionA',
+            href: '/testA',
+            ...override,
+        };
+    }
+
+    it('should use react-router links by default', () => {
+        const option = givenOptionA();
+
+        renderWithProviders(<NavListItem option={option} />);
+
+        const link = screen.getByRole('link', { name: 'Option A' });
+        expect(link).toBeInTheDocument();
+        expect(link).toHaveAttribute('href', '/testA');
+    });
+
+    it('displays screen-reader-only text when router link opens in a new tab (target="_blank")', () => {
+        const option = givenOptionA({ target: '_blank' });
+
+        renderWithProviders(<NavListItem option={option} />);
+
+        expect(screen.getByTestId('listitem-optionA-link-screen-reader-text')).toBeInTheDocument();
+    });
+
+    it('displays screen-reader-only text when html link opens in a new tab (target="_blank")', () => {
+        const option = givenOptionA({ target: '_blank', isHtmlLink: true });
+
+        renderWithProviders(<NavListItem option={option} />);
+
+        expect(screen.getByTestId('listitem-optionA-link-screen-reader-text')).toBeInTheDocument();
+    });
+
+    it('should use html links when isHtmlLink is set to true', () => {
+        const option = givenOptionA({ isHtmlLink: true });
+
+        renderWithProviders(<NavListItem option={option} />);
+
+        const link = screen.getByRole('link', { name: 'Option A' });
+        expect(link).toBeInTheDocument();
+        expect(link).toHaveAttribute('href', '/testA');
+    });
+
+    it('should have start-icon when startIcon prop is defined', () => {
+        const option = givenOptionA({ startIcon: 'home' });
+
+        renderWithProviders(<NavListItem option={option} />);
+
+        expect(screen.getByTestId('start-icon')).toBeInTheDocument();
+    });
+
+    it('should have end-icon when endIcon prop is defined', () => {
+        const option = givenOptionA({ endIcon: 'home' });
+
+        renderWithProviders(<NavListItem option={option} />);
+
+        expect(screen.getByTestId('end-icon')).toBeInTheDocument();
+    });
+
+    it('calls option.onClick when an htmlLink is clicked', async () => {
+        const onClick = jest.fn((e) => e.preventDefault());
+        const option = givenOptionA({ isHtmlLink: true, onClick });
+        renderWithProviders(<NavListItem option={option} />);
+
+        await userEvent.click(screen.getByTestId('listitem-optionA-link'));
+
+        expect(onClick).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls option.onClick when an ReactRouterNavLink is clicked', async () => {
+        const onClick = jest.fn((e) => e.preventDefault());
+        const option = givenOptionA({ onClick });
+        renderWithProviders(<NavListItem option={option} />);
+
+        await userEvent.click(screen.getByTestId('listitem-optionA-link'));
+
+        expect(onClick).toHaveBeenCalledTimes(1);
     });
 });
--- packages/react/src/components/nav-list/nav-list.test.tsx
+++ packages/react/src/components/nav-list/nav-list.test.tsx
@@ -1,4 +1,98 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { NavList } from './nav-list';
+import { NavListOption } from './nav-list-option';
+import { renderWithProviders } from '../../test-utils/renderer';
+
+const options: NavListOption[] = [
+    {
+        label: 'Option A',
+        value: 'optionA',
+        href: '/testA',
+    },
+    {
+        label: 'Option B',
+        value: 'optionB',
+        href: '/testB',
+    },
+    {
+        label: 'Option C',
+        value: 'optionC',
+        href: '/testC',
+    },
+    {
+        label: 'Option D',
+        value: 'optionD',
+        href: '/testD',
+    },
+];
+const optionsDisabled: NavListOption[] = [
+    {
+        label: 'Option A',
+        value: 'optionA',
+        href: '/testA',
+        disabled: true,
+    },
+    {
+        label: 'Option B',
+        value: 'optionB',
+        href: '/testB',
+        disabled: true,
+    },
+];
 describe('NavList', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('Calls onChange callback when an option is clicked', async () => {
+        const callback = jest.fn();
+        renderWithProviders(<NavList options={options} onChange={callback} />);
+
+        await userEvent.click(screen.getByTestId('listitem-optionC-link'));
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not call onChange callback when a disabled option is clicked', async () => {
+        const callback = jest.fn();
+        renderWithProviders(<NavList options={optionsDisabled} onChange={callback} />);
+
+        await expect(userEvent.click(screen.getByTestId('listitem-optionA-link'))).rejects
+            .toThrow(/pointer-events: none/);
+        expect(callback).toHaveBeenCalledTimes(0);
+    });
+
+    it('calls onChange callback when enter key is pressed on option', async () => {
+        const callback = jest.fn();
+        renderWithProviders(<NavList options={options} onChange={callback} />);
+
+        const option = screen.getByTestId('listitem-optionC-link');
+        option.focus();
+        await userEvent.keyboard('{Enter}');
+
+        expect(callback).toHaveBeenCalledTimes(2);
+    });
+
+    it('calls onKeyDown callback when a key is pressed on option', async () => {
+        const callback = jest.fn();
+        renderWithProviders(<NavList options={options} onKeyDown={callback} />);
+
+        const option = screen.getByTestId('listitem-optionA-link');
+        option.focus();
+        await userEvent.keyboard('a');
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('should update focused value when focusedValue prop changes', () => {
+        const { rerender } = renderWithProviders(<NavList options={options} />);
+
+        rerender(<NavList focusedValue="optionB" options={options} />);
+
+        const optionB = screen.getByTestId('listitem-optionB-link');
+        expect(optionB).toHaveFocus();
+    });
+
+    it('matches the snapshot', () => {
+        const { asFragment } = renderWithProviders(<NavList options={options} />);
+
+        expect(asFragment()).toMatchSnapshot();
     });
 });
