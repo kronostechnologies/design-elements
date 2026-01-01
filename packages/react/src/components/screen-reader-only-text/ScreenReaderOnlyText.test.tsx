bff9295ac1e7678f482ce1ae29896ca9332f1959
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-01 14:20:34 -0500

    test(ScreenReaderOnlyText): rewrite tests with RTL

--- packages/react/src/components/screen-reader-only-text/ScreenReaderOnlyText.test.tsx
+++ packages/react/src/components/screen-reader-only-text/ScreenReaderOnlyText.test.tsx
@@ -1,4 +1,10 @@
+import { renderWithProviders } from '../../test-utils/renderer';
+import { ScreenReaderOnlyText } from './ScreenReaderOnlyText';
+
 describe('ScreenReaderOnlyText', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('matches snapshot', () => {
+        const { asFragment } = renderWithProviders(<ScreenReaderOnlyText label="test" />);
+
+        expect(asFragment()).toMatchSnapshot();
     });
 });
