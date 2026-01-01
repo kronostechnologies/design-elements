b2243cdd82c92ce65fd856fce7c6a763131b96e9
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-01 15:16:53 -0500

    test(SkipLink): rewrite tests with RTL

--- packages/react/src/components/skip-link/skip-link.test.tsx
+++ packages/react/src/components/skip-link/skip-link.test.tsx
@@ -1,16 +1,27 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { SkipLink } from './skip-link';
 
 describe('SkipLink', () => {
-    test('Matches Snapshot (Desktop)', () => {
+    it('matches Snapshot (Desktop)', () => {
         const { container } = renderWithProviders(<SkipLink href="test" />, 'desktop');
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('Matches Snapshot (Mobile)', () => {
+    it('matches Snapshot (Mobile)', () => {
         const { container } = renderWithProviders(<SkipLink href="test" />, 'mobile');
 
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('should call onClick callback when clicked', async () => {
+        const callback = jest.fn();
+        renderWithProviders(<SkipLink href="test" onClick={callback} />);
+
+        await userEvent.click(screen.getByRole('link'));
+
+        expect(callback).toHaveBeenCalled();
     });
 });
