8dc869e8619e0e98f657536f2dcc9fa41d191a65
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-01 15:13:09 -0500

    test(ShadowWrapper): rewrite tests with RTL

--- packages/react/src/components/shadow-wrapper/shadow-wrapper.test.tsx
+++ packages/react/src/components/shadow-wrapper/shadow-wrapper.test.tsx
@@ -1,4 +1,28 @@
-describe('Shadow Wrapper', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+import { renderWithProviders } from '../../test-utils/testing-library';
+import { ShadowWrapper } from './shadow-wrapper';
+
+jest.mock('../../styles', () => ({
+    mainCss: 'main css',
+    useStyle: jest.fn(),
+}));
+
+describe('ShadowWrapper', () => {
+    it('is div element by default', () => {
+        const { container } = renderWithProviders(<ShadowWrapper>Test</ShadowWrapper>);
+
+        expect(container.firstElementChild?.tagName).toBe('DIV');
+    });
+
+    it('is set to tagName element', () => {
+        const { container } = renderWithProviders(<ShadowWrapper tagName="section">Test</ShadowWrapper>);
+
+        expect(container.firstElementChild?.tagName).toBe('SECTION');
+    });
+
+    it('should inject main css', () => {
+        const { container } = renderWithProviders(<ShadowWrapper>Test</ShadowWrapper>);
+
+        const shadowRoot = container.firstElementChild?.shadowRoot;
+        expect(shadowRoot?.querySelector('style')?.textContent).toEqual('main css');
     });
 });
