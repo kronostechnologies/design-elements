d85bc913ea936ae7f7495823127df68c43e8875d
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-01 14:55:20 -0500

    test(SectionalBanner): rewrite tests with RTL

--- packages/react/src/components/sectional-banner/sectional-banner.test.tsx
+++ packages/react/src/components/sectional-banner/sectional-banner.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { SectionalBanner } from './sectional-banner';
 
@@ -38,5 +40,60 @@ describe('SectionalBanner', () => {
         );
 
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    (['mobile', 'desktop'] as const).forEach((device) => {
+        it(`should show destructive button when type is alert (${device})`, () => {
+            renderWithProviders(
+                <SectionalBanner
+                    type="alert"
+                    buttonLabel="some button"
+                    onButtonClicked={jest.fn()}
+                >
+                    Test
+                </SectionalBanner>,
+                device,
+            );
+
+            const button = screen.getByTestId('button');
+            expect(button).toBeInTheDocument();
+        });
+
+        it(`should call callback when dismiss button is clicked (${device})`, async () => {
+            const onDismiss = jest.fn();
+            const user = userEvent.setup();
+            renderWithProviders(
+                <SectionalBanner
+                    type="info"
+                    onDismiss={onDismiss}
+                >
+                    Test
+                </SectionalBanner>,
+                device,
+            );
+
+            await user.click(screen.getByTestId('dismiss-button'));
+
+            expect(onDismiss).toHaveBeenCalled();
+        });
+
+        it(`should call callback when button is clicked (${device})`, async () => {
+            const onButtonClicked = jest.fn();
+            const user = userEvent.setup();
+            renderWithProviders(
+                <SectionalBanner
+                    type="info"
+                    buttonLabel="some button"
+                    onButtonClicked={onButtonClicked}
+                >
+                    Test
+                </SectionalBanner>,
+                device,
+            );
+
+            await user.click(screen.getByTestId('button'));
+
+            expect(onButtonClicked).toHaveBeenCalled();
+        });
     });
 });
