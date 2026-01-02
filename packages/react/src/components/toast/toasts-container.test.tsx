14ecf936f9767079b55c78b7c627a75f3f757a7a
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 01:51:53 -0500

    test(Toast): rewrite tests with RTL

--- packages/react/src/components/toast/toasts-container.test.tsx
+++ packages/react/src/components/toast/toasts-container.test.tsx
@@ -1,4 +1,34 @@
+import { screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useToasts } from '../../hooks/use-toasts';
+import { renderWithProviders } from '../../test-utils/testing-library';
+import { type Toast, type ToastContextProps } from './toast-context';
+import { ToastTypeEnum } from './toast-type';
+
+jest.mock('../../hooks/use-toasts');
+
 describe('ToastsContainer', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('should display toasts in order', async () => {
+        const removeToast = jest.fn();
+        const toasts: Toast[] = [
+            { id: 'an id', type: ToastTypeEnum.NEUTRAL, message: 'a message' },
+            { id: 'another id', type: ToastTypeEnum.SUCCESS, message: 'another message' },
+        ];
+        jest.mocked(useToasts).mockReturnValue({ toasts, removeToast } as unknown as ToastContextProps);
+
+        // ToastContainer is part of the required DS providers
+        renderWithProviders(<div />);
+
+        const toastsContainer = screen.getByTestId('toasts');
+        expect(toastsContainer.children).toHaveLength(2);
+
+        const toast0 = screen.getByTestId('toast-0');
+        const toast1 = screen.getByTestId('toast-1');
+
+        await userEvent.click(within(toast0).getByTestId('dismiss'));
+        expect(removeToast).toHaveBeenCalledWith('an id');
+
+        await userEvent.click(within(toast1).getByTestId('dismiss'));
+        expect(removeToast).toHaveBeenCalledWith('another id');
     });
 });
