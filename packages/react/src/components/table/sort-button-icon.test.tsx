cc4c6853bad200842376c88882dc3bda73f335cb
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 00:10:35 -0500

    test(Table): rewrite tests with RTL

--- packages/react/src/components/table/sort-button-icon.test.tsx
+++ packages/react/src/components/table/sort-button-icon.test.tsx
@@ -1,4 +1,22 @@
+import { renderWithProviders } from '../../test-utils/renderer';
+import { SortButtonIcon } from './sort-button-icon';
+
 describe('SortButtonIcon', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('should display arrow up when sort is ascending', () => {
+        const { asFragment } = renderWithProviders(<SortButtonIcon sort="ascending" />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('should display arrow down when sort is descending', () => {
+        const { asFragment } = renderWithProviders(<SortButtonIcon sort="descending" />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('should display reorder when no sort', () => {
+        const { asFragment } = renderWithProviders(<SortButtonIcon sort="none" />);
+
+        expect(asFragment()).toMatchSnapshot();
     });
 });
