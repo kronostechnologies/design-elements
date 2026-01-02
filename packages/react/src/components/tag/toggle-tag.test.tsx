9fbf412b99edcdb624b55a26c7dc79df5e33d262
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 00:45:58 -0500

    test(Tag): rewrite tests with RTL

--- packages/react/src/components/tag/toggle-tag.test.tsx
+++ packages/react/src/components/tag/toggle-tag.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { DeviceType } from '../device-context-provider';
 import { ToggleTag, ToggleTagProps } from './toggle-tag';
@@ -47,6 +49,45 @@ describe('ToggleTag', () => {
                     );
 
                     expect(container.firstChild).toMatchSnapshot();
+                });
+
+                it('calls onClick callback when tag is clicked', async () => {
+                    const callback = jest.fn();
+                    renderWithProviders(
+                        <ToggleTag size={size} value={{ label: 'Test' }} onClick={callback} />,
+                        deviceType,
+                    );
+
+                    await userEvent.click(screen.getByRole('button', { name: 'Test' }));
+
+                    expect(callback).toHaveBeenCalledTimes(1);
+                });
+
+                it(
+                    'has aria-hidden="false" and an aria-label on icon when the label is not the same as iconName',
+                    () => {
+                        renderWithProviders(
+                            <ToggleTag size={size} iconName="home" value={{ label: 'Test' }} />,
+                            deviceType,
+                        );
+
+                        const icon = screen.getByTestId('Test-icon');
+
+                        expect(icon).toHaveAttribute('aria-label', 'home');
+                        expect(icon).toHaveAttribute('aria-hidden', 'false');
+                    },
+                );
+
+                it('has aria-hidden="true" and no label on icon when the label is the same as the iconName', () => {
+                    renderWithProviders(
+                        <ToggleTag size={size} iconName="home" value={{ label: 'Home' }} />,
+                        deviceType,
+                    );
+
+                    const icon = screen.getByTestId('Home-icon');
+
+                    expect(icon).not.toHaveAttribute('aria-label');
+                    expect(icon).toHaveAttribute('aria-hidden', 'true');
                 });
             });
         });
