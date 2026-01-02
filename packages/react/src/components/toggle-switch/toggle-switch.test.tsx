7377c92d3fe30014b136fde39575715857160b1d
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 01:56:19 -0500

    test(ToggleSwitch): rewrite tests with RTL

--- packages/react/src/components/toggle-switch/toggle-switch.test.tsx
+++ packages/react/src/components/toggle-switch/toggle-switch.test.tsx
@@ -1,30 +1,32 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { ToggleSwitch } from './toggle-switch';
 
 describe('ToggleSwitch', () => {
-    test('renders label to the right when labelPosition is "right" (by default)', () => {
-        const { getByTestId: byTestId, getByRole } = renderWithProviders(
+    it('renders label to the right when labelPosition is "right" (by default)', () => {
+        renderWithProviders(
             <ToggleSwitch label="Switch" toggled onToggle={jest.fn()} />,
         );
 
-        const label = byTestId('switch-label');
-        const button = getByRole('switch');
+        const label = screen.getByTestId('switch-label');
+        const button = screen.getByRole('switch');
 
         expect(button?.nextSibling).toBe(label);
     });
 
-    test('renders label to the left when labelPosition is "left"', () => {
-        const { getByTestId: byTestId, getByRole } = renderWithProviders(
+    it('renders label to the left when labelPosition is "left"', () => {
+        renderWithProviders(
             <ToggleSwitch label="Switch" labelPosition="left" toggled onToggle={jest.fn()} />,
         );
 
-        const label = byTestId('switch-label');
-        const button = getByRole('switch');
+        const label = screen.getByTestId('switch-label');
+        const button = screen.getByRole('switch');
 
         expect(label?.nextSibling).toBe(button);
     });
 
-    test('Matches snapshot (desktop)', () => {
+    it('matches snapshot (desktop)', () => {
         const { container } = renderWithProviders(
             <ToggleSwitch label="Switch" toggled onToggle={jest.fn()} />,
             'desktop',
@@ -33,10 +35,39 @@ describe('ToggleSwitch', () => {
         expect(container.children).toMatchSnapshot();
     });
 
-    test('Matches snapshot (mobile)', () => {
+    it('matches snapshot (mobile)', () => {
         const { container } = renderWithProviders(
             <ToggleSwitch label="Switch" toggled onToggle={jest.fn()} />,
             'mobile',
+        );
+
+        expect(container.children).toMatchSnapshot();
+    });
+
+    it('calls onToggle callback when clicked', async () => {
+        const onToggle = jest.fn();
+        renderWithProviders(<ToggleSwitch label="Switch" toggled onToggle={onToggle} />);
+
+        const switchButton = screen.getByRole('switch');
+        await userEvent.click(switchButton);
+
+        expect(onToggle).toHaveBeenCalledTimes(1);
+        expect(onToggle).toHaveBeenCalledWith(false);
+    });
+
+    it('does not call onToggle callback when disabled', async () => {
+        const onToggle = jest.fn();
+        renderWithProviders(<ToggleSwitch label="Switch" toggled disabled onToggle={onToggle} />);
+
+        const switchButton = screen.getByRole('switch');
+        await userEvent.click(switchButton);
+
+        expect(onToggle).not.toHaveBeenCalled();
+    });
+
+    it('matches snapshot when disabled', () => {
+        const { container } = renderWithProviders(
+            <ToggleSwitch label="Switch" toggled disabled onToggle={jest.fn()} />,
         );
 
         expect(container.children).toMatchSnapshot();
