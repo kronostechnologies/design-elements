9954cf7c1ba5bcb89cef5165feaf09f760477235
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 01:51:53 -0500

    test(Toast): rewrite tests with RTL

--- packages/react/src/components/toast/toast-container.test.tsx
+++ packages/react/src/components/toast/toast-container.test.tsx
@@ -1,4 +1,37 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useToasts } from '../../hooks/use-toasts';
+import { renderWithProviders } from '../../test-utils/renderer';
+import { ToastContainer } from './toast-container';
+import { ToastContextProps } from './toast-context';
+import { ToastTypeEnum } from './toast-type';
+
+jest.mock('../../hooks/use-toasts');
+
 describe('ToastContainer', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('should remove toast when dismiss is clicked', async () => {
+        const user = userEvent.setup();
+        const removeToast = jest.fn();
+        jest.mocked(useToasts).mockReturnValue({ removeToast, toasts: [] } as unknown as ToastContextProps);
+
+        renderWithProviders(
+            <ToastContainer id="an id" type="success" message="a message" position="bottom-right" />,
+        );
+
+        await user.click(screen.getByTestId('dismiss'));
+
+        expect(removeToast).toHaveBeenCalled();
+    });
+
+    Object.values(ToastTypeEnum).forEach((type) => {
+        it(`should match snapshot (${type})`, () => {
+            jest.mocked(useToasts).mockReturnValue({ toasts: [] } as unknown as ToastContextProps);
+
+            const { asFragment } = renderWithProviders(
+                <ToastContainer id="an id" type={type} message="a message" position="bottom-right" />,
+            );
+
+            expect(asFragment()).toMatchSnapshot();
+        });
     });
 });
--- packages/react/src/components/toast/toast-provider.test.tsx
+++ packages/react/src/components/toast/toast-provider.test.tsx
@@ -1,4 +1,53 @@
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { type FC } from 'react';
+import { useToasts } from '../../hooks/use-toasts';
+import { renderWithProviders } from '../../test-utils/renderer';
+
+const MESSAGE = 'A message';
+
+const TestConsumer: FC = () => {
+    const { addToast, removeToast, toasts } = useToasts();
+
+    return (
+        <>
+            <button type="button" data-testid="btn-add" onClick={() => addToast('success', MESSAGE)} />
+            <button type="button" data-testid="btn-remove" onClick={() => removeToast(toasts[0]?.id || '')} />
+            {toasts.map((toast, i) => (
+                <div key={toast.id} data-testid={`toast-${i}-message`}>{toast.message}</div>
+            ))}
+        </>
+    );
+};
+
 describe('ToastProvider', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('should add toast to context', async () => {
+        renderWithProviders(<TestConsumer />);
+
+        await userEvent.click(screen.getByTestId('btn-add'));
+
+        const toast = screen.getByTestId('toast-0-message');
+        expect(toast).toHaveTextContent(MESSAGE);
+    });
+
+    it('should remove toast from context', async () => {
+        renderWithProviders(<TestConsumer />);
+        await userEvent.click(screen.getByTestId('btn-add'));
+
+        await userEvent.click(screen.getByTestId('btn-remove'));
+
+        expect(screen.queryByTestId('toast-0')).not.toBeInTheDocument();
+    });
+
+    it('should remove toast after dismiss time', async () => {
+        jest.useFakeTimers();
+        const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+        renderWithProviders(<TestConsumer />);
+        await user.click(screen.getByTestId('btn-add'));
+
+        act(() => jest.runAllTimers());
+
+        expect(screen.queryByTestId('toast-0')).not.toBeInTheDocument();
+        jest.useRealTimers();
     });
 });
