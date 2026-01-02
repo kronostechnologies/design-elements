9ad1a2dc14af1a657720e2553827245d031842f8
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 13:13:03 -0500

    refactor: remove legacy-tests package

--- packages/legacy-tests/src/test-utils/callbacks.ts
+++
@@ -1,4 +0,0 @@
-export function doNothing(): void {
-    // do nothing
-}
-
--- packages/legacy-tests/test/snapshot-resolver.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    resolveSnapshotPath: (testPath, snapshotExtension) => `${testPath}${snapshotExtension}`,
-
-    resolveTestPath: (snapshotFilePath, snapshotExtension) => snapshotFilePath.replace(snapshotExtension, ''),
-
-    testPathForConsistencyCheck: '../../src/components/button/button.test.tsx',
-};
