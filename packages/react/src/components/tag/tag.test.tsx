19740254d77169485e75df327eef3d4977fdafec
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 00:45:58 -0500

    test(Tag): rewrite tests with RTL

--- packages/react/src/components/tag/tag.test.tsx
+++ packages/react/src/components/tag/tag.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { DeviceType } from '../device-context-provider';
 import { Tag, TagProps } from './tag';
@@ -50,6 +52,64 @@ describe('Tag', () => {
 
                         expect(container.firstChild).toMatchSnapshot();
                     });
+
+                    it(`calls onRemove callback when delete-button is clicked (${deviceType})`, async () => {
+                        const callback = jest.fn();
+                        const user = userEvent.setup();
+                        renderWithProviders(
+                            <Tag
+                                size={size}
+                                color={color}
+                                value={{ label: 'Test' }}
+                                onRemove={callback}
+                            />,
+                            deviceType,
+                        );
+
+                        await user.click(screen.getByTestId('Test-remove-button'));
+
+                        expect(callback).toHaveBeenCalledTimes(1);
+                    });
+
+                    it(
+                        `has aria-hidden="false" and an icon aria-label when label is not the same as iconName (${deviceType})`,
+                        () => {
+                            renderWithProviders(
+                                <Tag
+                                    size={size}
+                                    color={color}
+                                    iconName="home"
+                                    value={{ label: 'Test' }}
+                                />,
+                                deviceType,
+                            );
+
+                            const icon = screen.getByTestId('Test-icon');
+
+                            expect(icon).toHaveAttribute('aria-label', 'home');
+                            expect(icon).toHaveAttribute('aria-hidden', 'false');
+                        },
+                    );
+
+                    it(
+                        `has aria-hidden="true" and no label on icon when the label is the same as the iconName (${deviceType})`,
+                        () => {
+                            renderWithProviders(
+                                <Tag
+                                    size={size}
+                                    color={color}
+                                    iconName="home"
+                                    value={{ label: 'Home' }}
+                                />,
+                                deviceType,
+                            );
+
+                            const icon = screen.getByTestId('Home-icon');
+
+                            expect(icon).not.toHaveAttribute('aria-label');
+                            expect(icon).toHaveAttribute('aria-hidden', 'true');
+                        },
+                    );
                 });
             });
         });
