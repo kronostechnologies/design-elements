49de9d26bdc205649498838b81806f6b62e5048b
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 01:21:12 -0500

    test(ThemeWrapper): rewrite tests with RTL

--- packages/react/src/components/theme-wrapper/theme-wrapper.test.tsx
+++ packages/react/src/components/theme-wrapper/theme-wrapper.test.tsx
@@ -1,4 +1,56 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../test-utils/testing-library';
+import { buildTheme, ThemeCustomization } from '../../themes';
+import { Button } from '../buttons';
+
 describe('Theme Wrapper', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    it('returns component with default theme', () => {
+        const { asFragment } = renderWithProviders(
+            <Button buttonType="primary" />,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('returns component with custom theme', () => {
+        const themeCustomization: ThemeCustomization = {
+            ref: {
+                'color-brand-50': '#00874E',
+            },
+            component: {
+                'button-primary-background-color': 'color-brand-50',
+            },
+        };
+
+        const builtTheme = buildTheme(themeCustomization);
+
+        const { asFragment } = renderWithProviders(
+            <Button buttonType="primary" />,
+            undefined,
+            { theme: builtTheme },
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('should not use ShadowWrapper by default', () => {
+        const { container } = renderWithProviders(<div data-testid="test-child">Child</div>);
+
+        expect(screen.getByTestId('test-child')).toBeInTheDocument();
+        expect(container.firstElementChild!.shadowRoot).toBeNull();
+    });
+
+    it('should use ShadowWrapper when styles are isolated', () => {
+        const { container } = renderWithProviders(
+            <div data-testid="test-child">Child</div>,
+            undefined,
+            { isolateStyles: true },
+        );
+
+        expect(screen.queryByTestId('test-child')).not.toBeInTheDocument();
+
+        const shadowHost = container.firstElementChild;
+        expect(shadowHost?.shadowRoot).toBeTruthy();
+        expect(shadowHost?.shadowRoot?.textContent).toContain('Child');
     });
 });
