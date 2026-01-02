9adbf17a5ed99c048adc43c9e6c43f4a4500e699
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 02:10:35 -0500

    test(Toggletip): rewrite tests with RTL

--- packages/react/src/components/toggletip/toggletip.test.tsx
+++ packages/react/src/components/toggletip/toggletip.test.tsx
@@ -1,4 +1,71 @@
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithProviders } from '../../test-utils/renderer';
+import { Toggletip } from './toggletip';
+
 describe('Toggletip', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    describe('desktop', () => {
+        it('opens on mouseClick', async () => {
+            renderWithProviders(<Toggletip>Test Content</Toggletip>, 'desktop');
+
+            const toggletip = screen.getByTestId('toggletip');
+            await userEvent.click(toggletip);
+
+            expect(screen.getByTestId('toggletip-content-container')).toBeInTheDocument();
+        });
+
+        it('closes on mouseClick given toggletip is open', async () => {
+            renderWithProviders(<Toggletip defaultOpen>Test Content</Toggletip>, 'desktop');
+
+            const toggletip = screen.getByTestId('toggletip');
+            await userEvent.click(toggletip);
+
+            expect(screen.queryByTestId('toggletip-content-container')).not.toBeInTheDocument();
+        });
+
+        it('does not open on mouseClick given toggletip is disabled', async () => {
+            renderWithProviders(<Toggletip disabled>Test Content</Toggletip>, 'desktop');
+
+            const toggletip = screen.getByTestId('toggletip');
+            await expect(userEvent.click(toggletip)).toReject();
+
+            expect(screen.queryByTestId('toggletip-content-container')).not.toBeInTheDocument();
+        });
+    });
+
+    it('has default desktop styles', () => {
+        const { asFragment } = renderWithProviders(<Toggletip>Test Content</Toggletip>, 'desktop');
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('has default desktop styles (defaultOpen)', async () => {
+        const { asFragment } = await act(
+            () => renderWithProviders(<Toggletip defaultOpen>Test Content</Toggletip>, 'desktop'),
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('has mobile styles', () => {
+        const { asFragment } = renderWithProviders(<Toggletip>Test Content</Toggletip>, 'mobile');
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('has mobile styles (defaultOpen)', async () => {
+        const { asFragment } = await act(
+            () => renderWithProviders(<Toggletip defaultOpen>Test Content</Toggletip>, 'mobile'),
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    describe('mobile', () => {
+        it('renders content', () => {
+            renderWithProviders(<Toggletip defaultOpen>Test Content</Toggletip>, 'mobile');
+
+            expect(screen.getByTestId('toggletip-content-container')).toHaveTextContent('Test Content');
+        });
     });
 });
