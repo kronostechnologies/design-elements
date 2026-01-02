263c26f06c61b5f7df563fd40a89557b58aa89f7
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-01-02 00:36:27 -0500

    test(Tabs): rewrite tests with RTL

--- packages/react/src/components/tabs/tab-button.test.tsx
+++ packages/react/src/components/tabs/tab-button.test.tsx
@@ -1,4 +1,79 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { doNothing } from '../../test-utils/callbacks';
+import { renderWithProviders } from '../../test-utils/renderer';
+import { TabButton } from './tab-button';
+
 describe('TabButton', () => {
-    it.skip('All the tests were moved to legacy-tests. RTL tests only here', () => {
+    const focusedAndSelected = {
+        id: 'aId',
+        panelId: 'aPanelId',
+        isSelected: true,
+        isFocused: true,
+        onClick: doNothing,
+        onFocus: doNothing,
+    };
+
+    it('should display button text', () => {
+        const expectedButtonText = 'some text';
+
+        renderWithProviders(
+            <TabButton {...focusedAndSelected} size="medium">{expectedButtonText}</TabButton>,
+        );
+
+        expect(screen.getByTestId('tab-text')).toHaveTextContent(expectedButtonText);
+    });
+
+    it('should not have a left icon in button when tab doesn\'t have a left icon name', () => {
+        renderWithProviders(<TabButton {...focusedAndSelected} size="medium">some text</TabButton>);
+
+        expect(screen.queryByTestId('tab-left-icon')).not.toBeInTheDocument();
+    });
+
+    it('should have a left icon in button when tab has a left icon name', () => {
+        const expectedLeftIcon = 'chevronUp';
+
+        renderWithProviders(
+            <TabButton {...focusedAndSelected} size="medium" leftIcon={expectedLeftIcon}>some text</TabButton>,
+        );
+
+        expect(screen.getByTestId('tab-left-icon')).toBeInTheDocument();
+    });
+
+    it('should not have a right icon in button when tab doesn\'t have a right icon name', () => {
+        renderWithProviders(<TabButton {...focusedAndSelected} size="medium">some text</TabButton>);
+
+        expect(screen.queryByTestId('tab-right-icon')).not.toBeInTheDocument();
+    });
+
+    it('should have a right icon in button when tab has a right icon name', () => {
+        const expectedRightIcon = 'chevronDown';
+
+        renderWithProviders(
+            <TabButton {...focusedAndSelected} size="medium" rightIcon={expectedRightIcon}>some text</TabButton>,
+        );
+
+        expect(screen.getByTestId('tab-right-icon')).toBeInTheDocument();
+    });
+
+    it('should call component onClick method when button is clicked', async () => {
+        const expectedOnClickCall = jest.fn();
+        const user = userEvent.setup();
+
+        renderWithProviders(
+            <TabButton
+                id="aId"
+                panelId="aPanelId"
+                isSelected
+                onClick={expectedOnClickCall}
+                size="medium"
+            >
+                some text
+            </TabButton>,
+        );
+
+        await user.click(screen.getByTestId('tab-button'));
+
+        expect(expectedOnClickCall).toHaveBeenCalled();
     });
 });
--- packages/react/src/components/tabs/tabs.test.tsx
+++ packages/react/src/components/tabs/tabs.test.tsx
@@ -1,4 +1,5 @@
-import { act, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { Tab, Tabs } from './tabs';
 
@@ -40,7 +41,7 @@ describe('Tabs', () => {
         }));
     });
 
-    test('matches snapshot', () => {
+    it('matches snapshot', () => {
         const tabs: Tab[] = givenTabs(2);
 
         const { container } = renderWithProviders(<Tabs tabs={tabs} forceRenderTabPanels />);
@@ -48,7 +49,7 @@ describe('Tabs', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('has small styles', () => {
+    it('has small styles', () => {
         const tabs: Tab[] = givenTabs(2);
 
         const { container } = renderWithProviders(
@@ -62,7 +63,7 @@ describe('Tabs', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test('matches snapshot (mobile)', () => {
+    it('matches snapshot (mobile)', () => {
         const tabs: Tab[] = givenTabs(2);
 
         const { container } = renderWithProviders(<Tabs tabs={tabs} forceRenderTabPanels />, 'mobile');
@@ -70,8 +71,17 @@ describe('Tabs', () => {
         expect(container.firstChild).toMatchSnapshot();
     });
 
+    it('tab-panels should all be initially mounted when forceRenderTabPanels is set to true', () => {
+        const tabs: Tab[] = makeTabsWithContent(2);
+
+        renderWithProviders(<Tabs tabs={tabs} forceRenderTabPanels />);
+
+        expectTabWithTabIndexToBeRendered(0);
+        expectTabWithTabIndexToBeRendered(1);
+    });
+
     describe('controlled mode', () => {
-        test('renders with activeTabId', () => {
+        it('renders with activeTabId', () => {
             const tabs = makeTabsWithContent(3);
 
             renderWithProviders(
@@ -83,43 +93,48 @@ describe('Tabs', () => {
             expectTabWithTabIndexToNotBeRendered(2);
         });
 
-        test('onTabChange is called when tab is clicked', () => {
+        it('onTabChange is called when tab is clicked', async () => {
+            const user = userEvent.setup();
             const tabs: Tab[] = givenTabs(2);
             const onTabChange = jest.fn();
-            const { getByTestId } = renderWithProviders(
+            renderWithProviders(
                 <Tabs tabs={tabs} activeTabId="tab-1" onTabChange={onTabChange} />,
             );
 
-            getByTestId('tab-2-button').click();
+            await user.click(screen.getByTestId('tab-2-button'));
 
             expect(onTabChange).toHaveBeenCalledWith('tab-2');
         });
 
-        test('does not change tab visually if activeTabId does not change', () => {
+        it('does not change tab visually if activeTabId does not change', async () => {
+            const user = userEvent.setup();
             const tabs: Tab[] = givenTabs(2);
-            const { getByTestId, rerender } = renderWithProviders(
-                <Tabs tabs={tabs} activeTabId="tab-1" />,
+            const onTabChange = jest.fn();
+            const { rerender } = renderWithProviders(
+                <Tabs tabs={tabs} activeTabId="tab-1" onTabChange={onTabChange} />,
             );
-            getByTestId('tab-2-button').click();
+            await user.click(screen.getByTestId('tab-2-button'));
 
-            expect(getByTestId('tab-1-button')).toHaveAttribute('aria-selected', 'true');
-            expect(getByTestId('tab-2-button')).toHaveAttribute('aria-selected', 'false');
+            expect(screen.getByTestId('tab-1-button')).toHaveAttribute('aria-selected', 'true');
+            expect(screen.getByTestId('tab-2-button')).toHaveAttribute('aria-selected', 'false');
 
-            rerender(<Tabs tabs={tabs} activeTabId="tab-2" />);
-            expect(getByTestId('tab-2-button')).toHaveAttribute('aria-selected', 'true');
+            rerender(<Tabs tabs={tabs} activeTabId="tab-2" onTabChange={onTabChange} />);
+            expect(screen.getByTestId('tab-2-button')).toHaveAttribute('aria-selected', 'true');
         });
 
-        test('calls onRemove when a tab is removed', () => {
+        it('calls onRemove when a tab is removed', async () => {
+            const user = userEvent.setup();
             const currentTabs = makeTabsWithContent(3);
             const activeTabId = 'tab-2';
             const onRemove = jest.fn();
 
-            const { getByTestId } = renderWithProviders(
+            renderWithProviders(
                 <Tabs
                     tabs={currentTabs}
                     activeTabId={activeTabId}
                     onRemove={onRemove}
-                    onTabChange={() => {}}
+                    onTabChange={() => {
+                    }}
                 />,
             );
 
@@ -127,7 +142,7 @@ describe('Tabs', () => {
             expectTabWithTabIndexToNotBeRendered(0);
             expectTabWithTabIndexToNotBeRendered(2);
 
-            act(() => getByTestId('tab-2-delete').click());
+            await user.click(screen.getByTestId('tab-2-delete'));
 
             expect(onRemove).toHaveBeenCalledTimes(1);
             expect(onRemove).toHaveBeenCalledWith('tab-2');
@@ -135,52 +150,54 @@ describe('Tabs', () => {
     });
 
     describe('uncontrolled mode', () => {
-        test('renders with defaultSelectedId', () => {
+        it('renders with defaultSelectedId', () => {
             const tabs = makeTabsWithContent(3);
 
-            const { getByTestId } = renderWithProviders(
+            renderWithProviders(
                 <Tabs tabs={tabs} defaultSelectedId="tab-2" />,
             );
 
-            expect(getByTestId('tab-2-button')).toHaveAttribute('aria-selected', 'true');
+            expect(screen.getByTestId('tab-2-button')).toHaveAttribute('aria-selected', 'true');
             expectTabWithTabIndexToBeRendered(1);
             expectTabWithTabIndexToNotBeRendered(0);
             expectTabWithTabIndexToNotBeRendered(2);
         });
 
-        test('changes tab visually when tab is clicked', () => {
+        it('changes tab visually when tab is clicked', async () => {
+            const user = userEvent.setup();
             const tabs = makeTabsWithContent(2);
-            const { getByTestId } = renderWithProviders(
+            renderWithProviders(
                 <Tabs tabs={tabs} defaultSelectedId="tab-1" />,
             );
 
-            act(() => getByTestId('tab-2-button').click());
+            await user.click(screen.getByTestId('tab-2-button'));
 
-            expect(getByTestId('tab-2-button')).toHaveAttribute('aria-selected', 'true');
+            expect(screen.getByTestId('tab-2-button')).toHaveAttribute('aria-selected', 'true');
             expectTabWithTabIndexToBeRendered(1);
             expectTabWithTabIndexToNotBeRendered(0);
         });
 
-        test('defaults to first tab if no defaultSelectedId', () => {
+        it('defaults to first tab if no defaultSelectedId', () => {
             const tabs = makeTabsWithContent(2);
 
-            const { getByTestId } = renderWithProviders(
+            renderWithProviders(
                 <Tabs tabs={tabs} />,
             );
 
-            expect(getByTestId('tab-1-button')).toHaveAttribute('aria-selected', 'true');
+            expect(screen.getByTestId('tab-1-button')).toHaveAttribute('aria-selected', 'true');
             expectTabWithTabIndexToBeRendered(0);
             expectTabWithTabIndexToNotBeRendered(1);
         });
 
-        test('removes tab and selects next tab', async () => {
+        it('removes tab and selects next tab', async () => {
+            const user = userEvent.setup();
             let tabs = makeTabsWithContent(3);
             const onRemove = jest.fn();
-            const { getByTestId, rerender } = renderWithProviders(
+            const { rerender } = renderWithProviders(
                 <Tabs tabs={tabs} defaultSelectedId="tab-2" onRemove={onRemove} />,
             );
 
-            act(() => getByTestId('tab-2-delete').click());
+            await user.click(screen.getByTestId('tab-2-delete'));
             tabs = tabs.filter((t) => t.id !== 'tab-2');
             rerender(<Tabs tabs={tabs} defaultSelectedId="tab-2" onRemove={onRemove} />);
 
@@ -188,10 +205,11 @@ describe('Tabs', () => {
             expectTabWithTabIndexToNotBeRendered(1);
         });
 
-        test('calls onRemove when a tab is removed', () => {
+        it('calls onRemove when a tab is removed', async () => {
+            const user = userEvent.setup();
             const currentTabs = makeTabsWithContent(3);
             const onRemove = jest.fn();
-            const { getByTestId } = renderWithProviders(
+            renderWithProviders(
                 <Tabs tabs={currentTabs} defaultSelectedId="tab-2" onRemove={onRemove} />,
             );
 
@@ -199,10 +217,128 @@ describe('Tabs', () => {
             expectTabWithTabIndexToNotBeRendered(0);
             expectTabWithTabIndexToNotBeRendered(2);
 
-            act(() => getByTestId('tab-2-delete').click());
+            await user.click(screen.getByTestId('tab-2-delete'));
 
             expect(onRemove).toHaveBeenCalledTimes(1);
             expect(onRemove).toHaveBeenCalledWith('tab-2');
+        });
+
+        it('tab panel should not change if onBeforeUnload cancels tab selection', async () => {
+            const user = userEvent.setup();
+            const tabs: Tab[] = makeTabsWithContent(2);
+            tabs[0].onBeforeUnload = jest.fn().mockResolvedValue(false);
+            renderWithProviders(<Tabs tabs={tabs} />);
+
+            await user.click(screen.getByTestId('tab-2-button'));
+
+            expectTabWithTabIndexToBeRendered(0);
+            expectTabWithTabIndexToNotBeRendered(1);
+        });
+
+        it('tab panel should change if onBeforeUnload confirms tab selection', async () => {
+            const user = userEvent.setup();
+            const tabs: Tab[] = makeTabsWithContent(2);
+            tabs[0].onBeforeUnload = jest.fn().mockResolvedValue(true);
+            renderWithProviders(<Tabs tabs={tabs} />);
+
+            await user.click(screen.getByTestId('tab-2-button'));
+
+            expectTabWithTabIndexToBeRendered(1);
+            expectTabWithTabIndexToNotBeRendered(0);
+        });
+    });
+
+    describe('focus', () => {
+        it('should go to the next tab-button when ArrowRight key is pressed on a tab-button', async () => {
+            const user = userEvent.setup();
+            const tabs = makeTabsWithContent(2);
+            renderWithProviders(<Tabs tabs={tabs} />);
+            const tab1 = screen.getByTestId('tab-1-button');
+            const tab2 = screen.getByTestId('tab-2-button');
+
+            tab1.focus();
+            await user.keyboard('{ArrowRight}');
+
+            expect(tab2).toHaveFocus();
+        });
+
+        it('should go to the first tab-button when ArrowRight key is pressed on last tab-button', async () => {
+            const user = userEvent.setup();
+            const tabs = makeTabsWithContent(3);
+            renderWithProviders(<Tabs tabs={tabs} />);
+            const tab1 = screen.getByTestId('tab-1-button');
+            const tab3 = screen.getByTestId('tab-3-button');
+
+            tab3.focus();
+            await user.keyboard('{ArrowRight}');
+
+            expect(tab1).toHaveFocus();
+        });
+
+        it('should go to the previous tab-button when ArrowLeft key is pressed on a tab-button', async () => {
+            const user = userEvent.setup();
+            const tabs = makeTabsWithContent(3);
+            renderWithProviders(<Tabs tabs={tabs} />);
+            const tab2 = screen.getByTestId('tab-2-button');
+            const tab3 = screen.getByTestId('tab-3-button');
+
+            tab3.focus();
+            await user.keyboard('{ArrowLeft}');
+
+            expect(tab2).toHaveFocus();
+        });
+
+        it('should go to the last tab-button when ArrowLeft key is pressed on first tab-button', async () => {
+            const user = userEvent.setup();
+            const tabs = makeTabsWithContent(3);
+            renderWithProviders(<Tabs tabs={tabs} />);
+            const tab1 = screen.getByTestId('tab-1-button');
+            const tab3 = screen.getByTestId('tab-3-button');
+
+            tab1.focus();
+            await user.keyboard('{ArrowLeft}');
+
+            expect(tab3).toHaveFocus();
+        });
+
+        it('should go to the selected tab-button when Tab key is pressed on a non-selected tab-button', async () => {
+            const user = userEvent.setup();
+            const tabs = makeTabsWithContent(3);
+            renderWithProviders(<Tabs tabs={tabs} />);
+            const tab2 = screen.getByTestId('tab-2-button');
+            const tab3 = screen.getByTestId('tab-3-button');
+
+            await user.click(tab2);
+            tab3.focus();
+            await user.keyboard('{Tab}');
+
+            expect(tab2).toHaveFocus();
+        });
+
+        it('should go to the first tab-button when Home key is pressed on a tab-button', async () => {
+            const user = userEvent.setup();
+            const tabs = makeTabsWithContent(3);
+            renderWithProviders(<Tabs tabs={tabs} />);
+            const tab1 = screen.getByTestId('tab-1-button');
+            const tab3 = screen.getByTestId('tab-3-button');
+
+            await user.click(tab3);
+            await user.keyboard('{Home}');
+
+            expect(tab1).toHaveFocus();
+        });
+
+        it('should go to the last tab-button when End key is pressed on a tab-button', async () => {
+            const user = userEvent.setup();
+            const tabs = makeTabsWithContent(3);
+            renderWithProviders(<Tabs tabs={tabs} />);
+            const tab1 = screen.getByTestId('tab-1-button');
+            const tab3 = screen.getByTestId('tab-3-button');
+
+            tab1.focus();
+            await user.keyboard('{End}');
+
+            expect(tab3).toHaveFocus();
         });
     });
 });
