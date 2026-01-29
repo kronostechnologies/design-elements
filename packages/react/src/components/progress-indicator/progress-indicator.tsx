9f8f1687b6588955027379244eedcfa68e43534d
Pierre-Yves Lafleur <pierre-yves.lafleur@equisoft.com>
2026-01-29 16:34:32 -0500

    feat(ProgressIndicator): fix spinner animation (#1352)

--- packages/react/src/components/progress-indicator/progress-indicator.tsx
+++ packages/react/src/components/progress-indicator/progress-indicator.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import { FC, SVGProps } from 'react';
 
-const Spinner: FC<SVGProps<SVGSVGElement>> = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="16" height="16">
+const Spinner: FC<SVGProps<SVGSVGElement>> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="16" height="16" className={className}>
         <radialGradient id="a4" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)">
             <stop offset="0" stopColor="currentColor" stopOpacity="0" />
             <stop offset="0.5" stopColor="currentColor" stopOpacity="0.3" />
@@ -15,6 +15,7 @@ const Spinner: FC<SVGProps<SVGSVGElement>> = () => (
             strokeLinecap="round"
             strokeDasharray="400 400"
             strokeDashoffset="0"
+            transform-origin="center" /* eslint-disable-line react/no-unknown-property */
             cx="100"
             cy="100"
             r="88"
@@ -36,6 +37,7 @@ const Spinner: FC<SVGProps<SVGSVGElement>> = () => (
             stroke="currentColor"
             strokeWidth="24"
             strokeLinecap="round"
+            transform-origin="center" /* eslint-disable-line react/no-unknown-property */
             cx="100"
             cy="100"
             r="88"
