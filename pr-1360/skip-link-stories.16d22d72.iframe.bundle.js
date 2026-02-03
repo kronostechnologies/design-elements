de82b26b4453e9e76d4f558fad6eb76e67d6b45d
kronostechnologies-build <na-qc@equisoft.com>
2026-02-03 18:10:57 +0000

    docs(storybook): update pull request pr-1360 to d0836ff307e6797625c95d46759e677288d7d8f5

---
+++ pr-1360/skip-link-stories.16d22d72.iframe.bundle.js
@@ -0,0 +1,19 @@
+"use strict";(self.webpackChunk_equisoft_design_elements_storybook=self.webpackChunk_equisoft_design_elements_storybook||[]).push([[7287],{"./stories/skip-link.stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>__WEBPACK_DEFAULT_EXPORT__});var _equisoft_design_elements_react__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("../../.yarn/__virtual__/@equisoft-design-elements-react-virtual-0c2f823c14/1/packages/react/dist/bundle.js"),styled_components__WEBPACK_IMPORTED_MODULE_2__=__webpack_require__("../../.yarn/__virtual__/styled-components-virtual-0e8c327dd2/4/.yarn/berry/cache/styled-components-npm-5.3.11-d45616b9af-10c0.zip/node_modules/styled-components/dist/styled-components.browser.esm.js"),react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("../../../../../.yarn/berry/cache/react-npm-17.0.2-99ba37d931-10c0.zip/node_modules/react/jsx-runtime.js");const Navigation=styled_components__WEBPACK_IMPORTED_MODULE_2__.default.nav`
+    align-items: center;
+    background-color: ${({theme})=>theme.ref["color-black"]};
+    border-radius: var(--border-radius) var(--border-radius) 0 0;
+    color: ${({theme})=>theme.ref["color-white"]};
+    display: flex;
+    height: 56px;
+    justify-content: center;
+    width: 100%;
+`,Main=styled_components__WEBPACK_IMPORTED_MODULE_2__.default.main`
+    align-items: center;
+    background-color: ${({theme})=>theme.ref["color-neutral-15"]};
+    border-radius: 0 0 var(--border-radius) var(--border-radius);
+    display: flex;
+    height: 150px;
+    justify-content: center;
+    width: 100%;
+`,SkipLinkMeta={title:"Components/Skip Link",component:_equisoft_design_elements_react__WEBPACK_IMPORTED_MODULE_0__.SkipLink,argTypes:{onClick:{control:{disable:!0}}},render:args=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.Fragment,{children:[(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_equisoft_design_elements_react__WEBPACK_IMPORTED_MODULE_0__.SkipLink,{...args,href:"#main"}),(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(Navigation,{children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("p",{tabIndex:0,children:"Navigation"})}),(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(Main,{id:"main",children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("p",{tabIndex:0,children:"Main content"})})]})},__WEBPACK_DEFAULT_EXPORT__=SkipLinkMeta,Default={...SkipLinkMeta},__namedExportsOrder=["Default"];Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  ...SkipLinkMeta\n}",...Default.parameters?.docs?.source}}}}}]);
+//# sourceMappingURL=skip-link-stories.16d22d72.iframe.bundle.js.map
