44df21eb3d7214d4e7a1015b90e6fb8d02f60601
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2026-02-02 21:34:19 -0500

    fix(Spinner): allow data attributes on the component (DS-1474)

--- packages/react/src/components/spinner/spinner.tsx
+++ packages/react/src/components/spinner/spinner.tsx
@@ -1,8 +1,15 @@
 import type { SVGProps, VFC } from 'react';
 import styled from 'styled-components';
+import { useDataAttributes } from '../../hooks/use-data-attributes';
 
-const Enso: VFC<SVGProps<SVGSVGElement>> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="16" height="16" className={className}>
+const Enso: VFC<SVGProps<SVGSVGElement>> = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 200 200"
+        width="16"
+        height="16"
+        {...props /* eslint-disable-line react/jsx-props-no-spreading */}
+    >
         <radialGradient id="a4" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)">
             <stop offset="0" stopColor="currentColor" stopOpacity="0" />
             <stop offset="0.5" stopColor="currentColor" stopOpacity="0.3" />
@@ -46,10 +53,25 @@ const Enso: VFC<SVGProps<SVGSVGElement>> = ({ className }) => (
 );
 Enso.displayName = 'Enso';
 
-export const Spinner: VFC = styled(Enso)`
+export const StyledSpinner = styled(Enso)`
     color: ${(props) => props.theme.component['spinner-fill-color']};
     height: 64px;
     width: 64px;
 `;
+
+export interface SpinnerProps {
+    className?: string;
+}
+
+export const Spinner: VFC<SpinnerProps> = ({ className, ...props }) => {
+    const dataAttributes = useDataAttributes(props);
+
+    return (
+        <StyledSpinner
+            className={className}
+            {...dataAttributes /* eslint-disable-line react/jsx-props-no-spreading */}
+        />
+    );
+};
 
 Spinner.displayName = 'Spinner';
