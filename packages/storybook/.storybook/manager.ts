a143cf73d70bc4fef69b324ceac7fc62cde50af7
Maxime Meriouma-Caron <meriouma@users.noreply.github.com>
2026-05-08 13:58:31 -0400

    chore: replace changelog with link to GitHub releases (DS-1569) (#1443)

--- packages/storybook/.storybook/manager.ts
+++ packages/storybook/.storybook/manager.ts
@@ -1,12 +1,48 @@
+import { Icon } from '@equisoft/design-elements-react';
+import type { API_SidebarOptions } from '@storybook/core/types';
 import { addons } from '@storybook/manager-api';
 import { create } from '@storybook/theming';
+import { createElement, type MouseEventHandler, type ReactElement } from 'react';
+
+type Item = Parameters<NonNullable<API_SidebarOptions['renderLabel']>>[0];
+
+function createLinkToGithubReleases(item: Item): ReactElement {
+    const handleClick: MouseEventHandler = (event) => {
+        event.stopPropagation();
+    };
+    return createElement(
+        'a',
+        {
+            children: [item.name, createElement(Icon, { name: 'externalLink', size: '18' })],
+            href: 'https://github.com/kronostechnologies/design-elements/releases',
+            onClick: handleClick,
+            rel: 'noopener noreferrer',
+            style: {
+                color: 'inherit',
+                display: 'inline-flex',
+                gap: '0.5rem',
+                textDecoration: 'none',
+                width: '100%',
+            },
+            target: '_blank',
+        },
+    );
+}
 
 addons.setConfig({
+    sidebar: {
+        renderLabel: (item) => {
+            if (item.name === 'Changelog') {
+                return createLinkToGithubReleases(item);
+            }
+            return item.name;
+        },
+    },
     theme: create({
-        base: "light",
-        brandTitle: "Equisoft Design System",
-        brandUrl: "https://equisoft.com",
-        brandImage: "./logo-equisoft.svg", // using publicly served /public directory
+        base: 'light',
+        brandTitle: 'Equisoft Design System',
+        brandUrl: 'https://equisoft.com',
+        brandImage: './logo-equisoft.svg', // using publicly served /public directory
         brandTarget: '_self',
         colorPrimary: '#006296',
         appBg: '#FAFAFA',
