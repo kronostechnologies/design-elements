d8069f012488cf900d031c91f02a23dd3131e3d1
marc-dupuis-equisoft <marc.dupuis@equisoft.com>
2026-05-08 11:56:33 -0400

    fix(Avatar): allow numeric initials (LG-1262) (#1442)

--- packages/react/src/utils/user.ts
+++ packages/react/src/utils/user.ts
@@ -1,12 +1,12 @@
 export function getInitialsFromUsername(username: string): string {
-    const usernameWithoutNonAlphaCharacters = username.normalize('NFD').replace(/[^a-zA-Z\s]/g, '');
-    if (usernameWithoutNonAlphaCharacters.length === 0) {
+    const userNameAlphanumerical = username.normalize('NFD').replace(/[^a-zA-Z0-9\s]/g, '');
+    if (userNameAlphanumerical.length === 0) {
         return '';
     }
 
-    const nameParts: string[] = usernameWithoutNonAlphaCharacters.split(/\s/);
-    if (nameParts.length === 1 && usernameWithoutNonAlphaCharacters.length > 0) {
-        return usernameWithoutNonAlphaCharacters.substring(0, 2).toUpperCase();
+    const nameParts: string[] = userNameAlphanumerical.split(/\s/);
+    if (nameParts.length === 1 && userNameAlphanumerical.length > 0) {
+        return userNameAlphanumerical.substring(0, 2).toUpperCase();
     }
 
     return nameParts[0].charAt(0).toUpperCase() + nameParts[nameParts.length - 1].charAt(0).toUpperCase();
