3d525d8734c7a19761727f6054a22e4d009b7762
Jean-Simon Garneau <jean-simon.garneau@equisoft.com>
2026-06-19 11:09:07 -0400

    feat(Icon): added custom SVG support for Icon (DS-1592)  (#1465)

--- packages/react/src/components/icon/icon.test.tsx
+++ packages/react/src/components/icon/icon.test.tsx
@@ -1,10 +1,27 @@
+import { type FC, type SVGProps } from 'react';
 import { renderWithProviders } from '../../test-utils/renderer';
 import { Icon } from './icon';
+
+const CustomSvg: FC<SVGProps<SVGSVGElement>> = (props) => (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <svg data-testid="custom-svg" {...props}>
+        <path d="M0 0h24v24H0z" />
+    </svg>
+);
 
 describe('Icon', () => {
     it('matches the snapshot', () => {
         const { asFragment } = renderWithProviders(<Icon name="home" />);
 
         expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('renders a custom svg component passed through the svg prop', () => {
+        const { getByTestId } = renderWithProviders(<Icon svg={CustomSvg} size="32" color="red" />);
+
+        const svg = getByTestId('custom-svg');
+        expect(svg).toHaveAttribute('width', '32');
+        expect(svg).toHaveAttribute('height', '32');
+        expect(svg).toHaveAttribute('color', 'red');
     });
 });
